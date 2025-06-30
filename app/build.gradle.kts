f9fd97a31abe2caa0a5638971e611b6cb16133b6
Fares <dev.faresmohamed@gmail.com>
2025-06-30 18:53:28 +0300

    fix: change application id and namespace to match package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.novix"
+        applicationId = "com.baghdad.novix"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
