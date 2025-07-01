bc14fd71cb25b5d092e24f2eedcabedcd26c8143
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-07-01 19:11:13 +0300

    feat: initialize color system with darkThemeColor and lightThemeColor palettes

---
+++ design_system/src/main/java/com/baghdad/design_system/color/theme/darkThemeColor.kt
@@ -0,0 +1,34 @@
+package com.baghdad.design_system.color.theme
+
+import androidx.compose.ui.graphics.Color
+import com.baghdad.design_system.color.model.*
+
+val darkThemeColor = NovixColor(
+    primaryColor = Primary(
+        normal = Color(0xFFC65A42),
+        variant = Color(0xFF1F0E0A)
+    ),
+    secondaryColor = Color(0xFF4B0412),
+    textColor = TextColor(
+        title = Color(0xDEFFFFFF),
+        body = Color(0x99FFFFFF),
+        hint = Color(0x61FFFFFF),
+        stroke = Color(0x14FFFFFF),
+        onPrimary = Color(0xDEFFFFFF),
+        onPrimaryHint = Color(0x61FFFFFF),
+        iconBackground = Color(0xB2000000),
+        iconBackgroundLow = Color(0x1FFFFFFF),
+        backgroundLow = Color(0x08FFFFFF),
+        disable = Color(0xFF1F1C1B)
+    ),
+    surfaceColor = SurfaceColor(
+        surface = Color(0xFF0D0608),
+        surfaceHigh = Color(0xFF110E0F)
+    ),
+    status = StatusColor(
+        redAccent = Color(0xFFF75662),
+        yellowAccent = Color(0xFFCFC657),
+        greenAccent = Color(0xFF19744D),
+        greenVariant = Color(0xFF1D1F1E)
+    )
+)
---
+++ design_system/src/main/java/com/baghdad/design_system/color/theme/lightThemeColor.kt
@@ -0,0 +1,38 @@
+package com.baghdad.design_system.color.theme
+
+import androidx.compose.ui.graphics.Color
+import com.baghdad.design_system.color.model.NovixColor
+import com.baghdad.design_system.color.model.Primary
+import com.baghdad.design_system.color.model.StatusColor
+import com.baghdad.design_system.color.model.SurfaceColor
+import com.baghdad.design_system.color.model.TextColor
+
+val lightThemeColor = NovixColor(
+    primaryColor = Primary(
+        normal = Color(0xFFF77053),
+        variant = Color(0xFFF9E8E5)
+    ),
+    secondaryColor = Color(0xFFF9E8E5),
+    textColor = TextColor(
+        title = Color(0xDE1F1F1F),
+        body = Color(0x991F1F1F),
+        hint = Color(0x611F1F1F),
+        stroke = Color(0x141F1F1F),
+        onPrimary = Color(0xDEFFFFFF),
+        onPrimaryHint = Color(0x61FFFFFF),
+        iconBackground = Color(0xB2000000),
+        iconBackgroundLow = Color(0x52FFFFFF),
+        backgroundLow = Color(0x08FFFFFF),
+        disable = Color(0xFFCCBFBC)
+    ),
+    surfaceColor = SurfaceColor(
+        surface = Color(0xFFFCF9F5),
+        surfaceHigh = Color(0xFFFFFFFF)
+    ),
+    status = StatusColor(
+        redAccent = Color(0xFFF75662),
+        yellowAccent = Color(0xFFFFC74D),
+        greenAccent = Color(0xFF19744D),
+        greenVariant = Color(0xFFCFE5DC)
+    )
+)
