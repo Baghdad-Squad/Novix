c168eff5741766a08b1bd471a7a76341cb445e84
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-07-01 19:10:07 +0300

    feat: initialize color system with NovixColor palettes

---
+++ design_system/src/main/java/com/baghdad/design_system/color/model/NovixColor.kt
@@ -0,0 +1,41 @@
+package com.baghdad.design_system.color.model
+
+import androidx.compose.ui.graphics.Color
+
+data class NovixColor(
+    val primaryColor: Primary,
+    val secondaryColor: Color,
+    val textColor: TextColor,
+    val surfaceColor: SurfaceColor,
+    val status: StatusColor
+)
+
+data class Primary(
+    val normal: Color,
+    val variant: Color,
+)
+
+data class TextColor(
+    val title: Color,
+    val body: Color,
+    val hint: Color,
+    val stroke: Color,
+    val onPrimary: Color,
+    val onPrimaryHint: Color,
+    val iconBackground: Color,
+    val iconBackgroundLow: Color,
+    val backgroundLow: Color,
+    val disable: Color
+)
+
+data class SurfaceColor(
+    val surface: Color,
+    val surfaceHigh: Color,
+)
+
+data class StatusColor(
+    val redAccent: Color,
+    val yellowAccent: Color,
+    val greenAccent: Color,
+    val greenVariant: Color,
+)
