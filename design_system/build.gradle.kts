48df2b516309d9c4228c80c7d327c63dd0799f50
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-07-02 05:49:02 +0300

    feat: implement NovixTheme.kt for theme setup and composition locals

--- design_system/build.gradle.kts
+++ design_system/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -37,6 +38,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.runtime.android)
+    implementation(libs.androidx.ui.graphics.android)
+    implementation(libs.androidx.ui.text.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
