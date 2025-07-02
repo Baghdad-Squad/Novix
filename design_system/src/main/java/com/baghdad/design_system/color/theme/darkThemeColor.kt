06ca536b2cc622b4f14905cb56367581c15baf70
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-07-02 22:44:53 +0300

    fix: reassign the dark and light values

--- design_system/src/main/java/com/baghdad/design_system/color/theme/darkThemeColor.kt
+++ design_system/src/main/java/com/baghdad/design_system/color/theme/darkThemeColor.kt
@@ -1,15 +1,13 @@
 package com.baghdad.design_system.color.theme
 
 import androidx.compose.ui.graphics.Color
-import com.baghdad.design_system.color.model.*
+import com.baghdad.design_system.color.model.NovixColor
 
-val darkThemeColor = NovixColor(
-    primary = Primary(
+val darkThemeColor =
+    NovixColor(
         normal = Color(0xFFC65A42),
-        variant = Color(0xFF1F0E0A)
-    ),
-    secondary = Color(0xFF4B0412),
-    text = TextColor(
+        variant = Color(0xFF1F0E0A),
+        secondary = Color(0xFF4B0412),
         title = Color(0xDEFFFFFF),
         body = Color(0x99FFFFFF),
         hint = Color(0x61FFFFFF),
@@ -19,16 +17,11 @@ val darkThemeColor = NovixColor(
         iconBackground = Color(0xB2000000),
         iconBackgroundLow = Color(0x1FFFFFFF),
         backgroundLow = Color(0x08FFFFFF),
-        disable = Color(0xFF1F1C1B)
-    ),
-    surface = SurfaceColor(
+        disable = Color(0xFF1F1C1B),
         surface = Color(0xFF0D0608),
-        surfaceHigh = Color(0xFF110E0F)
-    ),
-    status = StatusColor(
+        surfaceHigh = Color(0xFF110E0F),
         redAccent = Color(0xFFF75662),
         yellowAccent = Color(0xFFCFC657),
         greenAccent = Color(0xFF19744D),
         greenVariant = Color(0xFF1D1F1E)
     )
-)
--- design_system/src/main/java/com/baghdad/design_system/color/theme/lightThemeColor.kt
+++ design_system/src/main/java/com/baghdad/design_system/color/theme/lightThemeColor.kt
@@ -2,18 +2,12 @@ package com.baghdad.design_system.color.theme
 
 import androidx.compose.ui.graphics.Color
 import com.baghdad.design_system.color.model.NovixColor
-import com.baghdad.design_system.color.model.Primary
-import com.baghdad.design_system.color.model.StatusColor
-import com.baghdad.design_system.color.model.SurfaceColor
-import com.baghdad.design_system.color.model.TextColor
 
-val lightThemeColor = NovixColor(
-    primary = Primary(
+val lightThemeColor =
+    NovixColor(
         normal = Color(0xFFF77053),
-        variant = Color(0xFFF9E8E5)
-    ),
-    secondary = Color(0xFFF9E8E5),
-    text = TextColor(
+        variant = Color(0xFFF9E8E5),
+        secondary = Color(0xFFF9E8E5),
         title = Color(0xDE1F1F1F),
         body = Color(0x991F1F1F),
         hint = Color(0x611F1F1F),
@@ -23,16 +17,11 @@ val lightThemeColor = NovixColor(
         iconBackground = Color(0xB2000000),
         iconBackgroundLow = Color(0x52FFFFFF),
         backgroundLow = Color(0x08FFFFFF),
-        disable = Color(0xFFCCBFBC)
-    ),
-    surface = SurfaceColor(
+        disable = Color(0xFFCCBFBC),
         surface = Color(0xFFFCF9F5),
-        surfaceHigh = Color(0xFFFFFFFF)
-    ),
-    status = StatusColor(
+        surfaceHigh = Color(0xFFFFFFFF),
         redAccent = Color(0xFFF75662),
         yellowAccent = Color(0xFFFFC74D),
         greenAccent = Color(0xFF19744D),
         greenVariant = Color(0xFFCFE5DC)
     )
-)
