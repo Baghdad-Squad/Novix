b10d534866f66e305180bdcea13c23d488e53759
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-07-02 12:01:27 +0300

    feat: implement Dimens to contain all the values with dp

---
+++ design_system/src/main/java/com/baghdad/design_system/dimensions/Dimens.kt
@@ -0,0 +1,18 @@
+package com.baghdad.design_system.dimensions
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val Tiny = 4.dp
+    val Small = 8.dp
+    val XSmall = 12.dp
+    val XXSmall = 14.dp
+    val Medium = 16.dp
+    val Large = 24.dp
+    val XLarge = 32.dp
+    val XsLarge = 39.dp
+    val XXsLarge = 48.dp
+    val XxLarge = 52.dp
+    val XxxsLarge = 56.dp
+    val XxxLarge = 64.dp
+}
