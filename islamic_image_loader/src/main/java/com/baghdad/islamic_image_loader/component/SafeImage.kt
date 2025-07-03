492308dbb8f8ff70ef398347400192adff1752d4
Aboud <gh.abd.com.207@gmail.com>
2025-07-03 22:43:08 +0300

    fix: default image display in SafeImage composable

--- islamic_image_loader/src/main/java/com/baghdad/islamic_image_loader/component/SafeImage.kt
+++ islamic_image_loader/src/main/java/com/baghdad/islamic_image_loader/component/SafeImage.kt
@@ -1,6 +1,7 @@
 package com.baghdad.islamic_image_loader.component
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil3.Image
 import coil3.compose.asPainter
@@ -71,9 +73,8 @@ fun SafeImage(
             )
         } else {
             Image(
-                painter = R.drawable.img_defualt_image,
-                contentDescription = "Default Image",
-                modifier = Modifier.blur(radius = 16.dp),
+                painter = painterResource(R.drawable.img_defualt_image),
+                contentDescription = "Default Image"
             )
         }
     }
