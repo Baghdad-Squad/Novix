d87807c741181cb19cee7fd0ede2f11bbed27092
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-03 23:05:53 +0300

    feat: refactor NovixSelection component and add Selectable data class

--- design_system/src/main/java/com/baghdad/design_system/component/NovixSelection.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/NovixSelection.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -20,46 +18,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.baghdad.design_system.shared.Selectable
 import com.baghdad.design_system.theme.Theme
 
-
 @Composable
-fun NovixThemeSelection(
-    isDarkSelected: Boolean,
-    onThemeSelected: (Boolean) -> Unit
-) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = Modifier.padding(horizontal = 16.dp),
-    ) {
-        ButtonSelection(
-            selectedOption = "Dark",
-            isSelected = isDarkSelected,
-            onClick = { onThemeSelected(true) }
-        )
-
-        ButtonSelection(
-            selectedOption = "Light",
-            isSelected = !isDarkSelected,
-            onClick = { onThemeSelected(false) }
-        )
-    }
-}
-
-@Composable
-fun ButtonSelection(
-    selectedOption: String,
-    isSelected: Boolean,
+fun NovixSelection(
+    option: Selectable <String>,
     onClick: () -> Unit
 ) {
     val animatedBackgroundColor by animateColorAsState(
-        targetValue = if (isSelected) Theme.color.primaryVariant
+        targetValue = if (option.isSelected) Theme.color.primaryVariant
         else Color.Transparent,
         animationSpec = tween(500)
     )
 
     val animatedBorderColor by animateColorAsState(
-        targetValue = if (isSelected) Theme.color.primary
+        targetValue = if (option.isSelected) Theme.color.primary
         else Theme.color.stroke,
         animationSpec = tween(500)
     )
@@ -67,11 +41,9 @@ fun ButtonSelection(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(
-                animatedBackgroundColor, RoundedCornerShape(8.dp)
-            )
+            .background(animatedBackgroundColor, RoundedCornerShape(8.dp))
             .border(
-                width = 2.dp,
+                width = 1.5f.dp,
                 color = animatedBorderColor,
                 shape = RoundedCornerShape(8.dp)
             )
@@ -85,12 +57,10 @@ fun ButtonSelection(
         contentAlignment = Alignment.CenterStart
     ) {
         Text(
-            text = selectedOption,
-            modifier = Modifier
-                .padding(horizontal = 12.dp, vertical = 16.dp),
+            text = option.value,
+            modifier = Modifier.padding(horizontal = 12.dp, vertical = 16.dp),
             color = Theme.color.body,
             style = Theme.typography.label.large
         )
-
     }
 }
