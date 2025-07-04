0abebe4a55b443269110363b4a6b6294f691b8dd
Malak Raef <malak.raef20@gmail.com>
2025-07-04 14:25:10 +0300

    refactor: update rating star color and extract string resource

--- design_system/src/main/java/com/baghdad/design_system/component/RatingChip.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/RatingChip.kt
@@ -3,7 +3,6 @@ package com.baghdad.design_system.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -15,8 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.baghdad.design_system.R
@@ -28,7 +27,7 @@ fun RatingChip(
     rating: String,
     modifier: Modifier = Modifier
 ) {
-    Box(
+    Row(
         modifier = modifier
             .clip(RoundedCornerShape(8.dp))
             .background(color = Theme.color.iconBackground)
@@ -38,26 +37,21 @@ fun RatingChip(
                 shape = RoundedCornerShape(8.dp)
             )
             .padding(horizontal = 12.dp, vertical = 7.dp),
-        contentAlignment = Alignment.Center,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
 
-            Icon(
-                painter = painterResource(id = R.drawable.ic_rating_star),
-                contentDescription = null,
-                tint = Color(0xFFCFC657),
-                modifier = Modifier.size(12.dp)
-            )
-            Text(
-                text = rating,
-                style = Theme.typography.label.small,
-                color = Theme.color.onPrimary
-            )
-        }
-
+        Icon(
+            painter = painterResource(id = R.drawable.ic_rating_star),
+            contentDescription = stringResource(R.string.rating_star),
+            tint = Theme.color.yellowAccent,
+            modifier = Modifier.size(12.dp)
+        )
+        Text(
+            text = rating,
+            style = Theme.typography.label.small,
+            color = Theme.color.onPrimary
+        )
     }
 }
 
