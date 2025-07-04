55d67ac30f3207f2d93c132e4d29d860f9d79290
Malak Raef <malak.raef20@gmail.com>
2025-07-04 03:21:23 +0300

    feat: add rating star chip component

---
+++ design_system/src/main/java/com/baghdad/design_system/component/RatingChip.kt
@@ -0,0 +1,78 @@
+package com.baghdad.design_system.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.baghdad.design_system.R
+import com.baghdad.design_system.theme.NovixTheme
+import com.baghdad.design_system.theme.Theme
+
+@Composable
+fun RatingChip(
+    rating: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = Theme.color.iconBackground)
+            .border(
+                width = 1.dp,
+                color = Theme.color.stroke,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(horizontal = 12.dp, vertical = 7.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_rating_star),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier.size(12.dp)
+            )
+            Text(
+                text = rating,
+                style = Theme.typography.label.small,
+                color = Theme.color.onPrimary
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun RatingChipPrev() {
+    NovixTheme(isDarkTheme = true) {
+        Column(
+            modifier = Modifier
+                .background(color = Theme.color.surface)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            RatingChip(rating = "9.9")
+        }
+    }
+}
