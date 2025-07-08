2dce5cd389e5b89bcf754a8b7f97fbac8ed64a85
edrees <muhammed.edrees101@gmail.com>
2025-07-08 17:54:39 +0300

    feat: add media, person, and search entities

---
+++ entity/src/main/java/com/baghdad/entity/media/TvShow.kt
@@ -0,0 +1,19 @@
+package com.baghdad.entity.media
+
+import com.baghdad.entity.person.CastMember
+import kotlinx.datetime.LocalDate
+
+data class TvShow(
+    override val id: Long,
+    override val title: String,
+    override val genres: List<Genre>,
+    override val imdbRating: Double,
+    override val userRating: Double?,
+    override val releaseDate: LocalDate,
+    override val overview: String,
+    override val cast: List<CastMember>,
+    override val posterPictureURL: String,
+    override val backdropPicturesURLs: List<String>,
+    override val isBookmarked: Boolean,
+    val numberOfSeasons: Int
+) : Media
