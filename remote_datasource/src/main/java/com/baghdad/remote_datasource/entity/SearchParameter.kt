a4adac11f2fbad442c9e3c8a8c8bdc21b9fa6c70
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-09 01:56:46 +0300

    feat: add extension function to convert SearchParameter to query parameters

--- remote_datasource/src/main/java/com/baghdad/remote_datasource/entity/SearchParameter.kt
+++ remote_datasource/src/main/java/com/baghdad/remote_datasource/entity/SearchParameter.kt
@@ -6,4 +6,10 @@ data class SearchParameter(
     val language: String = "en-US",
     val includeAdult: Boolean = false,
 )
+fun SearchParameter.toParams(): Map<String, String> = mapOf(
+    "query" to query,
+    "page" to pageNumber.toString(),
+    "language" to language,
+    "include_adult" to includeAdult.toString()
+)
 
