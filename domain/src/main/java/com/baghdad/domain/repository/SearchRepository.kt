d3dea7c7d696dde5eafeccf8bf6ec707aca0336e
omer1998 <omerfaris11@gmail.com>
2025-07-09 16:26:23 +0300

    feat: implement search repository interface and search result model

---
+++ domain/src/main/java/com/baghdad/domain/repository/SearchRepository.kt
@@ -0,0 +1,13 @@
+package com.baghdad.domain.repository
+
+import com.baghdad.domain.model.SearchResult
+import com.baghdad.entity.search.RecentSearch
+
+interface SearchRepository {
+    suspend fun searchByName(query : String) : SearchResult
+    suspend fun getRecentSearches() : List<RecentSearch>
+    suspend fun saveRecentSearch(query : String)
+    suspend fun saveSearchResults(query : String, searchResult: SearchResult)
+    suspend fun deleteRecentSearchById(id : Long)
+    suspend fun deleteAllRecentSearches()
+}
