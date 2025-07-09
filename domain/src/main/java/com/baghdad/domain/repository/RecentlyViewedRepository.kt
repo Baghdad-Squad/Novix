3d3ada87d6bb7c93cd39d197114388ada22a6e23
omer1998 <omerfaris11@gmail.com>
2025-07-09 18:37:52 +0300

    feat: implement necessary repositories for movie, tv show and recently vierwed media

---
+++ domain/src/main/java/com/baghdad/domain/repository/RecentlyViewedRepository.kt
@@ -0,0 +1,8 @@
+package com.baghdad.domain.repository
+
+import com.baghdad.entity.media.Media
+
+interface RecentlyViewedRepository {
+    suspend fun getAllRecentlyViewed() : List<Media>
+    suspend fun deleteAllRecentlyViewed()
+}
