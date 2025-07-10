dd876f0aacc4d85c111460c83a3102827d3d05ae
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-10 09:05:08 +0300

    feat: add mappers for genre IDs and cast members in remote data source

---
+++ remote_datasource/src/main/java/com/baghdad/remote_datasource/mapper/getCastMembers.kt
@@ -0,0 +1,33 @@
+package com.baghdad.remote_datasource.mapper
+
+import com.baghdad.remote_datasource.entity.MultiSearchItem
+import com.baghdad.repository.model.ActorDto
+import com.baghdad.repository.model.CastMemberDto
+
+internal fun MultiSearchItem.toCastMember(): CastMemberDto? {
+    return if (this.mediaType == "person" && this.id != null) {
+        CastMemberDto(
+            actor = ActorDto(
+                id = this.id,
+                name = this.tvShowName ?: "Unknown Actor",
+                imageUrl = this.profilePath ?: ""
+            ),
+            characterName = this.knownForDepartment ?: "Unknown Character"
+        )
+    } else {
+        null
+    }
+}
+
+fun getCastMembers(items: List<MultiSearchItem>): List<CastMemberDto> {
+    return items.map{
+        it.toCastMember() ?: CastMemberDto(
+            actor = ActorDto(
+                id = -1,
+                name = "Unknown Actor",
+                imageUrl = ""
+            ),
+            characterName = "Unknown Character"
+        )
+    }
+}
