ed25e32288269e5aa1e13740069ca05a013c2c0d
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-10 10:51:17 +0300

    feat: add RecentSearch entity and RecentSearchDao for managing recent search data

---
+++ local_datasource/src/main/java/com/baghdad/local_datasource/roomDB/dao/RecentSearchDao.kt
@@ -0,0 +1,40 @@
+package com.baghdad.local_datasource.roomDB.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import com.baghdad.local_datasource.roomDB.entity.RecentSearch
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecentSearchDao {
+
+    @Query("SELECT * FROM RecentSearch")
+    suspend fun getAllRecentSearch(): Flow<List<RecentSearch>>
+
+    @Query("SELECT * FROM RecentSearch WHERE id = :id")
+    suspend fun getRecentSearchById(id: Long)
+
+    @Transaction
+    @Query("""
+        SELECT * FROM RecentSearch
+        ORDER BY time DESC 
+        LIMIT 10
+    """)
+    suspend fun getLastTenRecentSearchItems(): List<RecentViewedWithDetails>
+
+
+    @Query("DElETE FROM RecentSearch")
+    suspend fun clearAllRecentSearch()
+
+    @Query("SELECT COUNT(*) FROM RecentSearch")
+    suspend fun getCount(): Int
+
+    @Query("SELECT EXISTS(SELECT 1 FROM RecentSearch WHERE id = :id)")
+    suspend fun exists(id: Long): Boolean
+
+    @Query("DELETE FROM RecentSearch WHERE id = :id")
+    suspend fun deleteRecentSearchById(id: Long)
+
+
+}
