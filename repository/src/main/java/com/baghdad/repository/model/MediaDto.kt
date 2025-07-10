67de314832c755a7940e1ae013e11f296a99d69d
omer1998 <omerfaris11@gmail.com>
2025-07-10 15:49:05 +0300

    feat: add mediaDto interface to be implemented by movie and tv show

---
+++ repository/src/main/java/com/baghdad/repository/model/MediaDto.kt
@@ -0,0 +1,15 @@
+package com.baghdad.repository.model
+
+
+interface MediaDto {
+    val id: Long
+    val title: String
+    val genres: List<GenreDto>
+    val imdbRating: Double
+    val userRating: Double?
+    val releaseDate: String
+    val overview: String
+    val cast: List<CastMemberDto>
+    val posterPictureURL: String
+    val backdropPicturesURLs: List<String>
+}
