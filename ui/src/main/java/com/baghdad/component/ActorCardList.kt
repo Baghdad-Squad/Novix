7892262909f87c6dc492ff1b99cf7cf23d774e52
Farah315 <fara734isgreat@gmail.com>
2025-07-11 12:48:21 +0300

    feat: add ActorCardList

---
+++ ui/src/main/java/com/baghdad/component/ActorCardList.kt
@@ -0,0 +1,34 @@
+package com.baghdad.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.baghdad.design_system.component.ActorCard
+
+data class Actor(
+    val name: String,
+    val image: String
+)
+
+@Composable
+fun ActorCardList(
+    actors: List<Actor>,
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .height(900.dp)
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(horizontal = 8.dp, vertical = 8.dp)
+    ) {
+        items(actors) { actor ->
+            ActorCard(
+                actorName = actor.name,
+                actorImage = actor.image,
+                )
+        }
+    }
+}
