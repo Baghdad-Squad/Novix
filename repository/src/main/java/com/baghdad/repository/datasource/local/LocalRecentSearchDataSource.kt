5bf923ebd687805eb0259dfb37d3c1ab5a130d1f
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-12 02:39:23 +0300

    refactor: rename LocalSearchDataSource to LocalRecentSearchDataSource
    This commit renames the interface `LocalSearchDataSource` to `LocalRecentSearchDataSource` to better reflect its purpose of handling recent search queries.

--- repository/src/main/java/com/baghdad/repository/datasource/local/LocalRecentSearchDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/LocalRecentSearchDataSource.kt
@@ -3,7 +3,7 @@ package com.baghdad.repository.datasource.local
 import com.baghdad.repository.model.RecentSearchDto
 import kotlinx.coroutines.flow.Flow
 
-interface LocalSearchDataSource {
+interface LocalRecentSearchDataSource {
     suspend fun addRecentSearchQuery(query: String)
     fun getAllRecentSearches(): Flow<List<RecentSearchDto>>
     suspend fun deleteRecentSearchById(id: Long)
