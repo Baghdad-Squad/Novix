35659b4a3c4520869ad60ae0e62187efef2b2d35
Aboud <gh.abd.com.207@gmail.com>
2025-07-11 22:03:34 +0300

    refactor: update SearchResult import path

--- repository/src/main/java/com/baghdad/repository/mapper/SearchResultMapper.kt
+++ repository/src/main/java/com/baghdad/repository/mapper/SearchResultMapper.kt
@@ -1,6 +1,6 @@
 package com.baghdad.repository.mapper
 
-import com.baghdad.domain.result.SearchResult
+import com.baghdad.domain.model.search.SearchResult
 import com.baghdad.repository.model.SearchResultDto
 
 internal fun SearchResultDto.toEntity(): SearchResult {
