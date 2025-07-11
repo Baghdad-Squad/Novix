4c0b6bd647cd969bc6c7ddd30e43896a5dd60fb0
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-12 02:48:01 +0300

    feat: save search results to local data sources

--- repository/src/main/java/com/baghdad/repository/SearchRepositoryImpl.kt
+++ repository/src/main/java/com/baghdad/repository/SearchRepositoryImpl.kt
@@ -3,7 +3,10 @@ package com.baghdad.repository
 import com.baghdad.domain.model.search.SearchResult
 import com.baghdad.domain.repository.SearchRepository
 import com.baghdad.entity.search.RecentSearch
-import com.baghdad.repository.datasource.local.LocalSearchDataSource
+import com.baghdad.repository.datasource.local.LocalActorDataSource
+import com.baghdad.repository.datasource.local.LocalMovieDataSource
+import com.baghdad.repository.datasource.local.LocalRecentSearchDataSource
+import com.baghdad.repository.datasource.local.LocalTvShowDataSource
 import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
 import com.baghdad.repository.datasource.remote.RemoteSearchDataSource
 import com.baghdad.repository.mapper.toEntity
@@ -15,9 +18,12 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 class SearchRepositoryImpl(
-    val searchRemoteDataSource: RemoteSearchDataSource,
-    val remoteGenreDataSource: RemoteGenreDataSource,
-    val localSearchDataSource: LocalSearchDataSource
+    private val searchRemoteDataSource: RemoteSearchDataSource,
+    private val remoteGenreDataSource: RemoteGenreDataSource,
+    private val localRecentSearchDataSource: LocalRecentSearchDataSource,
+    private val localActorDataSource: LocalActorDataSource,
+    private val localMovieDataSource: LocalMovieDataSource,
+    private val localTvShowDataSource: LocalTvShowDataSource
 ) : SearchRepository {
     override suspend fun searchByName(query: String): SearchResult {
         val searchResult = executeSafely {
@@ -25,18 +31,29 @@ class SearchRepositoryImpl(
             val tvShowsGenres = remoteGenreDataSource.getTvShowGenre(language = "en")
             searchRemoteDataSource.searchMultiMedia(
                 query = query, pageNumber = 1, movieGenres, tvShowsGenres
-            ).toEntity()
+            )
         }
+
         executeSafely {
-            localSearchDataSource.addRecentSearchQuery(query)
+            localRecentSearchDataSource.addRecentSearchQuery(query)
+            searchResult.actors.forEach {
+                localActorDataSource.addActor(it.name, it.imageUrl)
+            }
+            searchResult.movies.forEach {
+                localMovieDataSource.addMovie(it)
+            }
+            searchResult.tvShows.forEach {
+                localTvShowDataSource.addTvShow(it)
+            }
         }
-        return searchResult
+
+        return searchResult.toEntity()
 
     }
 
     override fun getRecentSearches(): Flow<List<RecentSearch>> {
         return getFlowSafely {
-            localSearchDataSource.getAllRecentSearches().map {
+            localRecentSearchDataSource.getAllRecentSearches().map {
                 it.map {
                     it.toEntity()
                 }
@@ -47,18 +64,18 @@ class SearchRepositoryImpl(
 
     override suspend fun deleteRecentSearchById(id: Long) {
         return executeSafely {
-            localSearchDataSource.deleteRecentSearchById(id)
+            localRecentSearchDataSource.deleteRecentSearchById(id)
         }
     }
 
     override suspend fun deleteAllRecentSearches() {
         return executeSafely {
-            localSearchDataSource.deleteAllRecentSearches()
+            localRecentSearchDataSource.deleteAllRecentSearches()
         }
     }
 
     private suspend fun isUserSearchThisTermBeforeWithinHour(query: String): Boolean {
-        val recentSearches = localSearchDataSource.getAllRecentSearches().first()
+        val recentSearches = localRecentSearchDataSource.getAllRecentSearches().first()
         return recentSearches.any {
             it.query == query && (System.currentTimeMillis() - it.searchedAt) < 3600000
         }
