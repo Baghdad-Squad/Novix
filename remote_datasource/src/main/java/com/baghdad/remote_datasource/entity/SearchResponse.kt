c0c3f2c9b319b7aa2b5aa908d1ba61beabf7f378
Aboud <gh.abd.com.207@gmail.com>
2025-07-11 04:38:43 +0300

    refactor: simplify SearchResponse data classes

--- remote_datasource/src/main/java/com/baghdad/remote_datasource/entity/SearchResponse.kt
+++ remote_datasource/src/main/java/com/baghdad/remote_datasource/entity/SearchResponse.kt
@@ -5,147 +5,58 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchResponse(
-    @SerialName("page")
-    val pageNumber: Int? = null,
-
-    @SerialName("results")
-    val results: List<MultiSearchItem> = emptyList(),
-
-    @SerialName("total_results")
-    val totalResults: Int? = null,
-
-    @SerialName("total_pages")
-    val totalPages: Int? = null
+    @SerialName("page") val pageNumber: Int? = null,
+    @SerialName("results") val results: List<MultiSearchItem> = emptyList(),
+    @SerialName("total_results") val totalResults: Int? = null,
+    @SerialName("total_pages") val totalPages: Int? = null
 )
 
 @Serializable
 data class MultiSearchItem(
-    @SerialName("id")
-    val id: Long? = null,
-
-    @SerialName("media_type")
-    val mediaType: String? = null,
-
-    @SerialName("overview")
-    val movieOverview: String? = null,
-
-    @SerialName("title")
-    val movieTitle: String? = null,
-
-    @SerialName("name")
-    val tvShowName: String? = null,
-
-    @SerialName("release_date")
-    val releaseDate: String? = null,
-
-    @SerialName("first_air_date")
-    val firstAirDate: String? = null,
-
-    @SerialName("original_name")
-    val tvShowOriginalName: String? = null,
-
-    @SerialName("poster_path")
-    val tvShowPosterPath: String? = null,
-
-    @SerialName("profile_path")
-    val profilePath: String? = null,
-
-    @SerialName("known_for_department")
-    val knownForDepartment: String? = null,
-
-    @SerialName("known_for")
-    val knownFor: List<KnownForItem> = emptyList(),
-
-    @SerialName("gender")
-    val gender: Int? = null,
-
-    @SerialName("adult")
-    val adult: Boolean? = null,
-
-    @SerialName("backdrop_path")
-    val backdropPath: String? = null,
-
-    @SerialName("original_title")
-    val originalTitle: String? = null,
-
-    @SerialName("original_language")
-    val originalLanguage: String? = null,
-
-    @SerialName("genre_ids")
-    val genreIds: List<Int> = emptyList(),
-
-    @SerialName("popularity")
-    val popularity: Double? = null,
-
-    @SerialName("video")
-    val video: Boolean? = null,
-
-    @SerialName("vote_average")
-    val voteAverage: Double? = null,
-
-    @SerialName("vote_count")
-    val voteCount: Int? = null,
-
-    @SerialName("origin_country")
-    val originCountry: List<String> = emptyList()
+    @SerialName("id")val id: Long? = null,
+    @SerialName("media_type") val mediaType: String? = null,
+    @SerialName("overview") val movieOverview: String? = null,
+    @SerialName("title") val movieTitle: String? = null,
+    @SerialName("name") val tvShowName: String? = null,
+    @SerialName("release_date") val releaseDate: String? = null,
+    @SerialName("first_air_date") val firstAirDate: String? = null,
+    @SerialName("original_name") val tvShowOriginalName: String? = null,
+    @SerialName("poster_path") val tvShowPosterPath: String? = null,
+    @SerialName("profile_path") val profilePath: String? = null,
+    @SerialName("known_for_department") val knownForDepartment: String? = null,
+    @SerialName("known_for") val knownFor: List<KnownForItem> = emptyList(),
+    @SerialName("gender") val gender: Int? = null,
+    @SerialName("adult") val adult: Boolean? = null,
+    @SerialName("backdrop_path") val backdropPath: String? = null,
+    @SerialName("original_title") val originalTitle: String? = null,
+    @SerialName("original_language") val originalLanguage: String? = null,
+    @SerialName("genre_ids") val genreIds: List<Int> = emptyList(),
+    @SerialName("popularity") val popularity: Double? = null,
+    @SerialName("video") val video: Boolean? = null,
+    @SerialName("vote_average") val voteAverage: Double? = null,
+    @SerialName("vote_count") val voteCount: Int? = null,
+    @SerialName("origin_country") val originCountry: List<String> = emptyList()
 )
 
 @Serializable
 data class KnownForItem(
-    @SerialName("id")
-    val id: Long? = null,
-
-    @SerialName("title")
-    val title: String? = null,
-
-    @SerialName("name")
-    val name: String? = null,
-
-    @SerialName("media_type")
-    val mediaType: String? = null,
-
-    @SerialName("overview")
-    val overview: String? = null,
-
-    @SerialName("poster_path")
-    val posterPath: String? = null,
-
-    @SerialName("backdrop_path")
-    val backdropPath: String? = null,
-
-    @SerialName("release_date")
-    val releaseDate: String? = null,
-
-    @SerialName("first_air_date")
-    val firstAirDate: String? = null,
-
-    @SerialName("original_title")
-    val originalTitle: String? = null,
-
-    @SerialName("original_name")
-    val originalName: String? = null,
-
-    @SerialName("original_language")
-    val originalLanguage: String? = null,
-
-    @SerialName("genre_ids")
-    val genreIds: List<Int> = emptyList(),
-
-    @SerialName("popularity")
-    val popularity: Double? = null,
-
-    @SerialName("video")
-    val video: Boolean? = null,
-
-    @SerialName("vote_average")
-    val voteAverage: Double? = null,
-
-    @SerialName("vote_count")
-    val voteCount: Int? = null,
-
-    @SerialName("origin_country")
-    val originCountry: List<String> = emptyList(),
-
-    @SerialName("adult")
-    val adult: Boolean? = null
+    @SerialName("id") val id: Long? = null,
+    @SerialName("title") val title: String? = null,
+    @SerialName("name") val name: String? = null,
+    @SerialName("media_type") val mediaType: String? = null,
+    @SerialName("overview") val overview: String? = null,
+    @SerialName("poster_path") val posterPath: String? = null,
+    @SerialName("backdrop_path") val backdropPath: String? = null,
+    @SerialName("release_date") val releaseDate: String? = null,
+    @SerialName("first_air_date") val firstAirDate: String? = null,
+    @SerialName("original_title") val originalTitle: String? = null,
+    @SerialName("original_name") val originalName: String? = null,
+    @SerialName("original_language") val originalLanguage: String? = null,
+    @SerialName("genre_ids") val genreIds: List<Int> = emptyList(),
+    @SerialName("popularity") val popularity: Double? = null,
+    @SerialName("video") val video: Boolean? = null,
+    @SerialName("vote_average") val voteAverage: Double? = null,
+    @SerialName("vote_count") val voteCount: Int? = null,
+    @SerialName("origin_country") val originCountry: List<String> = emptyList(),
+    @SerialName("adult") val adult: Boolean? = null
 )
