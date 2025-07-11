355e24684a4dd50250b346fb02f3929110ccea69
omer1998 <omerfaris11@gmail.com>
2025-07-11 23:05:56 +0300

    feat: create domain exception and implement search repository

--- repository/src/main/java/com/baghdad/repository/SearchRepositoryImpl.kt
+++ repository/src/main/java/com/baghdad/repository/SearchRepositoryImpl.kt
@@ -3,17 +3,26 @@ package com.baghdad.repository
 import com.baghdad.domain.model.search.SearchResult
 import com.baghdad.domain.repository.SearchRepository
 import com.baghdad.entity.search.RecentSearch
+import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
+import com.baghdad.repository.datasource.remote.RemoteSearchDataSource
+import com.baghdad.repository.mapper.toEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
-class SearchRepositoryImpl : SearchRepository   {
+class SearchRepositoryImpl(
+    val searchRemoteDataSource: RemoteSearchDataSource,
+    val remoteSearchDataSource: RemoteGenreDataSource
+) : SearchRepository {
     override suspend fun searchByName(query: String): SearchResult {
-        // TODO("Not yet implemented")
-        return SearchResult(
-            actors = emptyList(),
-            movies = emptyList(),
-            tvShows = emptyList()
-        )
+        val movieGenres = remoteSearchDataSource.getMovieGenre(language = "en")
+        val tvShowsGenres = remoteSearchDataSource.getTvShowGenre(language = "en")
+        return searchRemoteDataSource.searchMultiMedia(
+            query = query,
+            pageNumber = 1,
+            movieGenres,
+            tvShowsGenres
+        ).toEntity()
+
     }
 
     override suspend fun getRecentSearches(): Flow<List<RecentSearch>> {
@@ -22,10 +31,10 @@ class SearchRepositoryImpl : SearchRepository   {
     }
 
     override suspend fun deleteRecentSearchById(id: Long) {
-       // TODO("Not yet implemented")
+        // TODO("Not yet implemented")
     }
 
     override suspend fun deleteAllRecentSearches() {
-       // TODO("Not yet implemented")
+        // TODO("Not yet implemented")
     }
 }
