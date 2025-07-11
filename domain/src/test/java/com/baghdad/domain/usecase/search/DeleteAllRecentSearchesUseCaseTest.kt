b606779cfa3c628de2399b6a1a2653c83e1abbd5
edrees <muhammed.edrees101@gmail.com>
2025-07-11 17:55:19 +0300

    feat: implement DeleteAllRecentSearchesUseCase and its tests

--- domain/src/test/java/com/baghdad/domain/usecase/search/DeleteAllRecentSearchesUseCaseTest.kt
+++ domain/src/test/java/com/baghdad/domain/usecase/search/DeleteAllRecentSearchesUseCaseTest.kt
@@ -1,16 +1,40 @@
 package com.baghdad.domain.usecase.search
 
+import com.baghdad.domain.repository.SearchRepository
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class DeleteAllRecentSearchesUseCaseTest {
-    private val deleteAllRecentSearchesUseCase = DeleteAllRecentSearchesUseCase()
+
+    private lateinit var searchRepository: SearchRepository
+    private lateinit var deleteAllRecentSearchesUseCase: DeleteAllRecentSearchesUseCase
+
+    @BeforeEach
+    fun setUp() {
+        searchRepository = mockk(relaxed = true)
+        deleteAllRecentSearchesUseCase = DeleteAllRecentSearchesUseCase(searchRepository)
+    }
 
     @Test
-    fun dummyTest() = runTest {
-        deleteAllRecentSearchesUseCase().let {
-            Assertions.assertTrue(true)
-        }
+    fun `should delete all recent searches successfully`() = runTest {
+        deleteAllRecentSearchesUseCase()
+
+        coVerify(exactly = 1) { searchRepository.deleteAllRecentSearches() }
+    }
+
+    @Test
+    fun `should propagate exception when repository throws exception`() = runTest {
+        val exception = RuntimeException()
+        coEvery { searchRepository.deleteAllRecentSearches() } throws exception
+
+        val resultException = runCatching { deleteAllRecentSearchesUseCase() }.exceptionOrNull()
+
+        assertThat(resultException).isNotNull()
+        assertThat(resultException).isInstanceOf(RuntimeException::class.java)
     }
 }
