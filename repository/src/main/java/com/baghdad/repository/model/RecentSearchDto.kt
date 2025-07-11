dfecbaa0badf21c2a6a7aab40bf780e3a8565028
omer1998 <omerfaris11@gmail.com>
2025-07-12 00:18:27 +0300

    feat: add toEntity function to RecentSearchDto

--- repository/src/main/java/com/baghdad/repository/model/RecentSearchDto.kt
+++ repository/src/main/java/com/baghdad/repository/model/RecentSearchDto.kt
@@ -1,7 +1,24 @@
 package com.baghdad.repository.model
 
+import com.baghdad.entity.search.RecentSearch
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
 data class RecentSearchDto(
     val id: Long,
     val query: String,
     val searchedAt: Long = System.currentTimeMillis()
 )
+
+
+@OptIn(ExperimentalTime::class)
+fun RecentSearchDto.toEntity(): RecentSearch {
+    val instant = Instant.fromEpochMilliseconds(this.searchedAt)
+    return RecentSearch(
+        id = this.id,
+        query = this.query,
+        searchedAt = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+    )
+}
