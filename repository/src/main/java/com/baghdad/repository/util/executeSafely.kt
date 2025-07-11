94cfa3d5c0a7abdefed46c36ef3480300c6da076
omer1998 <omerfaris11@gmail.com>
2025-07-11 23:57:06 +0300

    feat: add getFlowSafely function to handle exceptions in Flows

--- repository/src/main/java/com/baghdad/repository/util/executeSafely.kt
+++ repository/src/main/java/com/baghdad/repository/util/executeSafely.kt
@@ -4,8 +4,23 @@ import com.baghdad.domain.exception.LocalDataBaseException
 import com.baghdad.domain.exception.RequestTimeoutException
 import com.baghdad.repository.exception.DatabaseException
 import com.baghdad.repository.exception.NetworkException
+import kotlinx.coroutines.flow.Flow
 
 suspend fun <T> executeSafely(block: suspend () -> T): T {
+    return try {
+        block()
+    } catch (e: Exception) {
+        when (e) {
+            is NetworkException -> throw RequestTimeoutException()
+            is DatabaseException -> throw LocalDataBaseException()
+            else -> throw Exception()
+
+        }
+
+    }
+}
+
+fun <T> getFlowSafely(block: () -> Flow<T>): Flow<T> {
     return try {
         block()
     } catch (e: Exception) {
