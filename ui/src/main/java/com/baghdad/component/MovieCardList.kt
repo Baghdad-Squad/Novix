e1ab9643c9c24d198847fe612b2f532bc1208c59
Farah315 <fara734isgreat@gmail.com>
2025-07-12 00:34:04 +0300

    refactor: add parameters

--- ui/src/main/java/com/baghdad/component/MovieCardList.kt
+++ ui/src/main/java/com/baghdad/component/MovieCardList.kt
@@ -3,9 +3,6 @@ package com.baghdad.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -13,28 +10,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.baghdad.design_system.component.HomeCard
-import com.baghdad.ui.search_screen.fake.data.getFakeMovies
+import com.baghdad.viewmodel.search.SearchScreenState
 
 @Composable
-fun MovieCardList() {
-    val movies = getFakeMovies()
-
+fun MovieCardList(
+    movies: List<SearchScreenState.MovieUiState> ,
+    onSavedClick:(Long) -> Unit,
+    onMovieClick:(Long) -> Unit,
+    modifier: Modifier=Modifier
+) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 150.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(900.dp)
-            .padding(8.dp),
+        modifier = modifier
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
 
     ) {
         items(movies) { movie ->
             HomeCard(
-                url = movie.posterUrl,
-                contentDescription = movie.title,
+                url = movie.posterPictureURL,
+                contentDescription = null,
                 isSaved = movie.isSaved,
-                onSavedClick = {  },
+                onSavedClick = { onSavedClick(movie.id) },
+                onClick= { onMovieClick(movie.id) },
                 modifier = Modifier.aspectRatio(0.8f)
             )
         }
--- ui/src/main/java/com/baghdad/component/TvShowCardList.kt
+++ ui/src/main/java/com/baghdad/component/TvShowCardList.kt
@@ -3,9 +3,6 @@ package com.baghdad.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -13,28 +10,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.baghdad.design_system.component.HomeCard
-import com.baghdad.ui.search_screen.fake.data.getFakeTvShows
+import com.baghdad.viewmodel.search.SearchScreenState
 
 @Composable
-fun TvShowCardList() {
-    val tvShows = getFakeTvShows()
+fun TvShowCardList(
+    tvShows: List<SearchScreenState.TvShowUiState>,
+    onSavedClick:(Long) -> Unit,
+    onTVShowClick:(Long) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
 
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 150.dp),
-        modifier = Modifier
-            .fillMaxSize()
-           .aspectRatio(0.9f)
-            .padding(8.dp),
+        modifier = modifier
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
 
     ) {
         items(tvShows) { tvShow ->
             HomeCard(
-                url = tvShow.posterUrl,
-                contentDescription = tvShow.title,
+                url = tvShow.posterPictureURL,
+                contentDescription = null,
                 isSaved = tvShow.isSaved,
-                onSavedClick = { },
+                onSavedClick = { onSavedClick(tvShow.id) },
+                onClick = { onTVShowClick(tvShow.id) },
                 modifier = Modifier.aspectRatio(0.8f)
             )
         }
