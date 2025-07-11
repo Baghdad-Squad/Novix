866121b7e34486ad7cf7f954118f8f26df84955f
Malak Raef <malak.raef20@gmail.com>
2025-07-12 02:07:30 +0300

    refactor: create ui package inside ui module to carry all ui components and features

--- ui/src/main/java/com/baghdad/ui/feature/search/component/RecentSearchItem.kt
+++ ui/src/main/java/com/baghdad/ui/feature/search/component/RecentSearchItem.kt
@@ -1,4 +1,4 @@
-package com.baghdad.feature.search.component
+package com.baghdad.ui.feature.search.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
