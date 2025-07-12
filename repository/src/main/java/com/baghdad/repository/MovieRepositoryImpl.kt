2ae34ded46ac6d6376e03fb0e56b039700b42a56
omer1998 <omerfaris11@gmail.com>
2025-07-12 05:06:59 +0300

    feat: use device language for genre requests

--- repository/src/main/java/com/baghdad/repository/MovieRepositoryImpl.kt
+++ repository/src/main/java/com/baghdad/repository/MovieRepositoryImpl.kt
@@ -5,13 +5,14 @@ import com.baghdad.entity.media.Genre
 import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
 import com.baghdad.repository.model.toEntity
 import com.baghdad.repository.util.executeSafely
+import java.util.Locale
 
 class MovieRepositoryImpl(
     val remoteGenreDataSource: RemoteGenreDataSource
 ) : MovieRepository {
     override suspend fun getGenres(): List<Genre> {
         return executeSafely {
-            remoteGenreDataSource.getMovieGenre(language = "en").map {
+            remoteGenreDataSource.getMovieGenre(language = Locale.getDefault().language).map {
                 it.toEntity()
             }
         }
--- repository/src/main/java/com/baghdad/repository/TvShowRepositoryImpl.kt
+++ repository/src/main/java/com/baghdad/repository/TvShowRepositoryImpl.kt
@@ -5,13 +5,14 @@ import com.baghdad.entity.media.Genre
 import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
 import com.baghdad.repository.model.toEntity
 import com.baghdad.repository.util.executeSafely
+import java.util.Locale
 
 class TvShowRepositoryImpl(
     val remoteGenreDataSource: RemoteGenreDataSource
 ) : TvShowRepository {
     override suspend fun getGenres(): List<Genre> {
         return executeSafely {
-            remoteGenreDataSource.getTvShowGenre(language = "en")
+            remoteGenreDataSource.getTvShowGenre(language = Locale.getDefault().language)
         }.map {
             it.toEntity()
         }
