eea53c7f0a325dab6dbb4d6bc12a95fe5d6ba516
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-07-12 03:34:54 +0200

    refactor: remove duplicated dependancy

--- design_system/build.gradle.kts
+++ design_system/build.gradle.kts
@@ -42,5 +42,4 @@ dependencies {
     implementation(libs.bundles.material)
     implementation(libs.androidx.ui.graphics.android)
     implementation(libs.androidx.ui.text.android)
-    implementation(libs.androidx.material3.v140alpha17)
 }
