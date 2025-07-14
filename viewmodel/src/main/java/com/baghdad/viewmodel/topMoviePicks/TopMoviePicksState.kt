deb2f95e00cb538536818319c13e8216d3f69d97
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-15 02:37:17 +0300

    feat: add TopMoviePicksState

---
+++ viewmodel/src/main/java/com/baghdad/viewmodel/topMoviePicks/TopMoviePicksState.kt
@@ -0,0 +1,17 @@
+package com.baghdad.viewmodel.topMoviePicks
+
+import com.baghdad.viewmodel.base.BaseUiState
+
+data class TopMoviePicksState(
+    val movies: List<MovieUiState> = emptyList(),
+    override val isLoading: Boolean,
+) : BaseUiState{
+    data class MovieUiState(
+        val id: Long = 0,
+        val posterPictureURL: String = "",
+        val isSaved: Boolean = false
+    )
+}
+
+
+
