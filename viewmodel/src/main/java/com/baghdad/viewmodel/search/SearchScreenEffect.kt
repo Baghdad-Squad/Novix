e57545dcc1a32afae61967a9f890d5157b503a34
Fares <dev.faresmohamed@gmail.com>
2025-07-14 18:13:10 +0300

    refactor: integrate base snackbar system in search screen

--- viewmodel/src/main/java/com/baghdad/viewmodel/search/SearchScreenEffect.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/search/SearchScreenEffect.kt
@@ -4,4 +4,8 @@ import com.baghdad.viewmodel.base.BaseUiEffect
 
 
 sealed class SearchScreenEffect : BaseUiEffect {
+    data class NavigateToMovieDetails(val movieId: Long) : SearchScreenEffect()
+    data class NavigateToTvShowDetails(val tvShowId: Long) : SearchScreenEffect()
+    data class NavigateToActorDetails(val actorId: Long) : SearchScreenEffect()
+    data class NavigateToRecentlyViewedDetails(val mediaId: Long) : SearchScreenEffect()
 }
