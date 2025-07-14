1a38d05ad1c784e38d497f3b134ed6c3a2c363d9
Arzo-zi <zinahashraf88@gmail.com>
2025-07-14 16:36:21 +0300

    refactor: remove unused converters

--- local_datasource/src/main/java/com/baghdad/local_datasource/roomDB/converter/Converters.kt
+++ local_datasource/src/main/java/com/baghdad/local_datasource/roomDB/converter/Converters.kt
@@ -8,18 +8,8 @@ import java.time.LocalDate
 class Converters {
 
     @TypeConverter
-    fun fromLocalDate(date: LocalDate?): String? {
-        return date?.toString() // Stores as ISO-8601 string (e.g., "2023-12-31")
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    @TypeConverter
-    fun toLocalDate(dateString: String?): LocalDate? {
-        return dateString?.let { LocalDate.parse(it) }
-
-    }
-    @TypeConverter
     fun fromString(value: String): List<String> = value.split(",")
+
     @TypeConverter
     fun listToString(list: List<String>): String = list.joinToString(",")
 }
