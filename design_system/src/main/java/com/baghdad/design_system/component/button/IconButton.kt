548920524b0f863802ef457d595fd87e2bf06bca
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-14 22:57:08 +0300

    feat: enhance IconButton with customizable background, border, shape, and size

--- design_system/src/main/java/com/baghdad/design_system/component/button/IconButton.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/button/IconButton.kt
@@ -1,9 +1,9 @@
 package com.baghdad.design_system.component.button
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,30 +11,39 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.baghdad.design_system.R
+import com.baghdad.design_system.modifier.noRippleClickable
 import com.baghdad.design_system.theme.Theme
 
 @Composable
 fun IconButton(
     icon: Painter,
     modifier: Modifier = Modifier,
+    background: Color = Theme.color.backgroundLow,
     tintIcon: Color = Theme.color.title,
+    borderStroke: BorderStroke? = BorderStroke(width = 1.dp, Theme.color.stroke),
+    shape : Shape = RoundedCornerShape(12.dp),
+    size: Dp = 40.dp,
     onClick: () -> Unit = {},
 ) {
     Box(
         modifier = modifier
-            .size(40.dp)
-            .background(color = Theme.color.backgroundLow)
-            .border(
-                width = 1.dp,
-                color = Theme.color.stroke,
-                shape = RoundedCornerShape(12.dp)
+            .clip(shape)
+            .size(size)
+            .then(
+                if (borderStroke == null) Modifier else Modifier.border(borderStroke, shape = shape)
             )
-            .clickable {
+            .background(color = background)
+            .noRippleClickable {
                 onClick.invoke()
             }
     ) {
@@ -47,4 +56,17 @@ fun IconButton(
                 .align(Alignment.Center)
         )
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun IconButtonPreview() {
+    IconButton(
+        icon = painterResource(R.drawable.ic_star),
+        onClick = { /* Do something */ },
+        modifier = Modifier,
+        background = Theme.color.primary,
+        tintIcon = Theme.color.surface,
+        borderStroke = BorderStroke(1.dp, Theme.color.stroke),
+    )
 }
