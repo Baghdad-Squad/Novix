663f494d843a47946b3ecf1ff55189407bcd2b69
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-16 00:42:14 +0300

    refactor: refactor viewModelModule

--- app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
@@ -3,11 +3,16 @@ package com.baghdad.novix.di
 import com.baghdad.viewmodel.actorDetails.ActorDetailsViewModel
 import com.baghdad.viewmodel.search.SearchViewModel
 import com.baghdad.viewmodel.topMoviePicks.TopMoviePicksViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
-    viewModelOf(::ActorDetailsViewModel)
-    viewModelOf(::TopMoviePicksViewModel)
+    viewModel { (actorId: Long) ->
+        ActorDetailsViewModel(actorId, get(), get(), get(), get())
+    }
+    viewModel{ (actorId: Long) ->
+        TopMoviePicksViewModel(actorId, get())
+    }
 }
