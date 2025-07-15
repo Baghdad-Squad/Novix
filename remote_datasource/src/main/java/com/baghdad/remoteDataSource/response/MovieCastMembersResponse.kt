26b7ead93560e68790763461b0b63da62e493660
Aboud <gh.abd.com.207@gmail.com>
2025-07-15 11:41:29 +0300

    refactor: remove unused fields from CastMemberResponse and MovieResult

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/MovieCastMembersResponse.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/MovieCastMembersResponse.kt
@@ -13,28 +13,15 @@ data class CreditsResponse(
 
 @Serializable
 data class CastMemberResponse(
-    @SerialName("adult")
-    val adult: Boolean? = null,
-    @SerialName("gender")
-    val gender: Int? = null,
     @SerialName("id")
     val id: Int? = null,
-    @SerialName("known_for_department")
-    val knownForDepartment: String? = null,
     @SerialName("name")
     val name: String? = null,
-    @SerialName("original_name")
-    val originalName: String? = null,
-    @SerialName("popularity")
-    val popularity: Double? = null,
     @SerialName("profile_path")
     val profilePath: String? = null,
-    @SerialName("cast_id")
-    val castId: Int? = null,
+    @SerialName("known_for_department")
+    val knownForDepartment: String? = null,
     @SerialName("character")
-    val character: String? = null,
-    @SerialName("credit_id")
-    val creditId: String? = null,
-    @SerialName("order")
-    val order: Int? = null
+    val character: String? = null
 )
+
