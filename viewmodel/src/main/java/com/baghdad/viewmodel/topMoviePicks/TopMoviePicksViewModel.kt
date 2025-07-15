0f78f6b86dc72ff8009f4eabaca7ce5841c89c7e
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-16 00:43:00 +0300

    feat: implement TopMoviePicksViewModel

--- viewmodel/src/main/java/com/baghdad/viewmodel/topMoviePicks/TopMoviePicksViewModel.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/topMoviePicks/TopMoviePicksViewModel.kt
@@ -1,19 +1,45 @@
 package com.baghdad.viewmodel.topMoviePicks
 
+import com.baghdad.domain.usecase.actorDetails.GetActorMoviesUseCase
+import com.baghdad.entity.media.Movie
 import com.baghdad.viewmodel.base.BaseViewModel
 import com.baghdad.viewmodel.errorStates.BaseSnackBarMessage
 
 class TopMoviePicksViewModel(
-
+    val  actorId: Long,
+    private val getActorMoviesUseCase: GetActorMoviesUseCase,
 ): BaseViewModel<TopMoviePicksState, TopMoviePicksEffect>
-    (TopMoviePicksState(isLoading = false)), TopMoviePicksInteractionListener {
+    (TopMoviePicksState(
+        isLoading = false
+    )), TopMoviePicksInteractionListener {
+
+     init {
+         getActorMovies(actorId)
+     }
+
+    private fun getActorMovies(actorId: Long) {
+        tryToExecute(
+            callee = { getActorMoviesUseCase(actorId) },
+            onSuccess = ::onGetActorMoviesSuccess,
+            onStart = ::onLoading,
+            onFinally = ::onFinally
+        )
+    }
+
+    private fun onGetActorMoviesSuccess(movies: List<Movie>) {
+        updateState { topMoviePicksState ->
+            topMoviePicksState.copy(
+                movies = movies.map { it.toUIState() }
+            )
+        }
+    }
 
     override fun mapThrowableToErrorMessage(throwable: Throwable): BaseSnackBarMessage {
-        TODO("Not yet implemented")
+        return BaseSnackBarMessage.UnknownError
     }
 
     override fun onMovieDetailsClicked(movieId: Long) {
-        TODO("Not yet implemented")
+        sendEffect(TopMoviePicksEffect.NavigateToMovieDetails(movieId))
     }
 
     override fun onSaveMovieClicked(movieId: Long) {
@@ -21,6 +47,12 @@ class TopMoviePicksViewModel(
     }
 
     override fun onBackClicked() {
-        TODO("Not yet implemented")
+        sendEffect(TopMoviePicksEffect.NavigateBack)
+    }
+    private fun onLoading() {
+        updateState { it.copy(isLoading = true) }
+    }
+    private fun onFinally() {
+        updateState { it.copy(isLoading = false) }
     }
 }
