7c5cdb4606c49ed168ce072d59d64d43ec0c1888
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-16 01:31:13 +0300

    refactor: add scaffold to the screen Top Movie Picks screen

--- ui/src/main/java/com/baghdad/ui/feature/topMoviePicks/TopMoviePicks.kt
+++ ui/src/main/java/com/baghdad/ui/feature/topMoviePicks/TopMoviePicks.kt
@@ -1,17 +1,27 @@
 package com.baghdad.ui.feature.topMoviePicks
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.baghdad.design_system.component.HomeCard
+import com.baghdad.design_system.component.HotScaffold
+import com.baghdad.design_system.component.Text
+import com.baghdad.design_system.component.button.IconButton
+import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.base.ObserveAsEffect
 import com.baghdad.ui.navigation.graph.actorDetails.ActorDetailsNavEvent
 import com.baghdad.ui.navigation.graph.actorDetails.ActorDetailsNavEvent.NavigateToMovieDetails
@@ -56,23 +66,55 @@ private fun TopMoviePicksContent(
     uiState: TopMoviePicksState,
     listener: TopMoviePicksInteractionListener,
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 150.dp),
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
 
+    HotScaffold(
+        topBar = {
+            Row (modifier = Modifier.padding(top = 52.dp)
+            , verticalAlignment = Alignment.CenterVertically) {
+                IconButton(
+                    icon = painterResource(com.baghdad.design_system.R.drawable.ic_go_back),
+                    onClick = { listener.onBackClicked() },
+                    modifier = Modifier
+                        .padding(start = 16.dp, bottom = 8.dp)
+                )
+                Text(
+                    text = "Top Picks",
+                    style = Theme.typography.title.large,
+                    color = Theme.color.title,
+                    modifier = Modifier
+                        .padding(start = 8.dp, bottom = 8.dp)
+                )
+            }
+        },
+        modifier = Modifier,
+        floatingActionButton = null,
+        snackbar = null,
+        bottomBar = {
+        }
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 150.dp),
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding =PaddingValues(
+                start = 16.dp,
+                end = 16.dp,
+                top = 8.dp,
+                bottom = 8.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-        items(uiState.movies) { movie ->
-            HomeCard(
-                url = movie.posterPictureURL,
-                contentDescription = null,
-                isSaved = movie.isSaved,
-                onSavedClick = { listener.onSaveMovieClicked(movie.id) },
-                onClick = { listener.onMovieDetailsClicked(movie.id) },
-                modifier = Modifier.aspectRatio(0.8f)
-            )
+            items(uiState.movies) { movie ->
+                HomeCard(
+                    url = movie.posterPictureURL,
+                    contentDescription = null,
+                    isSaved = movie.isSaved,
+                    onSavedClick = { listener.onSaveMovieClicked(movie.id) },
+                    onClick = { listener.onMovieDetailsClicked(movie.id) },
+                    modifier = Modifier.aspectRatio(0.8f)
+                )
+            }
         }
     }
 }
