f6121deb55a255f4596b835ddb751e7b0f0cd2a8
Aboud <gh.abd.com.207@gmail.com>
2025-07-15 06:35:05 +0300

    feat: implement RemoteMovieDataSource

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteMovieDataSourceImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteMovieDataSourceImpl.kt
@@ -1,4 +1,45 @@
 package com.baghdad.remoteDataSource
 
-class RemoteMovieDataSourceImpl {
+import MovieDetailsResponse
+import com.baghdad.remoteDataSource.mapper.movie.toDto
+import com.baghdad.remoteDataSource.response.CreditsResponse
+import com.baghdad.remoteDataSource.response.SimilarMovieResponse
+import com.baghdad.remoteDataSource.util.handleRequest
+import com.baghdad.repository.model.CastMemberDto
+import com.baghdad.repository.model.MovieDto
+import io.ktor.client.HttpClient
+
+class RemoteMovieDataSourceImpl(
+    private val httpClient: HttpClient,
+    private val baseUrl: String
+) {
+    suspend fun getSimilarMovies(movieId: Int): List<MovieDto> {
+        val endpoint = SIMILAR_MOVIES_ENDPOINT.replace("{movie_id}", movieId.toString())
+        return handleRequest<SimilarMovieResponse>(
+            client = httpClient,
+            url = "$baseUrl$endpoint"
+        ).results?.map { it.toDto() } ?: emptyList()
+    }
+
+    suspend fun getMovieDetails(movieId: Int): MovieDto {
+        val endpoint = MOVIE_DETAILS_ENDPOINT.replace("{movie_id}", movieId.toString())
+        return handleRequest<MovieDetailsResponse>(
+            client = httpClient,
+            url = "$baseUrl$endpoint"
+        ).toDto()
+    }
+
+    suspend fun getMovieCredits(movieId: Int): List<CastMemberDto> {
+        val endpoint = MOVIE_CREDITS_ENDPOINT.replace("{movie_id}", movieId.toString())
+        return handleRequest<CreditsResponse>(
+            client = httpClient,
+            url = "$baseUrl$endpoint"
+        ).cast?.map { it.toDto() } ?: emptyList()
+    }
+
+    companion object {
+        private const val SIMILAR_MOVIES_ENDPOINT = "/movie/{movie_id}/similar"
+        private const val MOVIE_DETAILS_ENDPOINT = "/movie/{movie_id}"
+        private const val MOVIE_CREDITS_ENDPOINT = "/movie/{movie_id}/credits"
+    }
 }
