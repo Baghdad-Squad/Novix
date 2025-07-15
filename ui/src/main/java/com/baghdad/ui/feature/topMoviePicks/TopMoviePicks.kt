11be9522531b9f9a1a118ba15ef73d4505bff52d
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-16 00:46:03 +0300

    feat: implement TopMoviePicksScreen and event handling

--- ui/src/main/java/com/baghdad/ui/feature/topMoviePicks/TopMoviePicks.kt
+++ ui/src/main/java/com/baghdad/ui/feature/topMoviePicks/TopMoviePicks.kt
@@ -7,12 +7,49 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.baghdad.design_system.component.HomeCard
+import com.baghdad.ui.base.ObserveAsEffect
+import com.baghdad.ui.navigation.graph.actorDetails.ActorDetailsNavEvent
+import com.baghdad.ui.navigation.graph.actorDetails.ActorDetailsNavEvent.NavigateToMovieDetails
+import com.baghdad.viewmodel.topMoviePicks.TopMoviePicksEffect
 import com.baghdad.viewmodel.topMoviePicks.TopMoviePicksInteractionListener
 import com.baghdad.viewmodel.topMoviePicks.TopMoviePicksState
+import com.baghdad.viewmodel.topMoviePicks.TopMoviePicksViewModel
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
 
+@Composable
+fun TopMoviePicksScreen(
+    actorId: Long,
+    viewModel: TopMoviePicksViewModel = koinViewModel(parameters = { parametersOf(actorId) }),
+    handleNavigation: (ActorDetailsNavEvent) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    ObserveAsEffect(viewModel.uiEffect) { effect ->
+        handleEffect(effect, handleNavigation)
+    }
+    TopMoviePicksContent(
+        uiState = uiState,
+        listener = viewModel
+    )
+}
+private fun handleEffect(
+    effect: TopMoviePicksEffect,
+    handleNavigation: (ActorDetailsNavEvent) -> Unit
+) {
+    when (effect) {
+        is TopMoviePicksEffect.NavigateBack -> handleNavigation(
+            ActorDetailsNavEvent.NavigateBack
+        )
+        is TopMoviePicksEffect.NavigateToMovieDetails -> handleNavigation(
+            NavigateToMovieDetails(effect.movieId)
+        )
+    }
+}
 
 @Composable
 private fun TopMoviePicksContent(
