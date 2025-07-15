56acfc7ae63f2be01fa5d174d50ade8db895dbdd
edrees <muhammed.edrees101@gmail.com>
2025-07-15 16:10:02 +0300

    feat: implement home navigation events and graph

---
+++ ui/src/main/java/com/baghdad/ui/navigation/graph/movieDetails/MovieDetailsNavGraph.kt
@@ -0,0 +1,49 @@
+package com.baghdad.ui.navigation.graph.movieDetails
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.baghdad.ui.navigation.graph.DummyScreen
+import com.baghdad.ui.navigation.graph.util.toGraph
+import com.baghdad.ui.navigation.route.CategoriesRoute
+import com.baghdad.ui.navigation.route.Graph
+import com.baghdad.ui.navigation.route.MovieDetailsRoute
+
+fun NavGraphBuilder.movieDetailsNavGraph(navController: NavHostController) {
+    navigation<Graph.MovieDetailsGraph>(
+        startDestination = MovieDetailsRoute.MovieDetailsScreen
+    ) {
+        composable<MovieDetailsRoute.MovieDetailsScreen> { backStackEntry ->
+            val movieId = backStackEntry.toGraph<Graph.MovieDetailsGraph>(navController).movieId
+            DummyScreen("Movie Details Screen: $movieId")
+        }
+    }
+}
+
+private fun handleMovieDetailsNavigation(
+    event: MovieDetailsNavEvent,
+    navController: NavHostController
+) {
+    when (event) {
+        MovieDetailsNavEvent.NavigateBack -> navController.popBackStack()
+
+        MovieDetailsNavEvent.NavigateToLogin -> navController.navigate(Graph.AuthenticationGraph)
+
+        is MovieDetailsNavEvent.NavigateToCategoryMovies -> navController.navigate(
+            CategoriesRoute.CategoryMoviesScreen(event.categoryId)
+        )
+
+        is MovieDetailsNavEvent.NavigateToActorDetails -> navController.navigate(
+            Graph.ActorDetailsGraph(event.actorId)
+        )
+
+        is MovieDetailsNavEvent.NavigateToMovieDetails -> navController.navigate(
+            Graph.MovieDetailsGraph(event.movieId)
+        )
+
+        is MovieDetailsNavEvent.NavigateToReviews -> navController.navigate(
+            Graph.ReviewsGraph(event.movieId, "movie")
+        )
+    }
+}
