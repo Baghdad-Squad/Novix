4dcf7e475e91999bcf74a919a8e0e90272fe53d5
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-15 23:38:54 +0300

    feat: add TopMoviePicksViewModel to DI

--- app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
@@ -2,13 +2,12 @@ package com.baghdad.novix.di
 
 import com.baghdad.viewmodel.actorDetails.ActorDetailsViewModel
 import com.baghdad.viewmodel.search.SearchViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import com.baghdad.viewmodel.topMoviePicks.TopMoviePicksViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
-    viewModel { (actorId: Long) ->
-        ActorDetailsViewModel(actorId, get(), get(), get(), get())
-    }
+    viewModelOf(::ActorDetailsViewModel)
+    viewModelOf(::TopMoviePicksViewModel)
 }
