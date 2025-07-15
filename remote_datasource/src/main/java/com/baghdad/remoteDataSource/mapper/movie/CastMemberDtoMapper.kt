bec1de365b581e52ea30e5e18a8e334e2cc89ffa
Aboud <gh.abd.com.207@gmail.com>
2025-07-15 05:54:43 +0300

    feat: add mapper for CastMemberDto

---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/mapper/movie/CastMemberDtoMapper.kt
@@ -0,0 +1,21 @@
+package com.baghdad.remoteDataSource.mapper.movie
+
+import com.baghdad.repository.model.CastMemberDto
+import com.baghdad.repository.model.actor.ActorDto
+
+fun com.baghdad.remoteDataSource.response.CastMemberDto.toDto(): CastMemberDto {
+    return CastMemberDto(
+        actor = ActorDto(
+            id = (id ?: 0).toLong(),
+            name = name ?: "Unknown Actor",
+            imageUrl = profilePath?.let { "https://image.tmdb.org/t/p/w500$it" } ?: "",
+            biography = "",
+            birthdayDate = "",
+            deathDate = null,
+            placeOfBirth = "",
+            headerPictures = emptyList(),
+            department = knownForDepartment ?: "Unknown"
+        ),
+        characterName = character ?: "Unknown"
+    )
+}
