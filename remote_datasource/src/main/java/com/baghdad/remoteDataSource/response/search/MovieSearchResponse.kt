088872e4186f00cd220d2aa1a697f93146656815
edrees <muhammed.edrees101@gmail.com>
2025-07-16 16:06:57 +0300

    feat: add MovieSearchResponse and mapping functions for paginated movie data

---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/search/MovieSearchResponse.kt
@@ -0,0 +1,41 @@
+package com.baghdad.remoteDataSource.response.search
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieSearchResponse(
+    val page: Int? = null,
+    val results: List<Result?>? = null,
+    @SerialName("total_pages")
+    val totalPages: Int? = null,
+    @SerialName("total_results")
+    val totalResults: Int? = null
+) {
+    @Serializable
+    data class Result(
+        val adult: Boolean? = null,
+        @SerialName("backdrop_path")
+        val backdropPath: String? = null,
+        @SerialName("genre_ids")
+        val genreIds: List<Int?>? = null,
+        val id: Int? = null,
+        @SerialName("original_language")
+        val originalLanguage: String? = null,
+        @SerialName("original_title")
+        val originalTitle: String? = null,
+        val overview: String? = null,
+        val popularity: Double? = null,
+        @SerialName("poster_path")
+        val posterPath: String? = null,
+        @SerialName("release_date")
+        val releaseDate: String? = null,
+        val title: String? = null,
+        val video: Boolean? = null,
+        @SerialName("vote_average")
+        val voteAverage: Double? = null,
+        @SerialName("vote_count")
+        val voteCount: Int? = null
+    )
+}
