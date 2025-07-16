3041b1d053bf6d5691e4d502dc7dc42cb97f7801
edrees <muhammed.edrees101@gmail.com>
2025-07-16 16:07:29 +0300

    feat: update RemoteSearchDataSource to support separate movie, TV show, and actor search functions

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteSearchDataSourceImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteSearchDataSourceImpl.kt
@@ -1,35 +1,71 @@
 package com.baghdad.remoteDataSource
 
-import com.baghdad.remoteDataSource.mapper.toDto
+import com.baghdad.remoteDataSource.mapper.search.toPagedActorDtos
+import com.baghdad.remoteDataSource.mapper.search.toPagedMovieDtos
+import com.baghdad.remoteDataSource.mapper.search.toPagedTvShowDtos
 import com.baghdad.remoteDataSource.request.SearchParameter
-import com.baghdad.remoteDataSource.response.SearchResponse
 import com.baghdad.remoteDataSource.request.toParams
+import com.baghdad.remoteDataSource.response.search.ActorSearchResponse
+import com.baghdad.remoteDataSource.response.search.MovieSearchResponse
+import com.baghdad.remoteDataSource.response.search.TvShowSearchResponse
 import com.baghdad.remoteDataSource.util.handleRequest
 import com.baghdad.repository.datasource.remote.RemoteSearchDataSource
 import com.baghdad.repository.model.GenreDto
-import com.baghdad.repository.model.SearchResultDto
+import com.baghdad.repository.model.MovieDto
+import com.baghdad.repository.model.PagedResultDto
+import com.baghdad.repository.model.TvShowDto
+import com.baghdad.repository.model.actor.ActorDto
 import io.ktor.client.HttpClient
 
 class RemoteSearchDataSourceImpl(
     private val httpClient: HttpClient,
     private val baseUrl: String
 ) : RemoteSearchDataSource {
-    override suspend fun searchMultiMedia(
-        query: String, pageNumber: Int,
-        movieGenres: List<GenreDto>?,
-        tvGenres: List<GenreDto>?
-    ): SearchResultDto {
+    override suspend fun searchMovies(
+        query: String,
+        pageNumber: Int,
+        genres: List<GenreDto>
+    ): PagedResultDto<MovieDto> {
         val params = SearchParameter(query, pageNumber)
-        val searchResponse = handleRequest<SearchResponse>(
+        val searchResponse = handleRequest<MovieSearchResponse>(
             client = httpClient,
-            url = "$baseUrl$SEARCH_MULTI_ENDPOINT",
+            url = "$baseUrl$SEARCH_MOVIES_ENDPOINT",
             params = params.toParams(),
         )
 
-        return searchResponse.toDto(movieGenres, tvGenres)
+        return searchResponse.toPagedMovieDtos(genres = genres)
+    }
+
+    override suspend fun searchTvShows(
+        query: String,
+        pageNumber: Int,
+        genres: List<GenreDto>
+    ): PagedResultDto<TvShowDto> {
+        val params = SearchParameter(query, pageNumber)
+        val searchResponse = handleRequest<TvShowSearchResponse>(
+            client = httpClient,
+            url = "$baseUrl$SEARCH_TV_SHOWS_ENDPOINT",
+            params = params.toParams(),
+        )
+        return searchResponse.toPagedTvShowDtos(genres = genres)
+    }
+
+    override suspend fun searchActors(
+        query: String,
+        pageNumber: Int
+    ): PagedResultDto<ActorDto> {
+        val params = SearchParameter(query, pageNumber)
+        val searchResponse = handleRequest<ActorSearchResponse>(
+            client = httpClient,
+            url = "$baseUrl$SEARCH_ACTORS_ENDPOINT",
+            params = params.toParams(),
+        )
+        return searchResponse.toPagedActorDtos()
     }
 
     companion object {
-        private const val SEARCH_MULTI_ENDPOINT = "/search/multi"
+        private const val SEARCH_MOVIES_ENDPOINT = "/search/movie"
+        private const val SEARCH_TV_SHOWS_ENDPOINT = "/search/tv"
+        private const val SEARCH_ACTORS_ENDPOINT = "/search/person"
     }
 }
--- repository/src/main/java/com/baghdad/repository/datasource/remote/RemoteSearchDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/remote/RemoteSearchDataSource.kt
@@ -1,13 +1,26 @@
 package com.baghdad.repository.datasource.remote
 
 import com.baghdad.repository.model.GenreDto
-import com.baghdad.repository.model.SearchResultDto
+import com.baghdad.repository.model.MovieDto
+import com.baghdad.repository.model.PagedResultDto
+import com.baghdad.repository.model.TvShowDto
+import com.baghdad.repository.model.actor.ActorDto
 
 interface RemoteSearchDataSource {
-    suspend fun searchMultiMedia(
+    suspend fun searchMovies(
         query: String,
         pageNumber: Int,
-        movieGenres: List<GenreDto>? = null,
-        tvGenres: List<GenreDto>? = null
-    ): SearchResultDto
+        genres: List<GenreDto>
+    ): PagedResultDto<MovieDto>
+
+    suspend fun searchTvShows(
+        query: String,
+        pageNumber: Int,
+        genres: List<GenreDto>
+    ): PagedResultDto<TvShowDto>
+
+    suspend fun searchActors(
+        query: String,
+        pageNumber: Int
+    ): PagedResultDto<ActorDto>
 }
