f0513579d681f6360f5b65c3b8e7ebade14be510
Malak Raef <malak.raef20@gmail.com>
2025-07-16 23:55:49 +0300

    feat: setup tv show details mapper from entity to ui state

---
+++ viewmodel/src/main/java/com/baghdad/viewmodel/tvShowDetails/TvShowDetailsMapper.kt
@@ -0,0 +1,36 @@
+package com.baghdad.viewmodel.tvShowDetails
+
+import com.baghdad.entity.media.Episode
+import com.baghdad.entity.media.Genre
+import com.baghdad.entity.media.TvShow
+import com.baghdad.entity.person.Actor
+
+fun TvShow.toUiState() = TvShowDetailsScreenState.TvShowInfoUiState(
+    title = title,
+    genres = genres.map { it.toUiState() },
+    rating = averageRating,
+    releaseDate = releaseDate.toString(),
+    seasonCount = numberOfSeasons,
+    overView = overview,
+)
+
+fun Genre.toUiState() = TvShowDetailsScreenState.GenreUiState(
+    id = id,
+    name = name,
+)
+
+fun Actor.toUiState() = TvShowDetailsScreenState.CastMemberUiState(
+    id = id,
+    name = name,
+    imageUrl = profilePictureURL,
+    characterName = characterName ?: "",
+)
+
+fun Episode.toUiState() = TvShowDetailsScreenState.EpisodeUiState(
+    id = id,
+    name = title,
+    episodeNumber = episodeNumber,
+    rating = rating,
+    duration = duration,
+    releaseDate = releasedDate.toString(),
+)
