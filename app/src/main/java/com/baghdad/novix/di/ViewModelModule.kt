17d1a936bad7f0141ef2eb71a542679d72ed65d9
Malak Raef <malak.raef20@gmail.com>
2025-07-16 23:56:18 +0300

    feat: add tv show view model to di with required parameter

--- app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.baghdad.novix.di
 import com.baghdad.viewmodel.actorDetails.ActorDetailsViewModel
 import com.baghdad.viewmodel.actorGallery.ActorGalleryViewModel
 import com.baghdad.viewmodel.search.SearchViewModel
+import com.baghdad.viewmodel.tvShowDetails.TvShowDetailsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
@@ -14,6 +15,8 @@ val viewModelModule = module {
     }
     viewModel { (actorId: Long) ->
         ActorDetailsViewModel(actorId, get(), get(), get(), get())
-
+    }
+    viewModel { (tvShowId: Long) ->
+        TvShowDetailsViewModel(tvShowId, get(), get(), get(), get())
     }
 }
