c8f3e32a53998b7e80a1848e37b436fcc02bbefc
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-16 14:31:05 +0300

    refactor: update parameters and and add improve fab and bottom bar handling

--- design_system/src/main/java/com/baghdad/design_system/component/Scaffold.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/Scaffold.kt
@@ -3,7 +3,9 @@ package com.baghdad.design_system.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
@@ -13,42 +15,47 @@ import androidx.compose.ui.unit.dp
 import com.baghdad.design_system.theme.Theme
 
 @Composable
-fun HotScaffold(
-    topBar: @Composable () -> Unit,
+fun Scaffold(
     modifier: Modifier = Modifier,
+    topBar: (@Composable () -> Unit)? = null,
     floatingActionButton: (@Composable () -> Unit)? = null,
     snackbar: (@Composable () -> Unit)? = null,
-    bottomBar: @Composable () -> Unit = {  },
+    bottomBar: (@Composable () -> Unit)? = null,
+    bottomBarHeight: Int = 70,
     content: @Composable () -> Unit,
-
 ) {
     Box(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
             .background(Theme.color.surface),
         contentAlignment = Alignment.TopCenter
     ) {
         Column(
             modifier = Modifier.matchParentSize()
         ) {
-            topBar()
+            topBar?.invoke()
             content()
+            bottomBar?.let {
+                Spacer(Modifier.height(bottomBarHeight.dp))
+            }
         }
-        Box(
+
+        Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
         ) {
-            bottomBar()
-        }
-        floatingActionButton?.let { fab ->
-            Box(
-                modifier = Modifier
-                    .padding(end = 12.dp, bottom = 10.dp)
-                    .align(Alignment.BottomEnd),
-            ) {
-                fab()
+            floatingActionButton?.let { fab ->
+                Box(
+                    modifier = Modifier
+                        .padding(end = 12.dp, bottom = 10.dp)
+                        .align(Alignment.End),
+                ) {
+                    fab()
+                }
             }
+            bottomBar?.invoke()
         }
-        snackbar?.let {  snackbarContent ->
+        snackbar?.let { snackbarContent ->
             Box(
                 modifier = Modifier
                     .statusBarsPadding()
