b5c06e232d16c730f416613394f7dce8cd8be44c
Edrees <muhammed.edrees101@gmail.com>
2025-07-16 13:06:41 +0300

    fix: remove redundant viewmodel definitions in ViewModelModule.kt

--- app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
@@ -9,8 +9,6 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
-    viewModelOf(::ActorDetailsViewModel)
-    viewModelOf(::ActorDetailsViewModel)
     viewModel {
         (actorId: Long) -> ActorGalleryViewModel(get(), actorId)
     }
