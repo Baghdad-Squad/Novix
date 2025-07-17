258b151fbccefa135cc495bdca31d70e68e852b6
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-18 01:37:59 +0300

    refactor: add ReviewViewModel and ContentType import in ViewModelModule

--- app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/novix/di/ViewModelModule.kt
@@ -3,6 +3,8 @@ package com.baghdad.novix.di
 import com.baghdad.viewmodel.actorDetails.ActorDetailsViewModel
 import com.baghdad.viewmodel.actorGallery.ActorGalleryViewModel
 import com.baghdad.viewmodel.movieDetails.MovieDetailsViewModel
+import com.baghdad.viewmodel.review.ContentType
+import com.baghdad.viewmodel.review.ReviewViewModel
 import com.baghdad.viewmodel.search.SearchViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
