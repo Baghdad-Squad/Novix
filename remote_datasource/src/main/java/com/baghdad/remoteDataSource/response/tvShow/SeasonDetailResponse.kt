abeff0bff0056651076112597d1a15d8617979de
Aboud <gh.abd.com.207@gmail.com>
2025-07-17 17:15:01 +0300

    refactor: simplify SeasonDetailResponse and EpisodeResponse

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/tvShow/SeasonDetailResponse.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/tvShow/SeasonDetailResponse.kt
@@ -5,24 +5,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SeasonDetailResponse(
-    @SerialName("_id")
-    val id: String? = null,
-    @SerialName("air_date")
-    val airDate: String? = null,
     @SerialName("episodes")
     val episodes: List<EpisodeResponse>? = null,
-    @SerialName("name")
-    val name: String? = null,
-    @SerialName("overview")
-    val overview: String? = null,
-    @SerialName("id")
-    val seasonId: Int? = 0,
-    @SerialName("poster_path")
-    val posterPath: String? = null,
-    @SerialName("season_number")
-    val seasonNumber: Int? = 0,
-    @SerialName("vote_average")
-    val voteAverage: Double? = 0.0
 )
 
 @Serializable
@@ -37,46 +21,10 @@ data class EpisodeResponse(
     val name: String? = null,
     @SerialName("overview")
     val overview: String? = null,
-    @SerialName("production_code")
-    val productionCode: String? = null,
     @SerialName("runtime")
     val runtime: Int? = 0,
     @SerialName("season_number")
     val seasonNumber: Int? = 0,
-    @SerialName("show_id")
-    val showId: Int? = 0,
-    @SerialName("still_path")
-    val stillPath: String? = null,
     @SerialName("vote_average")
     val voteAverage: Double? = 0.0,
-    @SerialName("vote_count")
-    val voteCount: Int? = 0,
-    @SerialName("guest_stars")
-    val guestStars: List<GuestStar>? = null
-)
-
-@Serializable
-data class GuestStar(
-    @SerialName("character")
-    val character: String? = null,
-    @SerialName("credit_id")
-    val creditId: String? = null,
-    @SerialName("order")
-    val order: Int? = 0,
-    @SerialName("adult")
-    val adult: Boolean? = true,
-    @SerialName("gender")
-    val gender: Int? = 0,
-    @SerialName("id")
-    val id: Int? = 0,
-    @SerialName("known_for_department")
-    val knownForDepartment: String? = null,
-    @SerialName("name")
-    val name: String? = null,
-    @SerialName("original_name")
-    val originalName: String? = null,
-    @SerialName("popularity")
-    val popularity: Double? = 0.0,
-    @SerialName("profile_path")
-    val profilePath: String? = null
 )
