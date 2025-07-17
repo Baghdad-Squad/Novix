ec319d424ee8273e04af0c00417e66af1f3be1c4
Aboud <gh.abd.com.207@gmail.com>
2025-07-17 17:19:45 +0300

    refactor: simplify TvShowItemDto by removing unused fields

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/tvShow/TvShowResponse.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/response/tvShow/TvShowResponse.kt
@@ -17,24 +17,12 @@ data class TvShowResponse(
 
 @Serializable
 data class TvShowItemDto(
-    @SerialName("adult")
-    val adult: Boolean? = null,
-    @SerialName("backdrop_path")
-    val backdropPath: String? = null,
     @SerialName("genre_ids")
     val genreIds: List<Int>? = null,
     @SerialName("id")
     val id: Int? = null,
-    @SerialName("origin_country")
-    val originCountry: List<String>? = null,
-    @SerialName("original_language")
-    val originalLanguage: String? = null,
-    @SerialName("original_name")
-    val originalName: String? = null,
     @SerialName("overview")
     val overview: String? = null,
-    @SerialName("popularity")
-    val popularity: Double? = null,
     @SerialName("poster_path")
     val posterPath: String? = null,
     @SerialName("first_air_date")
@@ -43,6 +31,4 @@ data class TvShowItemDto(
     val name: String? = null,
     @SerialName("vote_average")
     val voteAverage: Double? = null,
-    @SerialName("vote_count")
-    val voteCount: Int? = null
 )
