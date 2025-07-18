bdf291f55ea4898c411a8da2eac0b400bf78da28
Malak Raef <malak.raef20@gmail.com>
2025-07-18 20:28:13 +0300

    feat: add overview and imageUrl variables for episode entities

--- entity/src/main/java/com/baghdad/entity/media/Episode.kt
+++ entity/src/main/java/com/baghdad/entity/media/Episode.kt
@@ -9,5 +9,7 @@ data class Episode(
     val rating: Double,
     val duration: String,
     val releasedDate: LocalDate,
-    val currentSeason: Int
+    val currentSeason: Int,
+    val imageUrl: String,
+    val overview: String
 )
