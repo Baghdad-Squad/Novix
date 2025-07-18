7207bd06a91729fb29156a43fe147bd3803cb56d
Malak Raef <malak.raef20@gmail.com>
2025-07-18 20:23:10 +0300

    feat: setup tv show details screen navigation

--- viewmodel/src/main/java/com/baghdad/viewmodel/tvShowDetails/TvShowDetailsScreenEffect.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/tvShowDetails/TvShowDetailsScreenEffect.kt
@@ -6,7 +6,8 @@ sealed class TvShowDetailsScreenEffect : BaseUiEffect {
     data object NavigateBack : TvShowDetailsScreenEffect()
     data object NavigateToLogin : TvShowDetailsScreenEffect()
     data class NavigateToActorDetails(val actorId: Long) : TvShowDetailsScreenEffect()
-    data class NavigateToEpisodeDetails(val episodeId: Long) : TvShowDetailsScreenEffect()
+    data class NavigateToEpisodeDetails(val seasonNumber: Int, val episodeNumber: Int) :
+        TvShowDetailsScreenEffect()
     data class NavigateToGenreScreen(val genreId: Long) : TvShowDetailsScreenEffect()
     data class NavigateToReviews(val tvShowId: Long) : TvShowDetailsScreenEffect()
 
