ca8172d4f138e43c0045fe48d8a0ecc9b37f4b2a
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-07-18 03:09:22 +0200

    refactor: add view model unit test to view model module

--- viewmodel/src/test/java/com/baghdad/viewmodel/ActorGalleryViewModelTest.kt
+++ viewmodel/src/test/java/com/baghdad/viewmodel/ActorGalleryViewModelTest.kt
@@ -1,6 +1,8 @@
-package com.baghdad.viewmodel.actorGallery
+package com.baghdad.viewmodel
 
 import com.baghdad.domain.usecase.actorDetails.GetActorGalleryUseCase
+import com.baghdad.viewmodel.actorGallery.ActorGalleryScreenEffect
+import com.baghdad.viewmodel.actorGallery.ActorGalleryViewModel
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
