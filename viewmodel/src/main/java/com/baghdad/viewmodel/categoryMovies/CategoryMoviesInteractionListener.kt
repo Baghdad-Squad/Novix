986f39ea44d5c048a16dbaec10baaa90c2af325e
omer1998 <omerfaris11@gmail.com>
2025-07-18 16:49:14 +0300

    feat: add save and movie click interactions to category movies

--- viewmodel/src/main/java/com/baghdad/viewmodel/categoryMovies/CategoryMoviesInteractionListener.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/categoryMovies/CategoryMoviesInteractionListener.kt
@@ -2,4 +2,6 @@ package com.baghdad.viewmodel.categoryMovies
 
 interface CategoryMoviesInteractionListener {
     fun onBackClicked()
+    fun onSavedClick(movieId: Long)
+    fun onMovieClicked(movieId: Long)
 }
