aa25e05a29ace681f40918bc602bcf8855d96642
Malak Raef <malak.raef20@gmail.com>
2025-07-18 20:33:30 +0300

    refactor: use fill max width when displaying actor card in search results

--- ui/src/main/java/com/baghdad/ui/feature/search/component/ActorCardList.kt
+++ ui/src/main/java/com/baghdad/ui/feature/search/component/ActorCardList.kt
@@ -3,6 +3,7 @@ package com.baghdad.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -31,6 +32,7 @@ fun ActorCardList(
                 actorName = actor.name,
                 actorImage = actor.profilePictureURL,
                 onClick = { onActorClick(actor.id) },
+                modifier = Modifier.fillMaxWidth()
             )
         }
     }
