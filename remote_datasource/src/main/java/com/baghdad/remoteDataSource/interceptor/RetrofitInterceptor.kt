cebf04cc6aacf02732fab3df3a0f9cb0fcdd23b7
Fares <dev.faresmohamed@gmail.com>
2025-07-20 23:42:56 +0300

    feat: add HeadersSetupInterceptor for request header management

---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/interceptor/RetrofitInterceptor.kt
@@ -0,0 +1,38 @@
+package com.baghdad.remoteDataSource.interceptor
+
+
+import com.baghdad.repository.language.LanguageProvider
+import okhttp3.Interceptor
+import okhttp3.Response
+import retrofit2.Invocation
+
+class HeadersSetupInterceptor(
+    private val languageProvider: LanguageProvider,
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val invocation = chain.request().tag(Invocation::class.java)
+            ?: return chain.proceed(chain.request())
+        val shouldAttachAuthHeader = invocation
+            .method()
+            .annotations
+            .any { it.annotationClass == Authenticated::class }
+
+        return chain.proceed(
+            chain.request()
+                .newBuilder().apply {
+                    if (shouldAttachAuthHeader) {
+                        // Uncomment the following lines if you have a user local data source to get the token
+//                        if (userLocalDataSource.getUserToken().isNotEmpty()) {
+//                            addHeader("Authorization", userLocalDataSource.getHeaderToken())
+//                        }
+                    }
+                    addHeader("Accept", "application/json")
+                    addHeader("language", languageProvider.getCurrentLanguage())
+                }
+                .build())
+    }
+}
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class Authenticated
