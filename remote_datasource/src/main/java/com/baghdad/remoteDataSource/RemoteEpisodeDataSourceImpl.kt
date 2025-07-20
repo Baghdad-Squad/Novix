5bcd744578d8b5206018e06619cd3d3e1ba1c889
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-20 23:07:35 +0300

    feat: add EpisodeApiService for Retrofit-based episode data retrieval

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteEpisodeDataSourceImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteEpisodeDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.baghdad.remoteDataSource
 
+import com.baghdad.remoteDataSource.apiServece.EpisodeApiService
 import com.baghdad.remoteDataSource.mapper.actor.toDto
 import com.baghdad.remoteDataSource.mapper.episode.mapToYoutubeTrailerUrl
 import com.baghdad.remoteDataSource.mapper.episode.toDto
@@ -12,48 +13,44 @@ import com.baghdad.repository.datasource.remote.RemoteEpisodeDataSource
 import com.baghdad.repository.logger.Logger
 import com.baghdad.repository.model.CastMemberDto
 import com.baghdad.repository.model.EpisodeDto
-import io.ktor.client.HttpClient
 
 class RemoteEpisodeDataSourceImpl(
-    private val httpClient: HttpClient,
+    private val episodeApiService: EpisodeApiService,
     private val logger: Logger,
-    private val baseUrl: String
 ): RemoteEpisodeDataSource {
 
     override suspend fun getEpisodeDetails(tvId: Long, seasonNumber: Int, episodeNumber: Int): EpisodeDto {
-        val endpoint = EPISODES_DETAILS_ENDPOINT
-            .replace("{tv_id}", tvId.toString())
-            .replace("{season_number}", seasonNumber.toString())
-            .replace("{episode_number}", episodeNumber.toString())
         return handleRequest<EpisodeDetailsResponse>(
-            client = httpClient,
+            apiCall = {episodeApiService.getEpisodeDetails(
+                tvId = tvId,
+                seasonNumber = seasonNumber,
+                episodeNumber = episodeNumber
+            )},
             logger = logger,
-            url = "$baseUrl$endpoint"
+
         ).toDto()
     }
 
     override suspend fun getEpisodeCastMembers(tvId: Long, seasonNumber: Int, episodeNumber: Int): List<CastMemberDto> {
-        val endpoint = EPISODE_CREDITS_ENDPOINT
-            .replace("{tv_id}", tvId.toString())
-            .replace("{season_number}", seasonNumber.toString())
-            .replace("{episode_number}", episodeNumber.toString())
         return handleRequest<CastMembersResponse>(
-            client = httpClient,
+            apiCall = { episodeApiService.getEpisodeCastMembers(
+                tvId = tvId,
+                seasonNumber = seasonNumber,
+                episodeNumber = episodeNumber
+            ) },
             logger = logger,
-            url = "$baseUrl$endpoint"
         ).cast?.map { it.toDto() } ?: emptyList()
 
     }
 
     override suspend fun getEpisodeImages(tvId: Long, seasonNumber: Int, episodeNumber: Int): List<String> {
-        val endpoint = EPISODE_IMAGES_ENDPOINT
-            .replace("{tv_id}", tvId.toString())
-            .replace("{season_number}", seasonNumber.toString())
-            .replace("{episode_number}", episodeNumber.toString())
         return handleRequest<EpisodeImageResponse>(
-            client = httpClient,
+            apiCall = {episodeApiService.getEpisodeImages(
+                tvId = tvId,
+                seasonNumber = seasonNumber,
+                episodeNumber = episodeNumber
+            )},
             logger = logger,
-            url = "$baseUrl$endpoint"
         ).stills.orEmpty().map { "https://image.tmdb.org/t/p/w500" + it.filePath }
     }
 
@@ -62,23 +59,14 @@ class RemoteEpisodeDataSourceImpl(
         seasonNumber: Int,
         episodeNumber: Int
     ): String {
-        val endpoint = EPISODE_VIDEOS_ENDPOINT
-            .replace("{tv_id}", tvId.toString())
-            .replace("{season_number}", seasonNumber.toString())
-            .replace("{episode_number}", episodeNumber.toString())
         return handleRequest<EpisodeVideosResponse>(
-            client = httpClient,
+            apiCall = {episodeApiService.getEpisodeTrailer(
+                tvId = tvId,
+                seasonNumber = seasonNumber,
+                episodeNumber = episodeNumber
+            )},
             logger = logger,
-            url = "$baseUrl$endpoint"
         ).mapToYoutubeTrailerUrl()
     }
 
-    companion object{
-        private const val EPISODES_DETAILS_ENDPOINT = "/tv/{tv_id}/season/{season_number}/episode/{episode_number}"
-        private const val EPISODE_CREDITS_ENDPOINT = "/tv/{tv_id}/season/{season_number}/episode/{episode_number}/credits"
-        private const val EPISODE_IMAGES_ENDPOINT = "/tv/{tv_id}/season/{season_number}/episode/{episode_number}/images"
-        private const val EPISODE_VIDEOS_ENDPOINT =
-            "/tv/{tv_id}/season/{season_number}/episode/{episode_number}/videos"
-
-    }
 }
---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiServece/EpisodeApiService.kt
@@ -0,0 +1,50 @@
+package com.baghdad.remoteDataSource.apiServece
+
+import com.baghdad.remoteDataSource.response.CastMembersResponse
+import com.baghdad.remoteDataSource.response.episode.EpisodeDetailsResponse
+import com.baghdad.remoteDataSource.response.episode.EpisodeImageResponse
+import com.baghdad.remoteDataSource.response.episode.EpisodeVideosResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface EpisodeApiService {
+
+    @GET(EPISODES_DETAILS_ENDPOINT)
+    suspend fun getEpisodeDetails(
+        @Path("tv_id") tvId: Long,
+        @Path ("season_number") seasonNumber: Int,
+        @Path ("episode_number") episodeNumber: Int
+    ): Response<EpisodeDetailsResponse>
+
+    @GET(EPISODE_CREDITS_ENDPOINT)
+    suspend fun getEpisodeCastMembers(
+        @Path("tv_id") tvId: Long,
+        @Path("season_number") seasonNumber: Int,
+        @Path ("episode_number") episodeNumber: Int
+    ): Response<CastMembersResponse>
+
+    @GET(EPISODE_IMAGES_ENDPOINT)
+    suspend fun getEpisodeImages(
+        @Path("tv_id") tvId: Long,
+        @Path("season_number") seasonNumber: Int,
+        @Path ("episode_number") episodeNumber: Int
+    ): Response<EpisodeImageResponse>
+
+    @GET(EPISODE_VIDEOS_ENDPOINT)
+    suspend fun getEpisodeTrailer(
+        @Path("tv_id") tvId: Long,
+        @Path("season_number") seasonNumber: Int,
+        @Path ("episode_number") episodeNumber: Int
+    ): Response<EpisodeVideosResponse>
+
+
+    companion object{
+        private const val EPISODES_DETAILS_ENDPOINT = "/tv/{tv_id}/season/{season_number}/episode/{episode_number}"
+        private const val EPISODE_CREDITS_ENDPOINT = "/tv/{tv_id}/season/{season_number}/episode/{episode_number}/credits"
+        private const val EPISODE_IMAGES_ENDPOINT = "/tv/{tv_id}/season/{season_number}/episode/{episode_number}/images"
+        private const val EPISODE_VIDEOS_ENDPOINT =
+            "/tv/{tv_id}/season/{season_number}/episode/{episode_number}/videos"
+
+    }
+}
