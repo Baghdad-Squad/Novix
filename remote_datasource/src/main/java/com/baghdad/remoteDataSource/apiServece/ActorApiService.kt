fbd79553f18846b6c0fab474327ab27d4571232c
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-20 23:07:16 +0300

    feat: implement ActorApiService for Retrofit-based remote data source

---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiServece/ActorApiService.kt
@@ -0,0 +1,39 @@
+package com.baghdad.remoteDataSource.apiServece
+
+import com.baghdad.remoteDataSource.response.actor.ActorDetailsResponse
+import com.baghdad.remoteDataSource.response.actor.ActorImagesResponse
+import com.baghdad.remoteDataSource.response.actor.ActorMoviesResponse
+import com.baghdad.remoteDataSource.response.actor.ActorTvShowsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ActorApiService {
+
+    @GET(PERSON_DETAILS_ENDPOINT)
+    suspend fun getActorDetails(
+        @Path("person_id") personId: Long
+    ): Response<ActorDetailsResponse>
+
+    @GET(PERSON_IMAGES_ENDPOINT)
+    suspend fun getActorImages(
+        @Path("person_id") personId: Long
+    ): Response<ActorImagesResponse>
+
+    @GET(PERSON_MOVIES_PICK_ENDPOINT)
+    suspend fun getActorMovies(
+        @Path("person_id") personId: Long
+    ): Response<ActorMoviesResponse>
+
+    @GET(PERSON_TV_SHOWS_PICK_ENDPOINT)
+    suspend fun getActorTvShows(
+        @Path("person_id") personId: Long,
+    ): Response<ActorTvShowsResponse>
+
+    companion object {
+        private const val PERSON_MOVIES_PICK_ENDPOINT = "/person/{person_id}/movie_credits"
+        private const val PERSON_TV_SHOWS_PICK_ENDPOINT = "/person/{person_id}/tv_credits"
+        private const val PERSON_IMAGES_ENDPOINT = "/person/{person_id}/images"
+        private const val PERSON_DETAILS_ENDPOINT = "/person/{person_id}"
+    }
+}
