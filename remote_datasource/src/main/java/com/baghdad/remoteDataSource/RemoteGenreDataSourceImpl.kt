ed60b15d4a735b3dd887051f370222a1223b7fc6
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-20 23:07:50 +0300

    feat: add GenreApiService for Retrofit-based genre data retrieval

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteGenreDataSourceImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteGenreDataSourceImpl.kt
@@ -1,35 +1,28 @@
 package com.baghdad.remoteDataSource
 
+import com.baghdad.remoteDataSource.apiServece.GenreApiService
 import com.baghdad.remoteDataSource.mapper.toDto
 import com.baghdad.remoteDataSource.response.GenreListResponse
 import com.baghdad.remoteDataSource.util.handleRequest
 import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
 import com.baghdad.repository.logger.Logger
 import com.baghdad.repository.model.GenreDto
-import io.ktor.client.HttpClient
 
 class RemoteGenreDataSourceImpl(
-    private val httpClient: HttpClient,
+    private val genreApiService: GenreApiService,
     private val logger: Logger,
-    private val baseUrl: String
 ): RemoteGenreDataSource {
     override suspend fun getMovieGenre(language: String): List<GenreDto> {
         return handleRequest<GenreListResponse>(
-            client = httpClient,
+            apiCall = {genreApiService.getMovieGenre(language)},
             logger = logger,
-            url = "$baseUrl$MOVIE_GENRE_ENDPOINT",
         ).toDto(genreType = GenreDto.GenreType.MOVIE)
     }
 
     override suspend fun getTvShowGenre(language: String): List<GenreDto> {
         return handleRequest<GenreListResponse>(
-            client = httpClient,
+            apiCall = {genreApiService.getTvShowGenre(language)},
             logger = logger,
-            url = "$baseUrl$TV_SHOW_GENRE_ENDPOINT",
         ).toDto(genreType = GenreDto.GenreType.TV_SHOW)
-    }
-    companion object {
-        private const val MOVIE_GENRE_ENDPOINT = "/genre/movie/list"
-        private const val TV_SHOW_GENRE_ENDPOINT = "/genre/tv/list"
     }
 }
---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiServece/GenreApiService.kt
@@ -0,0 +1,21 @@
+package com.baghdad.remoteDataSource.apiServece
+
+import com.baghdad.remoteDataSource.response.GenreListResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface GenreApiService {
+
+    @GET(MOVIE_GENRE_ENDPOINT)
+    suspend fun getMovieGenre(language: String): Response<GenreListResponse>
+
+
+  @GET(TV_SHOW_GENRE_ENDPOINT)
+    suspend fun getTvShowGenre(language: String): Response<GenreListResponse>
+
+
+    companion object {
+        private const val MOVIE_GENRE_ENDPOINT = "/genre/movie/list"
+        private const val TV_SHOW_GENRE_ENDPOINT = "/genre/tv/list"
+    }
+}
