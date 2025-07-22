ff3ff273ed9a1579d15c275e1c0f82a972488c36
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-07-22 23:30:35 +0200

    refactor: resolve conflict

--- design_system/src/main/java/com/baghdad/design_system/component/ActorCard.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/ActorCard.kt
@@ -3,11 +3,11 @@ package com.baghdad.design_system.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
@@ -19,9 +19,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import com.baghdad.design_system.R
 import com.baghdad.design_system.modifier.noRippleClickable
 import com.baghdad.design_system.modifier.threeSidedBorder
 import com.baghdad.design_system.theme.NovixTheme
