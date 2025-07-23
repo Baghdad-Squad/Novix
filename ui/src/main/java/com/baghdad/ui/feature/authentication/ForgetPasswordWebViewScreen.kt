6f595bfea4a0b506a1d876c04712a27067c1ed50
omer1998 <omerfaris11@gmail.com>
2025-07-23 03:21:59 +0300

    refactor: re structure screens and handle navigation after user complete his actions in these screens

--- ui/src/main/java/com/baghdad/ui/feature/authentication/ForgetPasswordWebViewScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/authentication/ForgetPasswordWebViewScreen.kt
@@ -1,19 +1,54 @@
 package com.baghdad.ui.feature.authentication
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.baghdad.design_system.component.Scaffold
+import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.feature.component.AppWebView
+import com.baghdad.ui.navigation.graph.authentication.AuthenticationNavEvent
+import kotlinx.coroutines.delay
 
 
 @Composable
 fun ForgotPasswordWebViewScreen(
-    onUrlChange: ((String) -> Unit)? = null
+    modifier: Modifier = Modifier,
+    handleNavigation: (AuthenticationNavEvent) -> Unit,
 ) {
-    AppWebView(
-        url = "https://www.themoviedb.org/reset-password",
-        modifier = Modifier.fillMaxSize(),
-        allowedDomains = listOf("themoviedb.org"),
-        onUrlChange = onUrlChange
-    )
+    var shouldNavigate by remember { mutableStateOf(false) }
+    LaunchedEffect(shouldNavigate) {
+        if (shouldNavigate) {
+            delay(5000)
+            handleNavigation(AuthenticationNavEvent.NavigateBack)
+        }
+    }
+    val screenUrl = remember { "https://www.themoviedb.org/reset-password" }
+
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Theme.color.surface)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+    ) {
+
+        AppWebView(
+            url = screenUrl,
+            modifier = Modifier.fillMaxSize(),
+            allowedDomains = listOf("themoviedb.org"),
+            onUrlChange = {
+                if (it.startsWith("https://www.themoviedb.org/login")) {
+                    shouldNavigate = true
+                }
+            }
+        )
+    }
 }
--- ui/src/main/java/com/baghdad/ui/feature/authentication/SignUpWebViewScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/authentication/SignUpWebViewScreen.kt
@@ -1,18 +1,52 @@
 package com.baghdad.ui.feature.authentication
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.baghdad.design_system.component.Scaffold
+import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.feature.component.AppWebView
+import com.baghdad.ui.navigation.graph.authentication.AuthenticationNavEvent
+import kotlinx.coroutines.delay
 
 @Composable
 fun SignUpWebViewScreen(
-    onUrlChange: ((String) -> Unit)? = null
+    handleNavigation: (AuthenticationNavEvent) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    AppWebView(
-        url = "https://www.themoviedb.org/signup",
-        modifier = Modifier.fillMaxSize(),
-        allowedDomains = listOf("themoviedb.org"),
-        onUrlChange = onUrlChange
-    )
+    val screenUrl = remember { "https://www.themoviedb.org/signup" }
+    var shouldNavigateBack by remember { mutableStateOf(false) }
+    LaunchedEffect(shouldNavigateBack) {
+        if (shouldNavigateBack) {
+            delay(5000)
+            handleNavigation(AuthenticationNavEvent.NavigateBack)
+        }
+    }
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Theme.color.surface)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+    ) {
+
+        AppWebView(
+            url = screenUrl,
+            modifier = Modifier.fillMaxSize(),
+            allowedDomains = listOf("themoviedb.org"),
+            onUrlChange = {
+                if (it.startsWith("https://www.themoviedb.org/login")) {
+                    shouldNavigateBack = true
+                }
+            }
+        )
+    }
 }
