dfa79b3361be5a9ea77869b94996b7fb5e31e1b9
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-07-24 01:59:16 +0200

    refactor: edit main activity to use home graph

--- app/src/main/java/com/baghdad/novix/MainActivity.kt
+++ app/src/main/java/com/baghdad/novix/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.baghdad.design_system.theme.NovixTheme
 import com.baghdad.ui.feature.trendingActors.TrendingActorsScreen
+import com.baghdad.ui.navigation.NovixNavHost
 import com.baghdad.ui.navigation.graph.home.HomeNavEvent
 import com.baghdad.ui.navigation.route.Graph
 import com.baghdad.viewmodel.people.TrendingActorViewModel
@@ -18,38 +19,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-          val viewModel: TrendingActorViewModel = koinViewModel()
             NovixTheme {
                 val navController = rememberNavController()
-//                NovixNavHost(
-//                    navController = navController,
-//                    startDestination = Graph.HomeGraph
-//                )
-               TrendingActorsScreen(
-                   viewModel,
-                   handleNavigation = {event ->
-                   when(event){
-                       is HomeNavEvent.NavigateBack -> {
-                           navController.popBackStack()
-                       }
-                       is HomeNavEvent.NavigateToActorDetails -> {
-                           navController.navigate(
-                               route = "${Graph.ActorDetailsGraph}/${event.actorId}"
-                           )
-                       }
+                NovixNavHost(
+                    navController = navController,
+                    startDestination = Graph.HomeGraph
+                )
 
-                       HomeNavEvent.NavigateToActors -> TODO()
-                       HomeNavEvent.NavigateToContinueWatching -> TODO()
-                       HomeNavEvent.NavigateToLogin -> TODO()
-                       is HomeNavEvent.NavigateToMovieDetails -> TODO()
-                       HomeNavEvent.NavigateToMovies -> TODO()
-                       HomeNavEvent.NavigateToPopularMovies -> TODO()
-                       HomeNavEvent.NavigateToTopRatingMovies -> TODO()
-                       is HomeNavEvent.NavigateToTvShowDetails -> TODO()
-                       HomeNavEvent.NavigateToTvShows -> TODO()
-                   }
-               })
-                //TODO
             }
         }
     }
