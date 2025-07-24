4959c41594816111ac87d44ed91d3993665b51bc
Malak Raef <malak.raef20@gmail.com>
2025-07-24 15:29:57 +0300

    feat: implement get trending tv show function in the repo

--- repository/src/main/java/com/baghdad/repository/TvShowRepositoryImpl.kt
+++ repository/src/main/java/com/baghdad/repository/TvShowRepositoryImpl.kt
@@ -1,20 +1,27 @@
 package com.baghdad.repository
 
+import com.baghdad.domain.model.PagedResult
 import com.baghdad.domain.repository.TvShowRepository
 import com.baghdad.entity.media.Episode
 import com.baghdad.entity.media.Genre
 import com.baghdad.entity.media.Review
 import com.baghdad.entity.media.TvShow
 import com.baghdad.entity.person.CastMember
+import com.baghdad.repository.datasource.local.LocalGenreDataSource
+import com.baghdad.repository.datasource.local.LocalTrendingTvShowsDataSource
 import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
 import com.baghdad.repository.datasource.remote.RemoteTvShowDataSource
 import com.baghdad.repository.mapper.toEntity
+import com.baghdad.repository.model.TvShowDto
 import com.baghdad.repository.util.executeSafely
+import com.baghdad.repository.util.getPagedSafely
 import java.util.Locale
 
 class TvShowRepositoryImpl(
     val remoteGenreDataSource: RemoteGenreDataSource,
-    val tvShowRemoteDataSource: RemoteTvShowDataSource
+    val tvShowRemoteDataSource: RemoteTvShowDataSource,
+    val localGenreDataSource: LocalGenreDataSource,
+    val localTrendingTvShowsDataSource: LocalTrendingTvShowsDataSource
 ) : TvShowRepository {
     override suspend fun getGenres(): List<Genre> {
         return executeSafely {
@@ -69,6 +76,32 @@ class TvShowRepositoryImpl(
                 it.toEntity()
             }
         }
+    }
+
+    override suspend fun getTrendingTvShows(page: Int): PagedResult<TvShow> {
+        return getPagedSafely(
+            page = page,
+            pageSize = 20,
+            getCachedPage = { page, pageSize ->
+                localTrendingTvShowsDataSource.getTrendingTvShows(page, pageSize)
+            },
+            getRemoteData = { page, _ ->
+                updateGenreCache()
+                tvShowRemoteDataSource.getTrendingTvShows(page)
+            },
+            cacheData = { data ->
+                localTrendingTvShowsDataSource.saveTrendingTvShows(data)
+            },
+            mapToEntity = TvShowDto::toEntity
+        )
+    }
+
+    private suspend fun updateGenreCache() {
+        val lang = Locale.getDefault().language
+
+        val tvGenres = remoteGenreDataSource.getTvShowGenre(lang)
+
+        tvGenres.forEach { localGenreDataSource.addGenre(it) }
     }
 
     companion object {
