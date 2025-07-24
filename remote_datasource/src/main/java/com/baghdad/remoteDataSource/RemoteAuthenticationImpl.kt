add2a3a3125474f1951aefeffee6f9059d74e746
Farah315 <fara734isgreat@gmail.com>
2025-07-24 03:22:20 +0300

    feat: add getUserDetails and deleteSession RemoteAuthenticationImpl

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteAuthenticationImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteAuthenticationImpl.kt
@@ -1,11 +1,14 @@
 package com.baghdad.remoteDataSource
 
 import com.baghdad.remoteDataSource.api.AuthenticationApi
+import com.baghdad.remoteDataSource.mapper.user.toDto
 import com.baghdad.remoteDataSource.request.CredentialDataBody
 import com.baghdad.remoteDataSource.request.RequestTokenBody
 import com.baghdad.remoteDataSource.util.safeRetrofitCall
+import com.baghdad.remote_datasource.R
 import com.baghdad.repository.datasource.remote.RemoteAuthenticationDataSource
 import com.baghdad.repository.logger.Logger
+import com.baghdad.repository.model.UserDto
 
 class RemoteAuthenticationImpl(
     private val api: AuthenticationApi,
@@ -49,4 +52,27 @@ class RemoteAuthenticationImpl(
             logger = logger
         )
     }
+
+    override suspend fun getUserDetails(sessionId: String): UserDto {
+        return safeRetrofitCall(
+            apiCall = {
+                api.getUserDetails(
+                    sessionId = sessionId
+                )
+            },
+            logger = logger
+        ).toDto()
+    }
+
+    override suspend fun deleteSession(sessionId: String): Boolean {
+        return safeRetrofitCall(
+            apiCall = {
+                api.deleteSession(
+                    sessionId = sessionId
+                )
+            },
+            logger = logger
+        ).success
+    }
+
 }
