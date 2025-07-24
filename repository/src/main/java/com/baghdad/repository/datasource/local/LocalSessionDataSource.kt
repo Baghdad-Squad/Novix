e46ef72681b21fa3450616eae56fce7f1871390b
Farah315 <fara734isgreat@gmail.com>
2025-07-24 03:17:55 +0300

    feat: add getUserDetails and clearSession interface

--- repository/src/main/java/com/baghdad/repository/datasource/local/LocalSessionDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/LocalSessionDataSource.kt
@@ -1,7 +1,12 @@
 package com.baghdad.repository.datasource.local
 
+import com.baghdad.repository.model.UserDto
+
 interface LocalSessionDataSource {
     suspend fun saveSessionId(sessionId: String)
+
     suspend fun getSessionId(): String?
+    suspend fun getUserDetails():UserDto?
+    suspend fun clearSession()
     suspend fun deleteSessionId()
 }
