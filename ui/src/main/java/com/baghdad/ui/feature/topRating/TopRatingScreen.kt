a738055ca7bc7bb06c56c21a8bd448a6385c03fb
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-25 09:39:30 +0300

    feat: add tv show tab to top rating screen

--- ui/src/main/java/com/baghdad/ui/feature/topRating/TopRatingScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/topRating/TopRatingScreen.kt
@@ -24,9 +24,11 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.baghdad.design_system.R
 import com.baghdad.design_system.component.Scaffold
 import com.baghdad.design_system.component.SnackBar
+import com.baghdad.design_system.component.Tab
 import com.baghdad.design_system.component.Text
 import com.baghdad.design_system.component.WavyLoadingIndicator
 import com.baghdad.design_system.component.button.IconButton
+import com.baghdad.design_system.component.button.PrimaryButton
 import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.base.ObserveAsEffect
 import com.baghdad.ui.base.toStringResource
@@ -40,7 +42,8 @@ import com.baghdad.viewmodel.base.SnackBarState
 import com.baghdad.viewmodel.errorStates.BaseSnackBarMessage
 import com.baghdad.viewmodel.topRating.TopRatingEffect
 import com.baghdad.viewmodel.topRating.TopRatingInteractionListener
-import com.baghdad.viewmodel.topRating.TopRatingMovieState
+import com.baghdad.viewmodel.topRating.TopRatingState
+import com.baghdad.viewmodel.topRating.TopRatingTab
 import com.baghdad.viewmodel.topRating.TopRatingViewModel
 import org.koin.androidx.compose.koinViewModel
 
@@ -48,7 +51,7 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun TopRatingMoviesScreen(
     viewModel: TopRatingViewModel = koinViewModel(),
-    handleNavigation: (HomeNavEvent) -> Unit
+    handleNavigation: (HomeNavEvent) -> Unit = {  }
 ) {
     ObserveAsEffect(viewModel.uiEffect) { effect ->
         handleEffect(effect, handleNavigation)
@@ -56,12 +59,14 @@ fun TopRatingMoviesScreen(
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val snackBarState by viewModel.snackBarState.collectAsStateWithLifecycle()
     val movieItems = uiState.moviesFlow.collectAsLazyPagingItems()
+    val tvShowItems = uiState.tvShowsFlow.collectAsLazyPagingItems()
 
     TopRatingMoviesContent(
         uiState = uiState,
         listener = viewModel,
+        snackBarState = snackBarState,
         movieItems = movieItems,
-        snackBarState = snackBarState
+        tvShowItems = tvShowItems
     )
 }
 
@@ -82,10 +87,11 @@ private fun handleEffect(
 
 @Composable
 fun TopRatingMoviesContent(
-    uiState: TopRatingMovieState,
+    uiState: TopRatingState,
     listener: TopRatingInteractionListener,
     snackBarState: SnackBarState,
-    movieItems: LazyPagingItems<TopRatingMovieState.MovieUiState>
+    movieItems: LazyPagingItems<TopRatingState.MovieUiState>,
+    tvShowItems: LazyPagingItems<TopRatingState.TvShowUiState>
 ) {
     Scaffold(
         topBar = {
@@ -112,6 +118,27 @@ fun TopRatingMoviesContent(
                         .padding(start = 8.dp, bottom = 8.dp)
                 )
             }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .background(Theme.color.surface)
+                    .padding( top = 4.dp, bottom = 12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                Tab(
+                    text = stringResource(com.baghdad.ui.R.string.movies),
+                    onClick = { listener.onSelectedTab(TopRatingTab.MOVIES) },
+                    isSelected = uiState.selectedTab == TopRatingTab.MOVIES,
+                    modifier = Modifier.weight(1f)
+                )
+                Tab(
+                    text = stringResource(com.baghdad.ui.R.string.tv_shows),
+                    onClick = { listener.onSelectedTab(TopRatingTab.TV_SHOWS) },
+                    isSelected = uiState.selectedTab == TopRatingTab.TV_SHOWS,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
             GenresSection(
                 allGenres = uiState.genres,
                 selectedGenres = uiState.selectedGenreId,
@@ -120,6 +147,8 @@ fun TopRatingMoviesContent(
                     .background(Theme.color.surface)
                     .padding(start = 16.dp, top = 12.dp, bottom = 12.dp)
             )
+
+
         },
         snackbar = {
             SnackBar(
@@ -135,35 +164,84 @@ fun TopRatingMoviesContent(
                 WavyLoadingIndicator(modifier = Modifier.align(Alignment.Center))
             }
         }
-        LazyPagingVerticalGrid<TopRatingMovieState.MovieUiState>(
-            columns = GridCells.Adaptive(minSize = 150.dp),
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Theme.color.surface),
-            contentPadding = PaddingValues(
-                start = 16.dp,
-                end = 16.dp,
-                top = 8.dp,
-                bottom = 8.dp
-            ),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            items = movieItems,
-        ) { movie ->
 
-            HomeCard(
-                url = movie.posterPictureURL,
-                contentDescription = null,
-                isSaved = movie.isSaved,
-                onSavedClick = { listener.onSaveMovieClick(movie.id) },
-                onClick = { listener.onMovieDetailsClick(movie.id) },
-                modifier = Modifier.aspectRatio(0.8f)
-            )
+        when (uiState.selectedTab) {
+            TopRatingTab.MOVIES -> {
+                LazyPagingVerticalGrid<TopRatingState.MovieUiState>(
+                    columns = GridCells.Adaptive(minSize = 150.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Theme.color.surface),
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 8.dp,
+                        bottom = 8.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    items = movieItems,
+                ) { movie ->
+                    Box(contentAlignment = Alignment.TopCenter) {
 
+                    }
+
+                    HomeCard(
+                        url = movie.posterPictureURL,
+                        contentDescription = null,
+                        isSaved = movie.isSaved,
+                        onSavedClick = { listener.onSaveMovieClick(movie.id) },
+                        onClick = { listener.onMovieDetailsClick(movie.id) },
+                        modifier = Modifier.aspectRatio(0.8f)
+                    )
+                }
+            }
+
+            TopRatingTab.TV_SHOWS -> {
+                LazyPagingVerticalGrid<TopRatingState.TvShowUiState>(
+                    columns = GridCells.Adaptive(minSize = 150.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Theme.color.surface),
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 8.dp,
+                        bottom = 8.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    items = tvShowItems,
+                ) { tvShow ->
+                    HomeCard(
+                        url = tvShow.posterPictureURL,
+                        contentDescription = null,
+                        isSaved = tvShow.isSaved,
+                        onSavedClick = { listener.onSaveMovieClick(tvShow.id) },
+                        onClick = { listener.onMovieDetailsClick(tvShow.id) },
+                        modifier = Modifier.aspectRatio(0.8f)
+                    )
+                }
+            }
         }
     }
 }
 @Composable
 private fun snackBarMessage(type: BaseSnackBarMessage): Int {
     return type.toStringResource()
+}
+
+@Composable
+fun fakeButton(
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    PrimaryButton(
+        onClick = onClick,
+        modifier = modifier,
+        isLoading = false,
+        isEnabled = true,
+        label = "Button",
+    )
 }
