81e580017cc5f10e157bd8e160a6820e4ce98c21
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-07-25 09:36:35 +0300

    refactor: update TopRatingState, Mapper, and interaction listener for TV shows

--- viewmodel/src/main/java/com/baghdad/viewmodel/topRating/TopRatingMapper.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/topRating/TopRatingMapper.kt
@@ -2,16 +2,22 @@ package com.baghdad.viewmodel.topRating
 
 import com.baghdad.entity.media.Genre
 import com.baghdad.entity.media.Movie
+import com.baghdad.entity.media.TvShow
 
 
 fun Genre.toTopRatingGenreUiState() =
-    TopRatingMovieState.GenreUiState(
+    TopRatingState.GenreUiState(
         id = this.id,
         name = this.name
     )
 
 fun Movie.toTopRatingMovieUiState() =
-    TopRatingMovieState.MovieUiState(
+    TopRatingState.MovieUiState(
+        id = this.id,
+        posterPictureURL = this.posterImageURL,
+    )
+fun TvShow.toTopRatingTvShowUiState() =
+    TopRatingState.TvShowUiState(
         id = this.id,
         posterPictureURL = this.posterImageURL,
     )
