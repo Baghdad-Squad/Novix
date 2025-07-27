f70a05290f00e0da47646c0ec67a1d7a661ccdc6
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-27 09:53:30 +0300

    feat: migrate RepositoryModule to use Hilt for dependency injection

--- app/src/main/java/com/baghdad/novix/di/RepositoryModule.kt
+++ app/src/main/java/com/baghdad/novix/di/RepositoryModule.kt
@@ -18,18 +18,145 @@ import com.baghdad.repository.MovieRepositoryImpl
 import com.baghdad.repository.RecentlyViewedRepositoryImpl
 import com.baghdad.repository.SearchRepositoryImpl
 import com.baghdad.repository.TvShowRepositoryImpl
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.module
+import com.baghdad.repository.datasource.local.LocalActorDataSource
+import com.baghdad.repository.datasource.local.LocalContinueWatchingDataSource
+import com.baghdad.repository.datasource.local.LocalFavoriteGenreDataSource
+import com.baghdad.repository.datasource.local.LocalGenreDataSource
+import com.baghdad.repository.datasource.local.LocalMovieDataSource
+import com.baghdad.repository.datasource.local.LocalRecentSearchDataSource
+import com.baghdad.repository.datasource.local.LocalRecentlyViewedDataSource
+import com.baghdad.repository.datasource.local.LocalSearchQueryDataSource
+import com.baghdad.repository.datasource.local.LocalSessionDataStore
+import com.baghdad.repository.datasource.local.LocalTvShowDataSource
+import com.baghdad.repository.datasource.local.LocalUserDataStore
+import com.baghdad.repository.datasource.remote.RemoteActorDataSource
+import com.baghdad.repository.datasource.remote.RemoteAuthenticationDataSource
+import com.baghdad.repository.datasource.remote.RemoteEpisodeDataSource
+import com.baghdad.repository.datasource.remote.RemoteGenreDataSource
+import com.baghdad.repository.datasource.remote.RemoteMovieDataSource
+import com.baghdad.repository.datasource.remote.RemoteSearchDataSource
+import com.baghdad.repository.datasource.remote.RemoteTvShowDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-val repositoryModule = module {
-    singleOf(::SearchRepositoryImpl) { bind<SearchRepository>() }
-    singleOf(::MovieRepositoryImpl) { bind<MovieRepository>() }
-    singleOf(::TvShowRepositoryImpl) { bind<TvShowRepository>() }
-    singleOf(::RecentlyViewedRepositoryImpl) { bind<RecentlyViewedRepository>() }
-    singleOf(::ActorRepositoryImpl) { bind<ActorRepository>() }
-    singleOf(::EpisodeRepositoryImpl) { bind<EpisodeRepository>() }
-    singleOf(::FavoriteGenreRepositoryImpl) { bind<FavoriteGenreRepository>() }
-    singleOf(::ContinueWatchingRepositoryImpl) { bind<ContinueWatchingRepository>() }
-    singleOf(::AuthenticationRepositoryImpl) { bind<AuthenticationRepository>() }
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RepositoryModule {
+
+    @Provides
+    fun provideSearchRepository(
+        searchRemoteDataSource: RemoteSearchDataSource,
+        remoteGenreDataSource: RemoteGenreDataSource,
+        localRecentSearchDataSource: LocalRecentSearchDataSource,
+        localActorDataSource: LocalActorDataSource,
+        localMovieDataSource: LocalMovieDataSource,
+        localTvShowDataSource: LocalTvShowDataSource,
+        localGenreDataSource: LocalGenreDataSource,
+        localSearchQueryDataSource: LocalSearchQueryDataSource,
+    ): SearchRepository {
+        return SearchRepositoryImpl(
+            searchRemoteDataSource = searchRemoteDataSource,
+            remoteGenreDataSource = remoteGenreDataSource,
+            localRecentSearchDataSource = localRecentSearchDataSource,
+            localActorDataSource = localActorDataSource,
+            localMovieDataSource = localMovieDataSource,
+            localTvShowDataSource = localTvShowDataSource,
+            localGenreDataSource = localGenreDataSource,
+            localSearchQueryDataSource = localSearchQueryDataSource
+        )
+    }
+
+    @Provides
+    fun provideMovieRepository(
+        remoteGenreDataSource: RemoteGenreDataSource,
+        localGenreDataSource: LocalGenreDataSource,
+        remoteMovieDataSource: RemoteMovieDataSource,
+    ): MovieRepository {
+        return MovieRepositoryImpl(
+            remoteGenreDataSource = remoteGenreDataSource,
+            localGenreDataSource = localGenreDataSource,
+            remoteMovieDataSource = remoteMovieDataSource
+        )
+    }
+
+    @Provides
+    fun provideTvShowRepository(
+        remoteGenreDataSource: RemoteGenreDataSource,
+        tvShowRemoteDataSource: RemoteTvShowDataSource,
+    ): TvShowRepository {
+        return TvShowRepositoryImpl(
+            remoteGenreDataSource = remoteGenreDataSource,
+            tvShowRemoteDataSource = tvShowRemoteDataSource
+        )
+    }
+
+    @Provides
+    fun provideRecentlyViewedRepository(
+        localRecentlyViewedDataSource: LocalRecentlyViewedDataSource,
+        localFavoriteGenreDataSource: LocalFavoriteGenreDataSource,
+        localMovieDataSource: LocalMovieDataSource,
+        localTvShowDataSource: LocalTvShowDataSource,
+    ): RecentlyViewedRepository {
+        return RecentlyViewedRepositoryImpl(
+            localRecentlyViewedDataSource = localRecentlyViewedDataSource,
+            localFavoriteGenreDataSource = localFavoriteGenreDataSource,
+            localMovieDataSource = localMovieDataSource,
+            localTvShowDataSource = localTvShowDataSource
+        )
+    }
+
+    @Provides
+    fun provideActorRepository(
+        remoteActorDataSource: RemoteActorDataSource,
+    ): ActorRepository {
+        return ActorRepositoryImpl(
+            remoteActorDataSource = remoteActorDataSource
+        )
+    }
+
+    @Provides
+    fun provideEpisodeRepository(
+        remoteEpisodeDataSource: RemoteEpisodeDataSource,
+        remoteTvShowDataSource: RemoteTvShowDataSource,
+    ): EpisodeRepository {
+        return EpisodeRepositoryImpl(
+            remoteEpisodeDataSource = remoteEpisodeDataSource,
+            remoteTvShowDataSource = remoteTvShowDataSource
+        )
+    }
+
+    @Provides
+    fun provideFavoriteGenreRepository(
+        favoriteGenreDataSource: LocalFavoriteGenreDataSource,
+    ): FavoriteGenreRepository {
+        return FavoriteGenreRepositoryImpl(
+            favoriteGenreDataSource = favoriteGenreDataSource
+        )
+
+    }
+
+    @Provides
+    fun provideContinueWatchingRepository(
+        localContinueWatchingDataSource: LocalContinueWatchingDataSource,
+    ): ContinueWatchingRepository {
+        return ContinueWatchingRepositoryImpl(
+            localContinueWatchingDataSource = localContinueWatchingDataSource
+        )
+    }
+
+    @Provides
+    fun provideAuthenticationRepository(
+        remoteAuthenticationDataSource: RemoteAuthenticationDataSource,
+        localSessionDataStore: LocalSessionDataStore,
+        localUserDataStore: LocalUserDataStore,
+    ): AuthenticationRepository {
+        return AuthenticationRepositoryImpl(
+            remoteAuthenticationDataSource = remoteAuthenticationDataSource,
+            localSessionDataStore = localSessionDataStore,
+            localUserDataStore = localUserDataStore
+        )
+    }
 }
