c8ef3970575bffc134fb8f22d62cdf0944946569
Aboud <gh.abd.com.207@gmail.com>
2025-07-27 13:43:31 +0300

    test: add unit tests for LoginUseCase and LogOutUseCase

---
+++ domain/src/test/java/com/baghdad/domain/usecase/login/LogOutUseCaseTest.kt
@@ -0,0 +1,44 @@
+package com.baghdad.domain.usecase.login
+
+import com.baghdad.domain.repository.AuthenticationRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class LogOutUseCaseTest {
+    lateinit var authenticationRepository: AuthenticationRepository
+    lateinit var logOutUseCase: LogOutUseCase
+
+    @BeforeEach
+    fun setUp() {
+        authenticationRepository = mockk()
+        logOutUseCase = LogOutUseCase(authenticationRepository)
+    }
+
+    @Test
+    fun `should return true when Successful logout`() = runTest{
+        // Given
+        coEvery { authenticationRepository.logOut() } returns true
+
+        // When
+        val result = logOutUseCase.invoke()
+
+        // Then
+        assert(result)
+    }
+
+    @Test
+    fun `should return false Invalid logout`() = runTest {
+        // Given
+        coEvery { authenticationRepository.logOut() } returns false
+
+        // When
+        val result = logOutUseCase.invoke()
+
+        // Then
+        assert(!result)
+    }
+
+}
