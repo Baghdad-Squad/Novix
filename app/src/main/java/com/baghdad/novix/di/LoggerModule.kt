a35fc8d87acaedc1b398af4913df23c7c459e88e
Hasan Ali <ihasanalihasan@gmail.com>
2025-07-27 09:52:51 +0300

    feat: convert LoggerModule to use Hilt for dependency injection

--- app/src/main/java/com/baghdad/novix/di/LoggerModule.kt
+++ app/src/main/java/com/baghdad/novix/di/LoggerModule.kt
@@ -2,13 +2,28 @@ package com.baghdad.novix.di
 
 import com.baghdad.novix.logger.CrashlyticsLogger
 import com.baghdad.repository.logger.Logger
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.module
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-val loggerModule = module {
-    singleOf(::CrashlyticsLogger) { bind<Logger>() }
-    single { Firebase.crashlytics }
+@InstallIn(SingletonComponent::class)
+@Module
+object LoggerModule {
+
+    @Provides
+    fun provideLogger(
+        crashlytics: FirebaseCrashlytics
+    ): Logger{
+       return CrashlyticsLogger(crashlytics = crashlytics)
+    }
+
+    @Provides
+    fun provideFirebaseCrashlytics(): FirebaseCrashlytics{
+        return  Firebase.crashlytics
+    }
+
 }
