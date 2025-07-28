f52355d04ccb8ab41419a01c0e96925e07f8fa4c
Farah315 <fara734isgreat@gmail.com>
2025-07-28 13:11:02 +0300

    refactor: rewrite HeadersSetupInterceptor again because i don't marge the devolve right

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/interceptor/RetrofitInterceptor.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/interceptor/RetrofitInterceptor.kt
@@ -1,6 +1,5 @@
 package com.baghdad.remoteDataSource.interceptor
 
-
 import com.baghdad.repository.language.LanguageProvider
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -11,13 +10,20 @@ class HeadersSetupInterceptor(
     private val authorizationToken: String
 ) : Interceptor {
 
+    private val acceptHeaderKey = "Accept"
+    private val acceptHeaderValue = "application/json"
+    private val authorizationHeaderKey = "Authorization"
+    private val authorizationHeaderPrefix = "Bearer "
+    private val languageQueryKey = "language"
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val invocation = chain.request().tag(Invocation::class.java)
-            ?: return chain.proceed(chain.request())
+        val originalRequest = chain.request()
+        val invocation = originalRequest.tag(Invocation::class.java)
         val shouldAttachAuthHeader = invocation
-            .method()
-            .annotations
-            .any { it.annotationClass == Authenticated::class }
+            ?.method()
+            ?.annotations
+            ?.any { it.annotationClass == Authenticated::class } == true
+
         val language = languageProvider.getCurrentLanguage()
 
         val modifiedRequest = originalRequest.newBuilder().apply {
