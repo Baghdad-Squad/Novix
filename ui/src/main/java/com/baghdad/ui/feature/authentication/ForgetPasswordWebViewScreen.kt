d9a726053423dbc45a175219277620f33e2781f3
Aboud <gh.abd.com.207@gmail.com>
2025-07-28 05:23:25 +0300

    feat: display toast message during automatic redirection

--- ui/src/main/java/com/baghdad/ui/feature/authentication/ForgetPasswordWebViewScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/authentication/ForgetPasswordWebViewScreen.kt
@@ -1,5 +1,6 @@
 package com.baghdad.ui.feature.authentication
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -9,12 +10,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.baghdad.ui.R
 import com.baghdad.ui.feature.component.AppWebView
 import com.baghdad.ui.navigation.graph.authentication.AuthenticationNavEvent
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 @Composable
@@ -38,7 +41,7 @@ fun ForgotPasswordWebViewContent(
     languageTag: String,
 ) {
     val context = LocalContext.current
-
+    val scope = rememberCoroutineScope()
     val messages = remember {
         mapOf(
             "pageNotFound" to context.getString(R.string.oops_we_can_t_find_the_page),
@@ -50,6 +53,14 @@ fun ForgotPasswordWebViewContent(
 
     LaunchedEffect(shouldNavigateBack.value) {
         if (shouldNavigateBack.value) {
+            scope.launch {
+                Toast
+                    .makeText(
+                        context,
+                        context.getString(R.string.automatically_redirecting_to_login),
+                        Toast.LENGTH_LONG,
+                    ).show()
+            }
             delay(5000)
             handleNavigation(AuthenticationNavEvent.NavigateBack)
         }
