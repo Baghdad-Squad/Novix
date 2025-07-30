bba66dafbbbf5e4d54d4a7b217f5a193a527dcee
omer1998 <omerfaris11@gmail.com>
2025-07-30 17:51:52 +0300

    feat: implement pagination for category tv shows screen

---
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/mapper/tvShow/DicoverTvShowsMapper.kt
@@ -0,0 +1,15 @@
+package com.baghdad.remoteDataSource.mapper.tvShow
+
+import com.baghdad.remoteDataSource.response.tvShow.TvShowResponse
+import com.baghdad.remoteDataSource.util.getNextKey
+import com.baghdad.remoteDataSource.util.getPreviousKey
+import com.baghdad.repository.model.PagedResultDto
+import com.baghdad.repository.model.TvShowDto
+
+fun TvShowResponse.toPagedTvShowDtos(): PagedResultDto<TvShowDto> {
+    return PagedResultDto(
+        data = this.results?.mapNotNull { it.toDto() } ?: emptyList(),
+        nextKey = getNextKey(page, totalPages),
+        prevKey = getPreviousKey(page)
+    )
+}
