3a439f644d1534478bacb6298b9d86756ebaae80
Arzo-zi <zinahashraf88@gmail.com>
2025-08-04 00:14:56 +0300

    feat: add createSavedList endpoint

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/SavedListApiService.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/SavedListApiService.kt
@@ -1,8 +1,13 @@
 package com.baghdad.remoteDataSource.apiService
 
+import com.baghdad.remoteDataSource.interceptor.Authenticated
 import com.baghdad.remoteDataSource.response.savedList.CreateSavedListResponse
 import retrofit2.Response
+import retrofit2.http.POST
 
 interface SavedListApiService {
+
+    @Authenticated
+    @POST("list")
     suspend fun createSavedList(title: String): Response<CreateSavedListResponse>
 }
