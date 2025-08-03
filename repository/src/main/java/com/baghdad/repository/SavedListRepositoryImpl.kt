c7bf78b900e0c4e6e0eecdc30bcff7a6f9c885c4
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-08-03 19:05:57 +0300

    feat: implement add item to saved list repository

--- repository/src/main/java/com/baghdad/repository/SavedListRepositoryImpl.kt
+++ repository/src/main/java/com/baghdad/repository/SavedListRepositoryImpl.kt
@@ -1,8 +1,28 @@
 package com.baghdad.repository
 
 import com.baghdad.domain.repository.SavedListRepository
+import com.baghdad.repository.datasource.local.LocalSessionDataStore
 import com.baghdad.repository.datasource.remote.RemoteSavedListDataSource
+import com.baghdad.repository.util.executeAuthorizedSafely
 
 class SavedListRepositoryImpl(
     private val remoteSavedListSource: RemoteSavedListDataSource,
-) : SavedListRepository
+    private val localSessionDataSource: LocalSessionDataStore
+) : SavedListRepository {
+
+
+    override suspend fun addMovieToSavedList(listId: Long, movieId: Long) {
+        val sessionId = localSessionDataSource.getSessionId().toString()
+        executeAuthorizedSafely(sessionId) {
+
+            remoteSavedListSource.addMovieToSavedList(listId, movieId, sessionId)
+        }
+    }
+
+    override suspend fun addTvShowToSavedList(listId: Long, tvShowId: Long) {
+        val sessionId = localSessionDataSource.getSessionId().toString()
+        executeAuthorizedSafely(sessionId) {
+            remoteSavedListSource.addTvShowToSavedList(listId, tvShowId, sessionId)
+        }
+    }
+}
