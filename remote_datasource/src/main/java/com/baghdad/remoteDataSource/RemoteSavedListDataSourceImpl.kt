967fc5d47c6ce7e97717c9451afb375229b39411
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-08-03 19:06:17 +0300

    feat: implement add item to saved list remote

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteSavedListDataSourceImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteSavedListDataSourceImpl.kt
@@ -1,10 +1,43 @@
 package com.baghdad.remoteDataSource
 
 import com.baghdad.remoteDataSource.apiService.SavedListApiService
+import com.baghdad.remoteDataSource.request.AddItemsRequest
+import com.baghdad.remoteDataSource.request.MediaItem
+import com.baghdad.remoteDataSource.util.handleRequest
 import com.baghdad.repository.datasource.remote.RemoteSavedListDataSource
 import com.baghdad.repository.logger.Logger
 
 class RemoteSavedListDataSourceImpl(
     private val savedListApiService: SavedListApiService,
     private val logger: Logger,
-) : RemoteSavedListDataSource
+) : RemoteSavedListDataSource {
+
+    override suspend fun addMovieToSavedList(listId: Long, movieId: Long, sessionId: String) {
+        val body = AddItemsRequest(
+            items = listOf(
+                MediaItem(mediaType = "movie", mediaId = movieId)
+            )
+        )
+
+        return handleRequest(
+            apiCall = { savedListApiService.addItemToSavedList(listId, body, sessionId) },
+            logger = logger
+        )
+    }
+
+    override suspend fun addTvShowToSavedList(listId: Long, tvShowId: Long, sessionId: String) {
+        val body = AddItemsRequest(
+            items = listOf(
+                MediaItem(
+                    mediaType = "tv",
+                    mediaId = tvShowId
+                )
+            )
+        )
+
+        return handleRequest(
+            apiCall = { savedListApiService.addItemToSavedList(listId, body, sessionId) },
+            logger = logger
+        )
+    }
+}
--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/SavedListApiService.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/SavedListApiService.kt
@@ -1,3 +1,23 @@
 package com.baghdad.remoteDataSource.apiService
 
-interface SavedListApiService
+import com.baghdad.remoteDataSource.interceptor.Authenticated
+import com.baghdad.remoteDataSource.request.AddItemsRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface SavedListApiService {
+    @Authenticated
+    @POST(ADD_ITEM_TO_SAVED_LIST_ENDPOINT)
+    suspend fun addItemToSavedList(
+        @Path("list_id") listId: Long,
+        @Body body: AddItemsRequest,
+        @Query("session_id") sessionId: String
+    ): Response<Unit>
+
+    companion object {
+        private const val ADD_ITEM_TO_SAVED_LIST_ENDPOINT = "list/{list_id}/items"
+    }
+}
