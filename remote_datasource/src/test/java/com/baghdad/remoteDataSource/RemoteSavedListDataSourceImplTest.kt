77723e7f5c2e3ded695fce3caf3605e131c4fed5
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-08-03 19:24:20 +0300

    test: add unit test for getSavedLists in RemoteSavedListDataSourceImpl

--- remote_datasource/src/test/java/com/baghdad/remoteDataSource/RemoteSavedListDataSourceImplTest.kt
+++ remote_datasource/src/test/java/com/baghdad/remoteDataSource/RemoteSavedListDataSourceImplTest.kt
@@ -1,10 +1,17 @@
 package com.baghdad.remoteDataSource
 
 import com.baghdad.remoteDataSource.apiService.SavedListApiService
+import com.baghdad.remoteDataSource.response.UserListDto
+import com.baghdad.remoteDataSource.response.UserListsResponse
 import com.baghdad.repository.datasource.remote.RemoteSavedListDataSource
 import com.baghdad.repository.logger.Logger
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import retrofit2.Response
 
 class RemoteSavedListDataSourceImplTest {
     private lateinit var savedListApiService: SavedListApiService
@@ -17,4 +24,22 @@ class RemoteSavedListDataSourceImplTest {
         logger = mockk(relaxed = true)
         remoteSource = RemoteSavedListDataSourceImpl(savedListApiService, logger)
     }
+
+    @Test
+    fun `should return paged results when it get user list of data`() = runTest {
+        //Given
+        val page =1
+        val response = UserListsResponse(
+            page = page,
+            results = listOf(
+                UserListDto(id = 1, name = "Super Man")
+            ),
+            totalPages = 10
+        )
+        coEvery { savedListApiService.getSavedLists(page,"") } returns Response.success(response)
+        val result = remoteSource.getSavedLists(page,"")
+
+        assertThat(result.data).hasSize(1)
+    }
+
 }
