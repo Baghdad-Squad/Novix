5f6ba0cb720b805e76055d062f7faa66dc52160d
Arzo-zi <zinahashraf88@gmail.com>
2025-08-04 02:15:42 +0300

    refactor: modify create saved list request body

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteSavedListDataSourceImpl.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/RemoteSavedListDataSourceImpl.kt
@@ -1,9 +1,11 @@
 package com.baghdad.remoteDataSource
 
 import com.baghdad.remoteDataSource.apiService.SavedListApiService
+import com.baghdad.remoteDataSource.request.CreateListRequest
 import com.baghdad.remoteDataSource.response.savedList.CreateSavedListResponse
 import com.baghdad.remoteDataSource.util.handleRequest
 import com.baghdad.repository.datasource.remote.RemoteSavedListDataSource
+import com.baghdad.repository.exception.ItemCreationFailedException
 import com.baghdad.repository.logger.Logger
 
 class RemoteSavedListDataSourceImpl(
@@ -12,13 +14,17 @@ class RemoteSavedListDataSourceImpl(
 ) : RemoteSavedListDataSource {
     override suspend fun createSavedList(title: String, sessionId: String) {
         val result = handleRequest<CreateSavedListResponse>(
-            apiCall = { savedListApiService.createSavedList(title) },
+            apiCall = {
+                savedListApiService.createSavedList(
+                    body = CreateListRequest(name = title)
+                )
+            },
             logger = logger,
         )
 
-        result.takeIf { it.success == true } ?: run {
+        if (result.success != true) {
             val message = result.statusMessage ?: "List creation failed"
-            logger.logException(Exception("Failed to create list: $message"))
+            throw ItemCreationFailedException(message)
         }
     }
 }
--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/SavedListApiService.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/SavedListApiService.kt
@@ -1,13 +1,22 @@
 package com.baghdad.remoteDataSource.apiService
 
 import com.baghdad.remoteDataSource.interceptor.Authenticated
+import com.baghdad.remoteDataSource.request.CreateListRequest
 import com.baghdad.remoteDataSource.response.savedList.CreateSavedListResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface SavedListApiService {
 
     @Authenticated
-    @POST("list")
-    suspend fun createSavedList(title: String): Response<CreateSavedListResponse>
+    @POST(CREATE_SAVED_LIST)
+    suspend fun createSavedList(
+        @Body body: CreateListRequest
+    ): Response<CreateSavedListResponse>
+
+    companion object {
+        private const val CREATE_SAVED_LIST = "list"
+    }
+
 }
