7fd8eb048bd3529b933785f4652d98db41aa8632
karrar-abbas <54599574+karrar-abbas@users.noreply.github.com>
2025-08-05 01:36:23 +0300

    fix: add default values to profile screen ui state

--- viewmodel/src/main/java/com/baghdad/viewmodel/profile/ProfileScreenState.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/profile/ProfileScreenState.kt
@@ -3,9 +3,10 @@ package com.baghdad.viewmodel.profile
 import com.baghdad.viewmodel.base.BaseUiState
 
 data class ProfileScreenUIState(
+    val isLoading: Boolean = true,
     val isLogin: Boolean = false,
-    val userName: String,
-    val imageUrl: String,
-    val appearance: String,
-    val language: String
+    val userName: String = "",
+    val imageUrl: String = "",
+    val appearance: String = "",
+    val language: String = "",
 ) : BaseUiState
