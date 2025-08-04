f86dacb56d45db018d76fbf9189e2039f306d608
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-08-04 18:35:39 +0300

    feat: add SavedListDetailsInteractionListener

--- viewmodel/src/main/java/com/baghdad/viewmodel/savedListDetails/SavedListDetailsInteractionListener.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/savedListDetails/SavedListDetailsInteractionListener.kt
@@ -1,3 +1,11 @@
 package com.baghdad.viewmodel.savedListDetails
 
-interface SavedListDetailsInteractionListener
+interface SavedListDetailsInteractionListener {
+    fun onBackClick()
+    fun onDeleteClick(listId: Long)
+    fun onCategoryClick(categoryId: Long)
+    fun onTvShowClick(tvShowId: Long)
+    fun onMovieClick(movieId: Long)
+    fun onSaveMovieClick(movieId: Long)
+    fun onSaveTvShowClick(tvShowId: Long)
+}
