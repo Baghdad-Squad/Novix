882ee67673c968e175f6d90b81144dde0e07d7ff
Farah315 <fara734isgreat@gmail.com>
2025-08-04 23:39:29 +0300

    feat: implement login status checks and logout functionality in BottomSheetViewModel

--- viewmodel/src/main/java/com/baghdad/viewmodel/profile/profileBottomSheet/BottomSheetViewModel.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/profile/profileBottomSheet/BottomSheetViewModel.kt
@@ -1,27 +1,35 @@
 package com.baghdad.viewmodel.profile.profileBottomSheet
 
 import com.baghdad.domain.model.AppAppearanceMode
-import com.baghdad.domain.usecase.profile.language.GetLanguageUseCase
+import com.baghdad.domain.usecase.login.IsLoggedInUseCase
+import com.baghdad.domain.usecase.login.LogOutUseCase
 import com.baghdad.domain.usecase.profile.appearance.GetAppearanceModeUseCase
-import com.baghdad.domain.usecase.profile.language.SetLanguageUseCase
 import com.baghdad.domain.usecase.profile.appearance.SetAppearanceModeUseCase
+import com.baghdad.domain.usecase.profile.language.GetLanguageUseCase
+import com.baghdad.domain.usecase.profile.language.SetLanguageUseCase
 import com.baghdad.viewmodel.base.BaseViewModel
 import com.baghdad.viewmodel.errorStates.BaseSnackBarMessage
+import com.baghdad.viewmodel.errorStates.LoginSnackBarMessage
+import com.baghdad.viewmodel.profile.ProfileEffect
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 @HiltViewModel
-class BottomSheetViewModel@Inject constructor(
+class BottomSheetViewModel @Inject constructor(
     private val getLanguageUseCase: GetLanguageUseCase,
     private val setLanguageUseCase: SetLanguageUseCase,
     private val getThemeModeUseCase: GetAppearanceModeUseCase,
     private val setAppearanceModeUseCase: SetAppearanceModeUseCase,
+    private val isLoggedInUseCase: IsLoggedInUseCase,
+    private val logOutUseCase: LogOutUseCase,
     private val ioDispatcher: CoroutineDispatcher,
-) : BaseViewModel<BottomSheetState,Nothing>(BottomSheetState()),
+) : BaseViewModel<BottomSheetState, ProfileEffect>(BottomSheetState()),
     BottomSheetInteractionListener {
 
     init {
+        isLoggedIn()
         getCurrentLanguage()
         getThemeMode()
     }
@@ -33,6 +41,25 @@ class BottomSheetViewModel@Inject constructor(
             onSuccess = ::getLanguageSuccess,
         )
     }
+
+    private fun isLoggedIn() {
+        tryToExecute(
+            dispatcher = ioDispatcher,
+            callee = { isLoggedInUseCase },
+            onSuccess = {// TODO isLoggedInSuccess }
+            }
+        )
+    }
+
+
+    private fun isLoggedOutSuccess() {
+        showSnackBar(
+            message = LoginSnackBarMessage.LoginOutSuccessfully,
+            isSuccess = true,
+        )
+        sendEffect(ProfileEffect.NavigateToLogin)
+    }
+
 
     private fun getLanguageSuccess(currentLanguage: String) {
         updateState { it.copy(currentLanguage = currentLanguage) }
@@ -118,9 +145,21 @@ class BottomSheetViewModel@Inject constructor(
         updateState { it.copy(isVisible = false) }
     }
 
+    override fun onLogOutClick() {
+        tryToExecute(
+            dispatcher = ioDispatcher,
+            callee = {
+                logOutUseCase()
+            },
+            onSuccess = {
+                isLoggedOutSuccess()
+            },
+        )
+    }
+
     override fun mapThrowableToErrorMessage(throwable: Throwable): BaseSnackBarMessage {
         return when (throwable) {
-            is java.net.UnknownHostException -> BaseSnackBarMessage.NetworkError
+            is UnknownHostException -> BaseSnackBarMessage.NetworkError
             else -> BaseSnackBarMessage.UnknownError
         }
     }
