81e581e9b7e845184a6d077163b445db8e516ad1
karrar-abbas <54599574+karrar-abbas@users.noreply.github.com>
2025-08-05 02:41:21 +0300

    refactor: add profile screen settings items list

--- ui/src/main/java/com/baghdad/ui/feature/profile/ProfileScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/profile/ProfileScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,9 +17,7 @@ import com.baghdad.design_system.component.Text
 import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.R
 import com.baghdad.ui.feature.profile.component.ProfileHeaderWithOption
-import com.baghdad.ui.feature.profile.component.ProfileItemWithDefaults
-import com.baghdad.ui.feature.profile.component.ProfileScreenDivider
-import com.baghdad.ui.feature.profile.component.ProfileScreenItem
+import com.baghdad.ui.feature.profile.component.ProfileScreenItemsList
 import com.baghdad.viewmodel.profile.ProfileInteractionListener
 import com.baghdad.viewmodel.profile.ProfileScreenUIState
 import com.baghdad.viewmodel.profile.ProfileViewModel
@@ -30,17 +27,16 @@ fun ProfileScreen(
     viewModel: ProfileViewModel = hiltViewModel(),
 ) {
     val state by viewModel.uiState.collectAsStateWithLifecycle()
-
     ProfileScreenContent(
         state = state,
-        interactionListener = viewModel,
+        listener = viewModel,
     )
 }
 
 @Composable
 private fun ProfileScreenContent(
     state: ProfileScreenUIState,
-    interactionListener: ProfileInteractionListener,
+    listener: ProfileInteractionListener,
 ) {
     Scaffold(
         modifier = Modifier
@@ -52,10 +48,9 @@ private fun ProfileScreenContent(
                 text = stringResource(R.string.my_account),
                 style = Theme.typography.title.large,
                 color = Theme.color.title,
-                modifier =
-                    Modifier
-                        .padding(horizontal = 16.dp)
-                        .padding(vertical = 25.dp),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .padding(vertical = 25.dp),
             )
         },
     ) {
@@ -65,46 +60,18 @@ private fun ProfileScreenContent(
             ProfileHeaderWithOption(
                 userName = state.userName,
                 imageUrl = state.imageUrl,
-                onLogoutClick = {},
+                onLogoutClick = listener::ontClickLogOut,
             )
-            ProfileScreenItem(
-                title = stringResource(R.string.watching_history),
-                icon = painterResource(com.baghdad.design_system.R.drawable.ic_time_schedule),
-                onClick = {}
+            ProfileScreenItemsList(
+                appearance = state.appearance,
+                language = state.language,
+                onclickWatchingHistory = listener::onclickWatchingHistory,
+                onclickMyRating = listener::onclickMyRating,
+                onclickContentRestriction = listener::onclickContentRestriction,
+                onclickChangePassword = listener::onclickChangePassword,
+                onclickAppearance = listener::onclickAppearance,
+                onclickLanguage = listener::onclickLanguage,
             )
-            ProfileScreenDivider()
-            ProfileScreenItem(
-                title = stringResource(R.string.my_rating),
-                icon = painterResource(com.baghdad.design_system.R.drawable.ic_star_square),
-                onClick = {}
-            )
-            ProfileScreenDivider()
-            ProfileScreenItem(
-                title = stringResource(R.string.content_restriction),
-                icon = painterResource(R.drawable.shield_energy),
-                onClick = {}
-            )
-            ProfileScreenDivider()
-            ProfileScreenItem(
-                title = stringResource(R.string.change_password),
-                icon = painterResource(com.baghdad.design_system.R.drawable.ic_lock_key),
-                onClick = {}
-            )
-            ProfileScreenDivider()
-            ProfileItemWithDefaults(
-                title = stringResource(R.string.appearance),
-                icon = painterResource(com.baghdad.design_system.R.drawable.ic_moon),
-                defaultValue = state.appearance,
-                onClick = {}
-            )
-            ProfileScreenDivider()
-            ProfileItemWithDefaults(
-                title = stringResource(R.string.language),
-                icon = painterResource(com.baghdad.design_system.R.drawable.ic_language_circle),
-                defaultValue = state.appearance,
-                onClick = {}
-            )
-
         }
     }
 }
