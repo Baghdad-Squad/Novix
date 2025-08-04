f3f0f4584155d7c194564df71bcffb1ce0b0a642
omer1998 <omerfaris11@gmail.com>
2025-08-04 13:53:49 +0300

    feat: add cache interceptor and initialize image loader

--- app/src/main/java/com/baghdad/novix/NovixApplication.kt
+++ app/src/main/java/com/baghdad/novix/NovixApplication.kt
@@ -1,8 +1,14 @@
 package com.baghdad.novix
 
 import android.app.Application
+import com.baghdad.islamic_image_loader.ui.CustomImageLoader
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
-class NovixApplication : Application()
+class NovixApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CustomImageLoader.init(this@NovixApplication)
+    }
+}
