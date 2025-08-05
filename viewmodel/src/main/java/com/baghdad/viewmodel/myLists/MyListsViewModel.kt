ac2723884b6af6fd589fbe93c4826c20edf16fa0
edrees <muhammed.edrees101@gmail.com>
2025-08-05 19:31:17 +0300

    feat: refactor MyListsViewModel for improved login handling and data fetching

--- viewmodel/src/main/java/com/baghdad/viewmodel/myLists/MyListsViewModel.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/myLists/MyListsViewModel.kt
@@ -7,183 +7,169 @@ import com.baghdad.domain.usecase.login.IsLoggedInUseCase
 import com.baghdad.domain.usecase.savedList.CreateSavedListUseCase
 import com.baghdad.domain.usecase.savedList.GetSavedListsUseCase
 import com.baghdad.entity.savedList.SavedList
+import com.baghdad.viewmodel.R
 import com.baghdad.viewmodel.base.BaseViewModel
 import com.baghdad.viewmodel.errorStates.BaseSnackBarMessage
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class MyListsViewModel(
-    private val getSavedListsUseCase: GetSavedListsUseCase,
-    private val createSavedListUseCase: CreateSavedListUseCase,
-    private val isUserLoggedInUseCase: IsLoggedInUseCase,
-    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
-) : BaseViewModel<MyListsScreenState, MyListsScreenEffect>(MyListsScreenState()),
-    MyListsInteractionListener {
-
-    init {
-        checkIfUserIsLoggedIn()
-    }
-
-    private fun checkIfUserIsLoggedIn() {
-        tryToExecute(
-            callee = { isUserLoggedInUseCase() },
-            onSuccess = ::handleLoginResult,
-            dispatcher = defaultDispatcher,
-            onError = ::onLoadDataError,
-            onStart = ::onCheckIfUserLoggedInStart,
-            onFinally = ::onCheckIfUserLoggedInFinished
-        )
-    }
-
-    private fun handleLoginResult(isLoggedIn: Boolean) {
-        updateState {
-            it.copy(isUsedLoggedIn = isLoggedIn)
+@HiltViewModel
+class MyListsViewModel
+    @Inject
+    constructor(
+        private val getSavedListsUseCase: GetSavedListsUseCase,
+        private val createSavedListUseCase: CreateSavedListUseCase,
+        private val isUserLoggedInUseCase: IsLoggedInUseCase,
+        private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO,
+    ) : BaseViewModel<MyListsScreenState, MyListsScreenEffect>(MyListsScreenState()),
+        MyListsInteractionListener {
+        init {
+            checkIfUserIsLoggedIn()
         }
-        if (isLoggedIn) {
-            getSavedLists()
-        }
-    }
 
-    private fun getSavedLists() {
-        collectPagingFlow(
-            loadData = ::fetchSavedLists,
-            onInitialLoadFinished = ::onInitialLoadSnackBarFinished,
-            onInitialLoadError = { error -> handleError(error) },
-            pageSize = PAGES_SIZE,
-            mapEntityToUiState = SavedList::toUiState,
-            onFlowCreated = ::handleSavedListsFlowCreated,
-            onLoadingChanged = ::addSavedListLoadingChanged
-        )
-    }
-
-    private fun handleSavedListsFlowCreated(flow: Flow<PagingData<MyListsScreenState.SavedListUiState>>) {
-        updateState {
-            it.copy(savedLists = flow)
-        }
-    }
-
-    private suspend fun fetchSavedLists(page: Int): PagedResult<SavedList> {
-        return getSavedListsUseCase(
-            page = page,
-            pageSize = PAGES_SIZE
-        )
-    }
-
-    private fun addSavedListLoadingChanged(isLoading: Boolean) {
-        updateState {
-            it.copy(
-                isLoading = isLoading
+        private fun checkIfUserIsLoggedIn() {
+            tryToExecute(
+                callee = { isUserLoggedInUseCase() },
+                onSuccess = ::onCheckIfUserIsLoggedInSuccess,
+                dispatcher = defaultDispatcher,
             )
         }
-    }
 
-    private fun onInitialLoadSnackBarFinished() {
-        {
+        private fun onCheckIfUserIsLoggedInSuccess(isLoggedIn: Boolean) {
+            updateState {
+                it.copy(isUsedLoggedIn = isLoggedIn)
+            }
+            if (isLoggedIn) {
+                getSavedLists()
+            }
+        }
+
+        private fun getSavedLists() {
+            collectPagingFlow(
+                loadData = ::fetchSavedLists,
+                onInitialLoadError = ::onLoadDataError,
+                pageSize = PAGES_SIZE,
+                mapEntityToUiState = SavedList::toUiState,
+                onFlowCreated = ::onSavedListsFlowCreated,
+                onLoadingChanged = ::onGetSavedListsLoadingChanged,
+            )
+        }
+
+        private suspend fun fetchSavedLists(page: Int): PagedResult<SavedList> =
+            getSavedListsUseCase(
+                page = page,
+                pageSize = PAGES_SIZE,
+            )
+
+        private fun onLoadDataError(throwable: Throwable) {
+            when (throwable) {
+                is NoInternetException -> showNoInternetSnackBar()
+                else -> handleError(throwable)
+            }
+        }
+
+        private fun onSavedListsFlowCreated(flow: Flow<PagingData<MyListsScreenState.SavedListUiState>>) {
+            updateState {
+                it.copy(savedLists = flow)
+            }
+            hideSnackBar()
+        }
+
+        private fun onGetSavedListsLoadingChanged(isLoading: Boolean) {
             updateState {
                 it.copy(
-                    savedLists = it.savedLists,
+                    isLoading = isLoading,
                 )
             }
         }
-    }
 
-    private fun onLoadDataError(throwable: Throwable) {
-        when (throwable) {
-            is NoInternetException -> showNoInternetSnackBar()
-            else -> handleError(throwable)
-        }
-    }
-
-    private fun onCheckIfUserLoggedInStart() {
-        updateState {
-            it.copy(
-                isUsedLoggedIn = true
+        private fun showNoInternetSnackBar() {
+            showSnackBar(
+                message = BaseSnackBarMessage.NetworkError,
+                actionLabelRes = R.string.retry,
+                isSuccess = false,
+                durationMillis = Int.MAX_VALUE.toLong(),
             )
         }
-    }
 
-    private fun onCheckIfUserLoggedInFinished() {
-        updateState {
-            it.copy(
-                isUsedLoggedIn = false
-            )
-        }
-    }
-
-    private fun showNoInternetSnackBar() {
-        updateState {
-            it.copy(
-            )
-        }
-        hideSnackBar()
-    }
-
-    override fun onAddListFabClick() {
-        updateState {
-            it.copy(
-                addListBottomSheetState = MyListsScreenState.AddListBottomSheetState(
-                    isVisible = true,
+        override fun onAddListFabClick() {
+            updateState {
+                it.copy(
+                    addListBottomSheetState =
+                        MyListsScreenState.AddListBottomSheetState(
+                            isVisible = true,
+                        ),
                 )
-            )
+            }
         }
-    }
 
-    override fun onLoginClick() {
-        sendEffect(MyListsScreenEffect.NavigateToLogin)
-    }
+        override fun onLoginClick() {
+            sendEffect(MyListsScreenEffect.NavigateToLogin)
+        }
 
-    override fun onAddedListNameChange(name: String) {
-        updateState {
-            it.copy(
-                addListBottomSheetState = it.addListBottomSheetState.copy(
-                    listName = name
+        override fun onAddedListNameChange(name: String) {
+            updateState {
+                it.copy(
+                    addListBottomSheetState =
+                        it.addListBottomSheetState.copy(
+                            listName = name,
+                        ),
                 )
-            )
+            }
         }
-    }
 
-    override fun onAddListBottomSheetDismiss() {
-        updateState {
-            it.copy(
-                addListBottomSheetState = it.addListBottomSheetState
-            )
-        }
-    }
-
-    override fun onAddListBottomSheetAddClick() {
-        val listName = uiState.value.addListBottomSheetState.listName
-
-        ::setBottomSheetLoading
-
-        tryToExecute(
-            callee = { createSavedListUseCase(listName) },
-            dispatcher = defaultDispatcher,
-            onSuccess = { hideAddListBottomSheet() },
-            onError = ::handleAddListError
-        )
-    }
-
-    private fun handleAddListError(error: Throwable) {
-        handleError(error)
-        setBottomSheetLoading(false)
-    }
-
-
-    private fun hideAddListBottomSheet() {
-        updateState {
-            it.copy(
-                addListBottomSheetState = MyListsScreenState.AddListBottomSheetState(
-                    isVisible = false
+        override fun onAddListBottomSheetDismiss() {
+            updateState {
+                it.copy(
+                    addListBottomSheetState =
+                        it.addListBottomSheetState.copy(
+                            isVisible = false,
+                        ),
                 )
+            }
+        }
+
+        override fun onAddListBottomSheetAddClick() {
+            val listName = uiState.value.addListBottomSheetState.listName
+            tryToExecute(
+                callee = { createSavedListUseCase(listName) },
+                dispatcher = defaultDispatcher,
+                onStart = ::onAddListStart,
+                onSuccess = { onAddListSuccess() },
+                onFinally = ::onAddListFinally,
             )
         }
-    }
 
-    private fun setBottomSheetLoading(isLoading: Boolean) {
-        updateState {
-            it.copy(
-                addListBottomSheetState = it.addListBottomSheetState.copy(isLoading = isLoading)
+        private fun onAddListSuccess() {
+            updateState {
+                it.copy(
+                    addListBottomSheetState =
+                        it.addListBottomSheetState.copy(
+                            isVisible = false,
+                        ),
+                )
+            }
+            getSavedLists()
+        }
+
+        private fun onAddListFinally() {
+            updateState {
+                it.copy(
+                    addListBottomSheetState =
+                        it.addListBottomSheetState.copy(
+                            isLoading = false,
+                        ),
+                )
+            }
+        }
+
+        private fun onAddListStart() {
+            updateState {
+                it.copy(
+                    addListBottomSheetState = it.addListBottomSheetState.copy(isLoading = true),
             )
         }
     }
@@ -196,9 +182,8 @@ class MyListsViewModel(
         getSavedLists()
     }
 
-    override fun mapThrowableToErrorMessage(throwable: Throwable): BaseSnackBarMessage {
-        return BaseSnackBarMessage.UnknownError
-    }
+    override fun mapThrowableToErrorMessage(throwable: Throwable): BaseSnackBarMessage =
+        BaseSnackBarMessage.UnknownError
 
     companion object {
         private const val PAGES_SIZE = 20
