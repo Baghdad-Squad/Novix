1fbc46f26b41723fe67331c40aadc493cd47b64a
Malak Raef <malak.raef20@gmail.com>
2025-08-05 06:15:58 +0300

    feat: add required endpoints for EpisodeApiService.kt

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/EpisodeApiService.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/apiService/EpisodeApiService.kt
@@ -1,13 +1,19 @@
 package com.baghdad.remoteDataSource.apiService
 
 import com.baghdad.remoteDataSource.interceptor.Authenticated
+import com.baghdad.remoteDataSource.request.RatingRequest
 import com.baghdad.remoteDataSource.response.CastMembersResponse
+import com.baghdad.remoteDataSource.response.RatingResponse
 import com.baghdad.remoteDataSource.response.episode.EpisodeDetailsResponse
 import com.baghdad.remoteDataSource.response.episode.EpisodeImageResponse
 import com.baghdad.remoteDataSource.response.episode.EpisodeVideosResponse
+import com.baghdad.remoteDataSource.response.MediaAccountStatesResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface EpisodeApiService {
 
@@ -43,6 +49,25 @@ interface EpisodeApiService {
         @Path("episode_number") episodeNumber: Int
     ): Response<EpisodeVideosResponse>
 
+    @Authenticated
+    @POST(RATE_EPISODE_ENDPOINT)
+    suspend fun addEpisodeRate(
+        @Path("series_id") seriesId: Long,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int,
+        @Query("session_id") sessionId: String,
+        @Body rating: RatingRequest
+    ): Response<RatingResponse>
+
+    @Authenticated
+    @GET(EPISODE_ACCOUNT_STATES)
+    suspend fun getEpisodeAccountStates(
+        @Path("series_id") seriesId: Long,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int,
+        @Query("session_id") sessionId: String,
+    ): Response<MediaAccountStatesResponse>
+
 
     companion object {
         private const val EPISODES_DETAILS_ENDPOINT =
@@ -53,6 +78,9 @@ interface EpisodeApiService {
             "tv/{tv_id}/season/{season_number}/episode/{episode_number}/images"
         private const val EPISODE_VIDEOS_ENDPOINT =
             "tv/{tv_id}/season/{season_number}/episode/{episode_number}/videos"
-
+        private const val RATE_EPISODE_ENDPOINT =
+           " tv/{series_id}/season/{season_number}/episode/{episode_number}/rating"
+        private const val EPISODE_ACCOUNT_STATES =
+            "tv/{series_id}/season/{season_number}/episode/{episode_number}/account_states"
     }
 }
