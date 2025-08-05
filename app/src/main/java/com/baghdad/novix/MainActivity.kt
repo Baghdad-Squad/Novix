9283a491a3b67b62d05811a543cef9eef288d598
omer1998 <omerfaris11@gmail.com>
2025-08-05 04:27:11 +0300

    feat: implement LanguageManager to track system language changes

--- app/src/main/java/com/baghdad/novix/MainActivity.kt
+++ app/src/main/java/com/baghdad/novix/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.baghdad.novix
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.baghdad.design_system.theme.NovixTheme
 import com.baghdad.ui.main.MainScreen
+import com.baghdad.viewmodel.util.LanguageManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +20,16 @@ class MainActivity : ComponentActivity() {
             NovixTheme {
                MainScreen()
             }
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val newLanguage = newConfig.locales[0].language
+        Log.i("LanguageManager", "Current language: ${LanguageManager.currentLanguage.value}")
+        Log.i("LanguageManager", "system language: $newLanguage")
+        if (newLanguage != LanguageManager.currentLanguage.value) {
+            LanguageManager.updateLanguage(newLanguage)
         }
     }
 }
