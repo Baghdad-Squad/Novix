bceb48fc0c0371c0c972facceee16a38a4141c73
Aboud <gh.abd.com.207@gmail.com>
2025-08-05 11:15:14 +0300

    feat: add BackgroundBlur component to MovieDetailsScreen

--- ui/src/main/java/com/baghdad/ui/feature/movieDetails/MovieDetailsScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/movieDetails/MovieDetailsScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.baghdad.design_system.component.BackgroundBlur
 import com.baghdad.design_system.component.SaveIcon
 import com.baghdad.design_system.component.Scaffold
 import com.baghdad.design_system.component.SnackBar
@@ -187,11 +188,13 @@ private fun MovieDetailsContent(
                 actionLabel = snackBarState.actionLabelRes?.let { stringResource(it) },
                 onActionClick = listener::onSnackBarActionLabelClick,
             )
+        },
+        backgroundBlur = {
+            BackgroundBlur(modifier = Modifier.zIndex(999f))
         }
     ) {
         Box(
             modifier = modifier
-                .background(Theme.color.surface)
                 .fillMaxSize()
                 .navigationBarsPadding()
         ) {
