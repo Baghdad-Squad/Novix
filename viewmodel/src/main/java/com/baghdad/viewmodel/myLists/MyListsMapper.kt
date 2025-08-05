5899416728b88294d4f201ab4d237905ca3b74f7
Arzo-zi <zinahashraf88@gmail.com>
2025-08-05 17:36:40 +0300

    feat: add SavedList toUiState mapper

--- viewmodel/src/main/java/com/baghdad/viewmodel/myLists/MyListsMapper.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/myLists/MyListsMapper.kt
@@ -1,1 +1,9 @@
 package com.baghdad.viewmodel.myLists
+
+import com.baghdad.entity.savedList.SavedList
+
+fun SavedList.toUiState() = MyListsScreenState.SavedListUiState(
+    id = this.id,
+    name = this.name,
+    itemCount = this.itemCount
+)
