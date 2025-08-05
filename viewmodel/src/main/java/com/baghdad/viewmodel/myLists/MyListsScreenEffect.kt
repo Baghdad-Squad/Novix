eabeb368e121221561935698304c2e6f2a688ba1
Arzo-zi <zinahashraf88@gmail.com>
2025-08-05 17:36:29 +0300

    feat: add navigation effects to MyListsScreenEffect

--- viewmodel/src/main/java/com/baghdad/viewmodel/myLists/MyListsScreenEffect.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/myLists/MyListsScreenEffect.kt
@@ -2,4 +2,7 @@ package com.baghdad.viewmodel.myLists
 
 import com.baghdad.viewmodel.base.BaseUiEffect
 
-sealed interface MyListsScreenEffect : BaseUiEffect
+sealed interface MyListsScreenEffect : BaseUiEffect {
+    data class NavigateToViewSavedDetails(val listId: Long) : MyListsScreenEffect
+    data object NavigateToLogin : MyListsScreenEffect
+}
