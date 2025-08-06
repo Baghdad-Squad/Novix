c5740727b6601785bf1e41d1bb60f2321f3e555e
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-08-06 13:56:29 +0300

    refactor: remove tvshow details navigation from SavedListDetailsEffect

--- viewmodel/src/main/java/com/baghdad/viewmodel/savedListDetails/SavedListDetailsEffect.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/savedListDetails/SavedListDetailsEffect.kt
@@ -4,6 +4,5 @@ import com.baghdad.viewmodel.base.BaseUiEffect
 
 sealed interface SavedListDetailsEffect : BaseUiEffect{
     data class NavigateToMovieDetails(val movieId: Long): SavedListDetailsEffect
-    data class NavigateToTvShowDetails(val tvShowId: Long):SavedListDetailsEffect
     data object NavigateBack: SavedListDetailsEffect
 }
