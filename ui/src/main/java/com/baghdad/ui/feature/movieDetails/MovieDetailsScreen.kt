387befbf02263b7c536d2750dbebc221fb92e682
Farah315 <fara734isgreat@gmail.com>
2025-08-06 12:33:25 +0300

    feat: add loading state for movie details screen

--- ui/src/main/java/com/baghdad/ui/feature/movieDetails/MovieDetailsScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/movieDetails/MovieDetailsScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
@@ -42,6 +43,7 @@ import com.baghdad.design_system.component.SaveIcon
 import com.baghdad.design_system.component.Scaffold
 import com.baghdad.design_system.component.SnackBar
 import com.baghdad.design_system.component.Text
+import com.baghdad.design_system.component.WavyLoadingIndicator
 import com.baghdad.design_system.component.appBar.TopAppBar
 import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.R
@@ -197,6 +199,11 @@ private fun MovieDetailsContent(
             )
         }
     ) {
+        if (state.isLoading) {
+            Box(Modifier.fillMaxSize()) {
+                WavyLoadingIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        }
         Box(
             modifier = modifier
                 .background(Theme.color.surface)
