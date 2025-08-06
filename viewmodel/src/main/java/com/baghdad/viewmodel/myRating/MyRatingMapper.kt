dec690442801f8f05f282a3c483fc56e06b942e1
Aboud <gh.abd.com.207@gmail.com>
2025-08-06 06:09:48 +0300

    feat: implement initial MyRating ViewModel, State, Effect, and InteractionListener and mapper

---
+++ viewmodel/src/main/java/com/baghdad/viewmodel/myRating/MyRatingMapper.kt
@@ -0,0 +1,22 @@
+package com.baghdad.viewmodel.myRating
+
+import com.baghdad.entity.media.Movie
+import com.baghdad.entity.media.TvShow
+
+fun Movie.toMediaItemUiState(): MyRatingState.MediaItemUiState {
+    return MyRatingState.MediaItemUiState(
+        id = id,
+        posterPictureURL = posterImageURL,
+        contentType = MyRatingState.ContentType.MOVIE,
+        rating = userRating?.toInt().toString(),
+    )
+}
+
+fun TvShow.toMediaItemUiState(): MyRatingState.MediaItemUiState {
+    return MyRatingState.MediaItemUiState(
+        id = id,
+        posterPictureURL = posterImageURL,
+        contentType = MyRatingState.ContentType.TV_SHOW,
+        rating = userRating?.toInt().toString(),
+    )
+}
---
+++ viewmodel/src/main/java/com/baghdad/viewmodel/myRating/MyRatingViewModel.kt
@@ -0,0 +1,125 @@
+package com.baghdad.viewmodel.myRating
+
+import com.baghdad.domain.usecase.movie.DeleteMovieRateUseCase
+import com.baghdad.domain.usecase.movie.GetUserRatedMoviesUseCase
+import com.baghdad.domain.usecase.tvShow.DeleteTvShowRateUseCase
+import com.baghdad.domain.usecase.tvShow.GetUserRatedTvShowsUseCase
+import com.baghdad.viewmodel.base.BaseViewModel
+import com.baghdad.viewmodel.errorStates.BaseSnackBarMessage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyRatingViewModel @Inject constructor(
+    private val getUserRatedMoviesUseCase: GetUserRatedMoviesUseCase,
+    private val getUserRatedTvShowsUseCase: GetUserRatedTvShowsUseCase,
+    private val deleteMovieRateUseCase: DeleteMovieRateUseCase,
+    private val deleteTvShowRateUseCase: DeleteTvShowRateUseCase,
+) : BaseViewModel<MyRatingState, MyRatingEffect>(MyRatingState()),
+    MyRatingInteractionListener {
+
+    init {
+        fetchUserRatedMedia()
+    }
+
+    private fun fetchUserRatedMedia() {
+        when (currentState.selectedMediaTab) {
+            MyRatingState.MediaTab.MOVIE -> fetchMovies(pageSize = 20)
+            MyRatingState.MediaTab.TV_SHOW -> fetchTvShows(pageSize = 20)
+            else -> {
+                fetchTvShows(pageSize = 10)
+                fetchMovies(pageSize = 10)
+            }
+        }
+    }
+
+    private fun fetchMovies(pageSize: Int) {
+        collectPagingFlow(
+            loadData = { page -> getUserRatedMoviesUseCase(page, pageSize) },
+            onInitialLoadFinished = ::onFinally,
+            mapEntityToUiState = { it.toMediaItemUiState() },
+            onFlowCreated = { mediaFlow -> updateState { it.copy(mediaFlow = mediaFlow) } },
+            onLoadingChanged = ::onGetMediaLoadingChanged,
+        )
+    }
+
+    private fun fetchTvShows(pageSize: Int) {
+        collectPagingFlow(
+            loadData = { page -> getUserRatedTvShowsUseCase(page, pageSize) },
+            onInitialLoadFinished = ::onFinally,
+            mapEntityToUiState = { it.toMediaItemUiState() },
+            onFlowCreated = { mediaFlow -> updateState { it.copy(mediaFlow = mediaFlow) } },
+            onLoadingChanged = ::onGetMediaLoadingChanged,
+        )
+    }
+
+    private fun onGetMediaLoadingChanged(isLoading: Boolean) {
+        updateState { it.copy(isLoading = isLoading) }
+    }
+
+    override fun mapThrowableToErrorMessage(throwable: Throwable): BaseSnackBarMessage {
+        return BaseSnackBarMessage.DefaultMessage
+    }
+
+    override fun onBackClick() {
+        sendEffect(MyRatingEffect.NavigateBack)
+    }
+
+    override fun onSnackBarActionLabelClick() {
+        sendEffect(MyRatingEffect.NavigateBack)
+    }
+
+    override fun onMediaClick(
+        mediaId: Long,
+        contentType: MyRatingState.ContentType
+    ) {
+        when (contentType) {
+            MyRatingState.ContentType.MOVIE -> sendEffect(
+                MyRatingEffect.NavigateToMovieDetails(
+                    mediaId
+                )
+            )
+
+            MyRatingState.ContentType.TV_SHOW -> sendEffect(
+                MyRatingEffect.NavigateToTvShowDetails(
+                    mediaId
+                )
+            )
+        }
+    }
+
+    override fun onMediaTabClick(mediaTab: MyRatingState.MediaTab?) {
+        updateState {
+            currentState.copy(selectedMediaTab = mediaTab, isLoading = true)
+        }
+        fetchUserRatedMedia()
+    }
+
+    override fun onDeleteClick(mediaId: Long, contentType: MyRatingState.ContentType) {
+        when (contentType) {
+            MyRatingState.ContentType.MOVIE -> onDelete { deleteMovieRateUseCase(mediaId) }
+            MyRatingState.ContentType.TV_SHOW -> onDelete { deleteTvShowRateUseCase(mediaId) }
+        }
+    }
+
+    private fun onDelete(cally: suspend () -> Unit) {
+        tryToExecute(
+            { cally() },
+            { onDeletedSuccess() },
+        )
+    }
+
+    private fun onDeletedSuccess() {
+        showSnackBar(
+            message = BaseSnackBarMessage.RemovedItemSuccessfully,
+            isSuccess = true
+        )
+    }
+
+    private fun onFinally() {
+        updateState {
+            currentState.copy(isLoading = false)
+        }
+    }
+
+}
