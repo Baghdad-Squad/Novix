5c977e04e977aec84f9a7bf0260bb3887a6b764e
Farah315 <fara734isgreat@gmail.com>
2025-08-06 12:46:47 +0300

    feat: show loading indicator on trending movies screen

--- ui/src/main/java/com/baghdad/ui/feature/trendingMovies/TrendingMoviesScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/trendingMovies/TrendingMoviesScreen.kt
@@ -2,6 +2,7 @@ package com.baghdad.ui.feature.trendingMovies
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -21,6 +23,7 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.baghdad.design_system.component.Scaffold
 import com.baghdad.design_system.component.SnackBar
+import com.baghdad.design_system.component.WavyLoadingIndicator
 import com.baghdad.design_system.component.appBar.TopAppBar
 import com.baghdad.design_system.theme.Theme
 import com.baghdad.ui.R
@@ -120,6 +123,11 @@ private fun TrendingMoviesContent(
         },
         isLoading = uiState.isLoading
     ) {
+        if (uiState.isLoading) {
+            Box(Modifier.fillMaxSize()) {
+                WavyLoadingIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
