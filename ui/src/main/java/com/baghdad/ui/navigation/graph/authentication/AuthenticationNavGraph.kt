2b1964fa1c2c1808d9298fc8f48d00ec029b8096
edrees <muhammed.edrees101@gmail.com>
2025-08-06 20:03:57 +0300

    fix: update navigation flow to redirect to login screen and improve localization strings

--- ui/src/main/java/com/baghdad/ui/navigation/graph/authentication/AuthenticationNavGraph.kt
+++ ui/src/main/java/com/baghdad/ui/navigation/graph/authentication/AuthenticationNavGraph.kt
@@ -95,11 +95,7 @@ private fun handleAuthenticationNavigation(
         }
 
         AuthenticationNavEvent.NavigateToLogin -> {
-            navController.navigate(AuthenticationRoute.LoginScreen) {
-                popUpTo(Graph.AuthenticationGraph) {
-                    inclusive = true
-                }
-            }
+            navController.navigate(AuthenticationRoute.LoginScreen)
         }
     }
 }
