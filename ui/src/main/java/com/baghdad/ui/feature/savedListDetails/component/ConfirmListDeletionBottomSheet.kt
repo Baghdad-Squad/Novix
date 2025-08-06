2ee262495cf5cffb4fb5546f243ed7efed0940dd
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-08-06 05:24:26 +0300

    feat: implement list deletion confirmation bottom sheet

---
+++ ui/src/main/java/com/baghdad/ui/feature/savedListDetails/component/ConfirmListDeletionBottomSheet.kt
@@ -0,0 +1,62 @@
+package com.baghdad.ui.feature.savedListDetails.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.baghdad.design_system.component.BaseBottomSheet
+import com.baghdad.design_system.component.button.PrimaryButton
+import com.baghdad.ui.feature.component.bottomSheet.EmptyMediaState
+import com.baghdad.ui.feature.search.component.BottomSheetHeader
+
+@Composable
+fun ConfirmListDeletionBottomSheet(
+    onBottomSheetCloseClick: () -> Unit,
+    title: String,
+    description: String,
+    isVisible: Boolean,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    BaseBottomSheet(
+        isVisible = isVisible,
+        onDismiss = { onBottomSheetCloseClick() },
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+
+            BottomSheetHeader(
+                onCloseClick = { onBottomSheetCloseClick() },
+                title = title,
+            )
+
+            EmptyMediaState(
+                imagePath = com.baghdad.ui.R.drawable.trash,
+                contentDescription = stringResource(com.baghdad.ui.R.string.bottom_sheet_content_description),
+                message = description,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            PrimaryButton(
+                label = stringResource(com.baghdad.ui.R.string.delete),
+                onClick = onDeleteClick,
+                modifier = Modifier.fillMaxWidth(),
+                isEnabled = true
+            )
+        }
+    }
+}
