f30ff8f58c7dc6f78842d02349137f0e9fd23cf0
omer1998 <omerfaris11@gmail.com>
2025-08-06 03:41:49 +0300

    feat: implement HeroCarousel component to improve carousel experience

--- design_system/src/main/java/com/baghdad/design_system/component/HorizontalCarousel.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/HorizontalCarousel.kt
@@ -3,10 +3,8 @@ package com.baghdad.design_system.component
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
@@ -24,40 +22,17 @@ fun <T> HorizontalCarousel(
     itemSpacing: Dp = 8.dp,
     largeItemWidth: Dp = 158.dp,
     smallItemWidth: Dp = 74.dp,
-    shape : Shape = RoundedCornerShape(12.dp),
+    shape: Shape = RoundedCornerShape(12.dp),
     state: CarouselState = rememberCarouselState { items.count() },
-    itemContent: @Composable (T , showSaveIcon : Boolean) -> Unit
+    itemContent: @Composable (T, showSaveIcon: Boolean) -> Unit
 ) {
-//    val state = rememberCarouselState { items.count() }
-//
-//    HorizontalMultiBrowseCarousel(
-//        state = state,
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .wrapContentHeight(),
-//        preferredItemWidth = largeItemWidth,
-//        minSmallItemWidth = smallItemWidth,
-//        maxSmallItemWidth = smallItemWidth,
-//        itemSpacing = itemSpacing,
-//        contentPadding = contentPadding
-//    ) { i ->
-//        val item = items[i]
-//
-//        Box(
-//            modifier =Modifier
-//                .fillMaxWidth()
-//                .maskClip(shape)
-//        ) {
-//            itemContent(item, true)
-//        }
-//    }
     HeroCarousel(
         modifier = modifier,
         carouselState = state,
         heroItemSize = largeItemWidth,
         smallItemSize = smallItemWidth,
         itemSpacing = itemSpacing,
-        contentPadding =contentPadding
+        contentPadding = contentPadding
     ) { index ->
         val item = items[index]
 
