aff0072eff2e9577e7382fce325a2316e5a9c444
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-08-08 00:30:22 +0300

    refactor: convert the PNG to WEPB, add fixed size to Empty Icon

--- ui/src/main/java/com/baghdad/ui/feature/savedListDetails/component/EmptyListScreen.kt
+++ ui/src/main/java/com/baghdad/ui/feature/savedListDetails/component/EmptyListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,7 +31,9 @@ fun EmptyListScreen() {
             Icon(
                 painter = showIconDependsOnTheme(),
                 contentDescription = stringResource(R.string.empty_list),
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                modifier = Modifier
+                    .size(128.dp)
+                    .align(Alignment.CenterHorizontally)
             )
 
             Text(
