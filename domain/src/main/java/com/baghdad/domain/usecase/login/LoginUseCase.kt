b0b5a2eae1323e24f37b3ad25267d132233ddc3b
omer1998 <omerfaris11@gmail.com>
2025-08-07 14:45:52 +0300

    refactor: improve login error handling, and remove character restriction to be aligned with the original website

--- domain/src/main/java/com/baghdad/domain/usecase/login/LoginUseCase.kt
+++ domain/src/main/java/com/baghdad/domain/usecase/login/LoginUseCase.kt
@@ -1,5 +1,7 @@
 package com.baghdad.domain.usecase.login
 
+import com.baghdad.domain.exception.EmptyFieldException
+import com.baghdad.domain.exception.InValidPasswordException
 import com.baghdad.domain.repository.AuthenticationRepository
 import javax.inject.Inject
 
@@ -8,9 +10,15 @@ class LoginUseCase @Inject constructor(
     private val authenticationRepository: AuthenticationRepository
 ) {
     suspend operator fun invoke(userName: String, password: String) {
+        validateCredential(userName, password)
         authenticationRepository.login(
             userName = userName,
             password = password
         )
+    }
+
+    fun validateCredential(userName: String, password: String) {
+        if (password.isEmpty() || userName.isEmpty()) throw EmptyFieldException()
+        if (password.length < 4) throw InValidPasswordException()
     }
 }
