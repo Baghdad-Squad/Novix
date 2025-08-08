8b0b9e468db589c52d3bfa6408d4ab7a17d04d95
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-08-08 04:18:28 +0200

    refactor: add duration in arabic language

--- viewmodel/src/main/java/com/baghdad/viewmodel/util/DateTimeUtil.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/util/DateTimeUtil.kt
@@ -29,32 +29,27 @@ fun LocalDate.toDDMMYYYYFormat(): String {
     return this.format(format)
 }
 
+
 fun LocalDate.toDDMMMYYYYFormat(): String {
     val currentLocale = Locale.getDefault()
     val isArabic = currentLocale.language == "ar"
 
-    val monthNames = if (isArabic) {
-        MonthNames(
+    return if (isArabic) {
+        val arabicMonths = arrayOf(
             "يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو",
             "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"
         )
+
+        val monthName = arabicMonths[this.monthNumber - 1]
+        "${this.dayOfMonth} $monthName ${this.year}"
     } else {
-        MonthNames.ENGLISH_ABBREVIATED
-    }
-
-    val format = LocalDate.Format {
-        day(padding = Padding.NONE)
-        char(' ')
-        monthName(names = monthNames)
-        char(' ')
-        year(padding = Padding.ZERO)
-    }
-
-    val formattedDate = this.format(format)
-
-    return if (isArabic) {
-        formattedDate
-    } else {
-        formattedDate
+        val format = LocalDate.Format {
+            dayOfMonth(padding = Padding.NONE)
+            char(' ')
+            monthName(names = MonthNames.ENGLISH_ABBREVIATED)
+            char(' ')
+            year(padding = Padding.ZERO)
+        }
+        this.format(format)
     }
 }
