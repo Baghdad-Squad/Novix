330f086ecce724062ecbcfa1541db87c7ad2a713
Aboud <gh.abd.com.207@gmail.com>
2025-08-08 03:15:16 +0300

    feat: add localization for delete icon content description

--- design_system/src/main/java/com/baghdad/design_system/component/DeleteChip.kt
+++ design_system/src/main/java/com/baghdad/design_system/component/DeleteChip.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +39,7 @@ fun DeleteChip(
     ) {
         Icon(
             imageVector = ImageVector.vectorResource(R.drawable.ic_delete),
-            contentDescription = "delete icon",
+            contentDescription = stringResource(R.string.delete_icon),
             tint = Theme.color.redAccent,
             modifier = Modifier.size(20.dp)
         )
