6889622a4c60123a1cd9dc62b857be2b56cb6307
Fares <dev.faresmohamed@gmail.com>
2025-08-11 15:32:41 +0300

    fix: update getAppLanguage return type to nullable

--- repository/src/main/java/com/baghdad/repository/datasource/local/LocalUserPreferencesDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/LocalUserPreferencesDataSource.kt
@@ -5,6 +5,6 @@ import kotlinx.coroutines.flow.Flow
 interface LocalUserPreferencesDataSource {
     suspend fun isAppInDarkTheme(): Flow<Boolean>
     suspend fun setDarkTheme(enabled: Boolean)
-    suspend fun getAppLanguage(): Flow<String>
+    suspend fun getAppLanguage(): Flow<String?>
     suspend fun setAppLanguage(language: String)
 }
