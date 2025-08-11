d9f97cd892604e5073b1dc5876e70de5102de263
Farah315 <fara734isgreat@gmail.com>
2025-08-12 00:45:21 +0300

    refactor: remove unused ActorDto.toSearchQueryDto function

--- repository/src/main/java/com/baghdad/repository/mapper/ActorMapper.kt
+++ repository/src/main/java/com/baghdad/repository/mapper/ActorMapper.kt
@@ -2,7 +2,6 @@ package com.baghdad.repository.mapper
 
 import com.baghdad.entity.person.Actor
 import com.baghdad.repository.model.ActorDto
-import com.baghdad.repository.model.SearchQueryDto
 import kotlinx.datetime.LocalDate
 
 fun ActorDto.toEntity(): Actor {
@@ -20,18 +19,5 @@ fun ActorDto.toEntity(): Actor {
         placeOfBirth = placeOfBirth,
         headerPictures = headerPictures,
         department = department
-    )
-}
-
-/**
- * we wanna change exaction  fun to dto
- * طب ليهه
- * نشيليهااااااااا
- */
-fun ActorDto.toSearchQueryDto(query: String): SearchQueryDto {
-    return SearchQueryDto(
-        queryName = query,
-        mediaId = id,
-        mediaType = SearchQueryDto.MediaType.ACTOR
     )
 }
