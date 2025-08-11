8ce69530255024ddafe4868f61de13c357e968bc
Arzo-zi <zinahashraf88@gmail.com>
2025-08-11 22:12:37 +0300

    refactor: improve readability in mapper functions

--- repository/src/main/java/com/baghdad/repository/mapper/SavedListItemMapper.kt
+++ repository/src/main/java/com/baghdad/repository/mapper/SavedListItemMapper.kt
@@ -3,9 +3,10 @@ package com.baghdad.repository.mapper
 import com.baghdad.domain.model.savedList.SavableMovie
 import com.baghdad.repository.model.savedList.SavableMovieDto
 
-fun SavableMovieDto.toEntity() =
-    SavableMovie(
+fun SavableMovieDto.toEntity(): SavableMovie {
+    return SavableMovie(
         movie = movie.toEntity(),
         isSaved = isSaved,
-        listId = listId,
+        listId = listId
     )
+}
