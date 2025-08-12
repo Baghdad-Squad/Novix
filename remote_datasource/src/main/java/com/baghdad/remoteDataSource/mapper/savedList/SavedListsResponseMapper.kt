10eb34fba216b26b71b4b20993a643f14d842810
edrees <muhammed.edrees101@gmail.com>
2025-08-12 05:31:48 +0300

    refactor: rename mappers for clarity and improve null handling in response transformations

--- remote_datasource/src/main/java/com/baghdad/remoteDataSource/mapper/savedList/SavedListsResponseMapper.kt
+++ remote_datasource/src/main/java/com/baghdad/remoteDataSource/mapper/savedList/SavedListsResponseMapper.kt
@@ -1,22 +1,24 @@
-package com.baghdad.remoteDataSource.mapper
+package com.baghdad.remoteDataSource.mapper.savedList
 
 import com.baghdad.remoteDataSource.response.savedList.UserListsResponse
 import com.baghdad.remoteDataSource.util.getNextKey
 import com.baghdad.remoteDataSource.util.getPreviousKey
 import com.baghdad.repository.model.PagedResultDto
 import com.baghdad.repository.model.SavedListDto
+
 fun UserListsResponse.toPagedSavedListsDtos(): PagedResultDto<SavedListDto> {
     return PagedResultDto(
-        data = results?.takeIf { it.isNotEmpty()}?.map{ it.toSavedListDto() } ?: emptyList(),
+        data = this.toSavedListDtos(),
         nextKey = getNextKey(page, totalPages),
         prevKey = getPreviousKey(page)
     )
 }
 
-internal fun UserListsResponse.UserListDto.toSavedListDto(): SavedListDto {
-    return SavedListDto(
-        id = id ?: 0,
+private fun UserListsResponse.toSavedListDtos(): List<SavedListDto> = results.orEmpty().map { it.toSavedListDto() }
+
+private fun UserListsResponse.UserListDto.toSavedListDto(): SavedListDto =
+    SavedListDto(
+        id = id ?: 0L,
         name = name.orEmpty(),
         itemCount = itemCount ?: 0,
     )
-}
