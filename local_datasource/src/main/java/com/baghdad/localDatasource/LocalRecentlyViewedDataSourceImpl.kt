422b54b24454b30e893d0605f5d65908fb9227ce
Fares <dev.faresmohamed@gmail.com>
2025-08-12 17:07:22 +0300

    refactor: implement LocalRecentlyViewedDataSource with CRUD operations

--- local_datasource/src/main/java/com/baghdad/localDatasource/LocalRecentlyViewedDataSourceImpl.kt
+++ local_datasource/src/main/java/com/baghdad/localDatasource/LocalRecentlyViewedDataSourceImpl.kt
@@ -1,11 +1,11 @@
-package com.baghdad.local_datasource
+package com.baghdad.localDatasource
 
-import com.baghdad.local_datasource.roomDB.dao.RecentlyViewedDao
-import com.baghdad.local_datasource.roomDB.entity.RecentlyViewed
-import com.baghdad.local_datasource.roomDB.entity.toDto
-import com.baghdad.local_datasource.roomDB.entity.toLocalDto
-import com.baghdad.local_datasource.roomDB.errorHandler.executeFlowWithErrorHandling
-import com.baghdad.local_datasource.roomDB.errorHandler.executeWithErrorHandling
+import com.baghdad.localDatasource.errorHandler.executeFlowWithErrorHandling
+import com.baghdad.localDatasource.errorHandler.executeWithErrorHandling
+import com.baghdad.localDatasource.mapper.toDto
+import com.baghdad.localDatasource.mapper.toLocalDto
+import com.baghdad.localDatasource.roomDB.dao.RecentlyViewedDao
+import com.baghdad.localDatasource.roomDB.entity.RecentlyViewed
 import com.baghdad.repository.datasource.local.LocalRecentlyViewedDataSource
 import com.baghdad.repository.logger.Logger
 import com.baghdad.repository.model.RecentlyViewedDto
@@ -19,18 +19,22 @@ class LocalRecentlyViewedDataSourceImpl @Inject constructor(
     private val recentlyViewedDao: RecentlyViewedDao,
     private val logger: Logger
 ) : LocalRecentlyViewedDataSource {
-    override fun getAllRecentlyViewed(): Flow<List<RecentlyViewedDto>> =
-        executeFlowWithErrorHandling(logger = logger) {
+    override fun getAllRecentlyViewed(): Flow<List<RecentlyViewedDto>> {
+        return executeFlowWithErrorHandling(logger = logger) {
             recentlyViewedDao.getAllRecentlyViewed().map { it.map(RecentlyViewed::toDto) }
         }
+    }
 
-    override suspend fun deleteAllRecentlyViewed() =
+    override suspend fun deleteAllRecentlyViewed() {
         executeWithErrorHandling(logger = logger) {
-            recentlyViewedDao.clearAllRecentlyViewed()
+            recentlyViewedDao.deleteAllRecentlyViewed()
         }
+    }
 
-    override suspend fun addMediaToRecentlyViewed(recentlyViewedDto: RecentlyViewedDto) =
+    override suspend fun addMediaToRecentlyViewed(recentlyViewedDto: RecentlyViewedDto) {
         executeWithErrorHandling(logger = logger) {
             recentlyViewedDao.upsertRecentlyViewed(recentlyViewedDto.toLocalDto())
         }
+    }
+
 }
