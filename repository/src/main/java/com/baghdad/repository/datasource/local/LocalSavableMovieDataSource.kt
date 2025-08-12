0c9b03fb964f697bc3aa6efc756f74d35e4d095a
Farah315 <fara734isgreat@gmail.com>
2025-08-12 10:59:34 +0300

    refactor: reformat LocalSavableMovieDataSource

--- repository/src/main/java/com/baghdad/repository/datasource/local/LocalSavableMovieDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/LocalSavableMovieDataSource.kt
@@ -2,10 +2,16 @@ package com.baghdad.repository.datasource.local
 
 import com.baghdad.repository.model.savedList.SavableMovieDto
 
-/**
- *  we wanna sure to format
- */
 interface LocalSavableMovieDataSource {
+
+    suspend fun deleteSavedMovie(movieId: Long)
+
+    suspend fun deleteListMovies(listId: Long)
+
+    suspend fun deleteAllSavedMovies()
+
+    suspend fun getSavedMovies(): Map<Long, Long>
+
     suspend fun saveMovies(
         listId: Long,
         movies: List<SavableMovieDto>,
@@ -15,12 +21,4 @@ interface LocalSavableMovieDataSource {
         listId: Long,
         movieId: Long,
     )
-
-    suspend fun deleteSavedMovie(movieId: Long)
-
-    suspend fun deleteListMovies(listId: Long)
-
-    suspend fun deleteAllSavedMovies()
-
-    suspend fun getSavedMovies(): Map<Long, Long>
 }
