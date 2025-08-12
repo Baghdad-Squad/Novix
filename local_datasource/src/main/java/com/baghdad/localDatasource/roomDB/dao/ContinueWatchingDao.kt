cfa94a9cd0f60bf8c88303628e39f7cda3a60e56
Fares <dev.faresmohamed@gmail.com>
2025-08-12 17:04:51 +0300

    refactor: add ContinueWatchingDao for managing continue watching data

--- local_datasource/src/main/java/com/baghdad/localDatasource/roomDB/dao/ContinueWatchingDao.kt
+++ local_datasource/src/main/java/com/baghdad/localDatasource/roomDB/dao/ContinueWatchingDao.kt
@@ -1,9 +1,9 @@
-package com.baghdad.local_datasource.roomDB.dao
+package com.baghdad.localDatasource.roomDB.dao
 
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
-import com.baghdad.local_datasource.roomDB.entity.ContinueWatching
+import com.baghdad.localDatasource.roomDB.entity.ContinueWatching
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,13 +13,17 @@ interface ContinueWatchingDao {
     suspend fun upsertContinueWatching(continueWatching: ContinueWatching)
 
     @Query("SELECT * FROM ContinueWatching WHERE userId = :userId ORDER BY viewedAt DESC LIMIT :pageSize OFFSET :offset")
-    suspend fun getContinueWatching(userId: Long , pageSize: Int , offset: Int): List<ContinueWatching>
+    suspend fun getContinueWatching(
+        userId: Long,
+        pageSize: Int,
+        offset: Int
+    ): List<ContinueWatching>
 
     @Query("SELECT * FROM ContinueWatching WHERE userId = :userId ORDER BY viewedAt DESC")
     fun observeContinueWatching(userId: Long): Flow<List<ContinueWatching>>
 
     @Query("SELECT * FROM ContinueWatching WHERE userId = :userId and contentType = 'MOVIE' ORDER BY viewedAt DESC")
-    fun getAllContinueWatchingMovies(userId: Long):Flow<List<ContinueWatching>>
+    fun getAllContinueWatchingMovies(userId: Long): Flow<List<ContinueWatching>>
 
     @Query("SELECT * FROM ContinueWatching WHERE userId = :userId and contentType = 'TV_SHOW' ORDER BY viewedAt DESC")
     fun getAllContinueWatchingTvShows(userId: Long): Flow<List<ContinueWatching>>
