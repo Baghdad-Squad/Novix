a44613edee2abe033db18651dba594b64585e4ec
Fares <dev.faresmohamed@gmail.com>
2025-08-12 17:08:25 +0300

    refactor: rename LocalUserDataStore to LocalUserDataSource

--- repository/src/main/java/com/baghdad/repository/datasource/local/LocalUserDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/LocalUserDataSource.kt
@@ -2,7 +2,7 @@ package com.baghdad.repository.datasource.local
 
 import com.baghdad.repository.model.UserDto
 
-interface LocalUserDataStore {
+interface LocalUserDataSource {
     suspend fun saveUser(id: Long, userName: String, imageUrl: String)
     suspend fun getUser(): UserDto?
     suspend fun deleteUser()
