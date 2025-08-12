a95a823476ca59be5c36cb01919202cefad8e68a
Arzo-zi <zinahashraf88@gmail.com>
2025-08-13 00:41:59 +0300

    refactor: remove redundant newlines

--- repository/src/main/java/com/baghdad/repository/mapper/SavedListItemMapper.kt
+++ repository/src/main/java/com/baghdad/repository/mapper/SavedListItemMapper.kt
@@ -1,10 +1,10 @@
 package com.baghdad.repository.mapper
 
-import com.baghdad.domain.model.savedList.SavableMovie
+import com.baghdad.domain.model.savedList.SavedMovie
 import com.baghdad.repository.model.savedList.SavableMovieDto
 
-fun SavableMovieDto.toEntity(): SavableMovie {
-    return SavableMovie(
+fun SavableMovieDto.toEntity(): SavedMovie {
+    return SavedMovie(
         movie = movie.toEntity(),
         isSaved = isSaved,
         listId = listId
--- repository/src/main/java/com/baghdad/repository/mapper/UserMapper.kt
+++ repository/src/main/java/com/baghdad/repository/mapper/UserMapper.kt
@@ -1,6 +1,6 @@
 package com.baghdad.repository.mapper
 
-import com.baghdad.entity.User
+import com.baghdad.entity.user.User
 import com.baghdad.repository.model.UserDto
 
 fun UserDto.toEntity(): User {
