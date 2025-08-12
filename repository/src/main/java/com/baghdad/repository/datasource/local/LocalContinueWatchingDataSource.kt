5e0a0f1fb31818da5340807258c5257ead909267
Farah315 <fara734isgreat@gmail.com>
2025-08-12 10:59:21 +0300

    refactor: reorder methods in LocalContinueWatchingDataSource

--- repository/src/main/java/com/baghdad/repository/datasource/local/LocalContinueWatchingDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/LocalContinueWatchingDataSource.kt
@@ -5,16 +5,18 @@ import kotlinx.coroutines.flow.Flow
 
 
 interface LocalContinueWatchingDataSource {
+
     suspend fun addContinueWatching(continueWatching: ContinueWatchingDto)
+
+    fun observeContinueWatching(userId: Long): Flow<List<ContinueWatchingDto>>
+
+    fun getAllContinueWatchingMovies(userId: Long): Flow<List<ContinueWatchingDto>>
+
+    fun getAllContinueWatchingTvShows(userId: Long): Flow<List<ContinueWatchingDto>>
 
     suspend fun getContinueWatching(
         userId: Long,
         pageSize: Int,
         page: Int,
     ): List<ContinueWatchingDto>
-
-    fun observeContinueWatching(userId: Long): Flow<List<ContinueWatchingDto>>
-
-    fun getAllContinueWatchingMovies(userId: Long): Flow<List<ContinueWatchingDto>>
-    fun getAllContinueWatchingTvShows(userId: Long): Flow<List<ContinueWatchingDto>>
 }
