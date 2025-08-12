a9cb9972ae7dea6dfe450118660d6d28361dfd1d
Fares <dev.faresmohamed@gmail.com>
2025-08-12 17:13:03 +0300

    refactor: add RecentlyViewedDao for managing recently viewed items

--- local_datasource/src/main/java/com/baghdad/localDatasource/roomDB/dao/RecentlyViewedDao.kt
+++ local_datasource/src/main/java/com/baghdad/localDatasource/roomDB/dao/RecentlyViewedDao.kt
@@ -1,22 +1,20 @@
-package com.baghdad.local_datasource.roomDB.dao
+package com.baghdad.localDatasource.roomDB.dao
 
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
-import com.baghdad.local_datasource.roomDB.entity.RecentlyViewed
+import com.baghdad.localDatasource.roomDB.entity.RecentlyViewed
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecentlyViewedDao {
 
-    @Upsert()
+    @Upsert
     suspend fun upsertRecentlyViewed(recentlyViewed: RecentlyViewed)
 
+    @Query("DELETE FROM recently_viewed")
+    suspend fun deleteAllRecentlyViewed()
 
-    @Query("DELETE FROM RecentlyViewed")
-    suspend fun clearAllRecentlyViewed()
-
-
-    @Query("SELECT * FROM RecentlyViewed")
+    @Query("SELECT * FROM recently_viewed")
     fun getAllRecentlyViewed(): Flow<List<RecentlyViewed>>
 }
