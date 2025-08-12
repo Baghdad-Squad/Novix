b84a36f7fa61b6d2823ccde3504678f96c4ea863
Fares <dev.faresmohamed@gmail.com>
2025-08-12 17:02:49 +0300

    refactor: rename LocalUserPreferencesDataSource to AppConfigurationDataSource and add first-time status methods

--- repository/src/main/java/com/baghdad/repository/datasource/local/AppConfigurationDataSource.kt
+++ repository/src/main/java/com/baghdad/repository/datasource/local/AppConfigurationDataSource.kt
@@ -2,9 +2,11 @@ package com.baghdad.repository.datasource.local
 
 import kotlinx.coroutines.flow.Flow
 
-interface LocalUserPreferencesDataSource {
+interface AppConfigurationDataSource {
     suspend fun isAppInDarkTheme(): Flow<Boolean>
     suspend fun setDarkTheme(enabled: Boolean)
     suspend fun getAppLanguage(): Flow<String?>
     suspend fun setAppLanguage(language: String)
+    suspend fun setFirstTimeStatus()
+    suspend fun isFirstTime(): Boolean
 }
