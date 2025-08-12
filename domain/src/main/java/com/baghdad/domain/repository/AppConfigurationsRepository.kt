cdb702adb2105acfafd1e4a66d868663b56f02a1
Fares <dev.faresmohamed@gmail.com>
2025-08-12 17:03:07 +0300

    refactor: rename UserPreferencesRepository to AppConfigurationsRepository and add first-time user methods

--- domain/src/main/java/com/baghdad/domain/repository/AppConfigurationsRepository.kt
+++ domain/src/main/java/com/baghdad/domain/repository/AppConfigurationsRepository.kt
@@ -2,9 +2,11 @@ package com.baghdad.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 
-interface UserPreferencesRepository {
+interface AppConfigurationsRepository {
     suspend fun isAppInDarkTheme(): Flow<Boolean>
     suspend fun setDarkTheme(enabled: Boolean)
     suspend fun getAppLanguage(): Flow<String>
     suspend fun setAppLanguage(language: String)
+    suspend fun isFirstTimeUser(): Boolean
+    suspend fun setFirstTimeUser(firstTime: Boolean)
 }
