441da13e2ae8677b8d83bb61e9b392d68eacd539
Farah315 <fara734isgreat@gmail.com>
2025-08-13 17:19:51 +0300

    test: add test to ActorRepositoryImplTest

--- repository/src/test/java/com/baghdad/repository/EpisodeRepositoryImplTest.kt
+++ repository/src/test/java/com/baghdad/repository/EpisodeRepositoryImplTest.kt
@@ -4,9 +4,13 @@ import com.baghdad.repository.datasource.remote.RemoteEpisodeDataSource
 import com.baghdad.repository.datasource.remote.RemoteTvShowDataSource
 import com.baghdad.repository.dummyData.DummyDataFactory.DummyDataFactory.createMockCastMember
 import com.baghdad.repository.dummyData.DummyDataFactory.DummyDataFactory.createMockCastMemberDto
+import com.baghdad.repository.dummyData.DummyDataFactory.DummyDataFactory.createMockEpisode
 import com.baghdad.repository.dummyData.DummyDataFactory.DummyDataFactory.createMockEpisodeDto
+import com.baghdad.repository.dummyData.DummyDataFactory.DummyDataFactory.createMockGenreDto
 import com.baghdad.repository.model.CastMemberDto
 import com.baghdad.repository.model.EpisodeDto
+import com.baghdad.repository.model.GenreDto
+import com.baghdad.repository.model.TvShowDto
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -20,38 +24,16 @@ class EpisodeRepositoryImplTest {
     private lateinit var remoteTvShowDataSource: RemoteTvShowDataSource
     private lateinit var episodeRepositoryImpl: EpisodeRepositoryImpl
 
+    private val tvId = 123L
+    private val seasonNumber = 2
+    private val episodeNumber = 5
+
     @BeforeEach
     fun setUp() {
         remoteTvShowDataSource = mockk()
         remoteEpisodeDataSource = mockk()
         episodeRepositoryImpl =
             EpisodeRepositoryImpl(remoteEpisodeDataSource, remoteTvShowDataSource)
-    }
-
-    private fun verifyGetEpisodeCastMembersCalled() {
-        coVerify {
-            remoteEpisodeDataSource.getEpisodeCastMembers(tvId, seasonNumber, episodeNumber)
-        }
-    }
-
-    private fun verifyGetEpisodeDetailsCalled() {
-        coVerify {
-            remoteEpisodeDataSource.getEpisodeDetails(tvId, seasonNumber, episodeNumber)
-        }
-    }
-
-    private fun verifyGetEpisodeTrailerCalled() {
-        coVerify {
-            remoteEpisodeDataSource.getEpisodeTrailer(tvId, seasonNumber, episodeNumber)
-        }
-    }
-
-    private fun verifyGetTvShowImagesCalled() {
-        coVerify { remoteTvShowDataSource.getTvShowImages(tvId) }
-    }
-
-    private fun verifyGetTvShowDetailsCalled() {
-        coVerify { remoteTvShowDataSource.getTvShowDetails(tvId) }
     }
 
     @Test
@@ -77,9 +59,54 @@ class EpisodeRepositoryImplTest {
         verifyGetEpisodeCastMembersCalled()
     }
 
-    private val tvId = 123L
-    private val seasonNumber = 2
-    private val episodeNumber = 5
+    @Test
+    fun `getEpisodeDetails returns episode with additional data when remote calls succeed`() =
+        runTest {
+            val episodeDto = createMockEpisodeDto()
+            val trailerUrl = "https://youtube.com/watch?v=episode_trailer"
+            val tvShowImages = listOf("/image1.jpg", "/image2.jpg", "/image3.jpg")
+            val mockGenres = listOf(
+                createMockGenreDto(18, "Drama"),
+                createMockGenreDto(35, "Comedy")
+            )
+
+            mockGetEpisodeDetails(episodeDto)
+            mockGetEpisodeTrailer(trailerUrl)
+            mockGetTvShowImages(tvShowImages)
+            mockGetTvShowDetails(mockGenres)
+
+            val result = episodeRepositoryImpl.getEpisodeDetails(tvId, seasonNumber, episodeNumber)
+
+            val expectedEpisode = createMockEpisode().copy(
+                trailerUrl = trailerUrl,
+                headerPictures = tvShowImages
+            )
+
+            assertThat(result.id).isEqualTo(expectedEpisode.id)
+            assertThat(result.title).isEqualTo(expectedEpisode.title)
+            assertThat(result.trailerUrl).isEqualTo(trailerUrl)
+            assertThat(result.headerPictures).isEqualTo(tvShowImages)
+
+            verifyGetEpisodeDetailsCalled()
+            verifyGetEpisodeTrailerCalled()
+            verifyGetTvShowImagesCalled()
+            verifyGetTvShowDetailsCalled()
+        }
+
+    @Test
+    fun `addTvEpisodeRate calls remote data source with correct parameters`() = runTest {
+        val rating = 8
+
+        coEvery {
+            remoteEpisodeDataSource.addEpisodeRate(tvId, seasonNumber, episodeNumber, rating)
+        } returns Unit
+
+        episodeRepositoryImpl.addTvEpisodeRate(tvId, seasonNumber, episodeNumber, rating)
+
+        coVerify {
+            remoteEpisodeDataSource.addEpisodeRate(tvId, seasonNumber, episodeNumber, rating)
+        }
+    }
 
     private fun mockGetEpisodeCastMembers(returnValue: List<CastMemberDto>) {
         coEvery {
@@ -103,5 +130,42 @@ class EpisodeRepositoryImplTest {
         coEvery {
             remoteTvShowDataSource.getTvShowImages(tvId)
         } returns images
+    }
+
+    private fun mockGetTvShowDetails(genres: List<GenreDto>) {
+        val mockTvShowDetails = mockk<TvShowDto> {
+            coEvery { this@mockk.genres } returns genres
+        }
+
+        coEvery {
+            remoteTvShowDataSource.getTvShowDetails(tvId)
+        } returns mockTvShowDetails
+    }
+
+
+    private fun verifyGetEpisodeCastMembersCalled() {
+        coVerify {
+            remoteEpisodeDataSource.getEpisodeCastMembers(tvId, seasonNumber, episodeNumber)
+        }
+    }
+
+    private fun verifyGetEpisodeDetailsCalled() {
+        coVerify {
+            remoteEpisodeDataSource.getEpisodeDetails(tvId, seasonNumber, episodeNumber)
+        }
+    }
+
+    private fun verifyGetEpisodeTrailerCalled() {
+        coVerify {
+            remoteEpisodeDataSource.getEpisodeTrailer(tvId, seasonNumber, episodeNumber)
+        }
+    }
+
+    private fun verifyGetTvShowImagesCalled() {
+        coVerify { remoteTvShowDataSource.getTvShowImages(tvId) }
+    }
+
+    private fun verifyGetTvShowDetailsCalled() {
+        coVerify { remoteTvShowDataSource.getTvShowDetails(tvId) }
     }
 }
