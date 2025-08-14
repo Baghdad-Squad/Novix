33a6d8908de50f0a1d502b3cb2cebf9af9676c50
karrar-abbas <54599574+karrar-abbas@users.noreply.github.com>
2025-08-14 14:03:09 +0300

    test: add more test cases to increase coverage and change movie with saved movie

--- viewmodel/src/test/java/com/baghdad/viewmodel/topMoviePicks/TopMoviePicksViewModelTest.kt
+++ viewmodel/src/test/java/com/baghdad/viewmodel/topMoviePicks/TopMoviePicksViewModelTest.kt
@@ -2,16 +2,19 @@ package com.baghdad.viewmodel.topMoviePicks
 
 import androidx.lifecycle.SavedStateHandle
 import com.baghdad.domain.exception.NoInternetException
+import com.baghdad.domain.model.savedList.SavedMovie
 import com.baghdad.domain.usecase.actor.GetActorMoviesUseCase
 import com.baghdad.domain.usecase.login.IsUserLoggedInUseCase
 import com.baghdad.domain.usecase.savedList.AddMovieToSavedListUseCase
 import com.baghdad.domain.usecase.savedList.CreateSavedListUseCase
 import com.baghdad.domain.usecase.savedList.GetSavedListsUseCase
+import com.baghdad.domain.usecase.savedList.RemoveMovieFromSavedListUseCase
 import com.baghdad.entity.media.Genre
 import com.baghdad.entity.media.Movie
 import com.baghdad.viewmodel.errorStates.BaseSnackBarMessage
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -26,30 +29,25 @@ import org.junit.jupiter.api.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TopMoviePicksViewModelTest {
-    private lateinit var getActorMoviesUseCase: GetActorMoviesUseCase
-    private lateinit var createSavedListUseCase: CreateSavedListUseCase
-    private lateinit var isUserLoggedInUseCase: IsUserLoggedInUseCase
-    private lateinit var addMovieToSavedListUseCase: AddMovieToSavedListUseCase
-    private lateinit var removeMovieFromListUseCase: AddMovieToSavedListUseCase
-    private lateinit var getSavedListsUseCase: GetSavedListsUseCase
-    private lateinit var topMoviePicksViewModel: TopMoviePicksViewModel
+    private var getActorMoviesUseCase: GetActorMoviesUseCase = mockk()
+    private var createSavedListUseCase: CreateSavedListUseCase = mockk()
+    private var isUserLoggedInUseCase: IsUserLoggedInUseCase = mockk()
+    private var addMovieToSavedListUseCase: AddMovieToSavedListUseCase = mockk()
+    private var removeMovieFromListUseCase: RemoveMovieFromSavedListUseCase = mockk()
+    private var getSavedListsUseCase: GetSavedListsUseCase = mockk()
+    private lateinit var viewModel: TopMoviePicksViewModel
+    private val testDispatcher = StandardTestDispatcher()
+
     private val actorId = 123L
     private val movieId = 1L
-    private val testDispatcher = StandardTestDispatcher()
 
     @BeforeEach
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
-        getActorMoviesUseCase = mockk(relaxed = true)
-        createSavedListUseCase = mockk(relaxed = true)
-        isUserLoggedInUseCase = mockk(relaxed = true)
-        addMovieToSavedListUseCase = mockk(relaxed = true)
-        removeMovieFromListUseCase = mockk(relaxed = true)
-        getSavedListsUseCase = mockk(relaxed = true)
 
         coEvery { getActorMoviesUseCase(actorId) } returns mockedMovies()
 
-        topMoviePicksViewModel = TopMoviePicksViewModel(
+        viewModel = TopMoviePicksViewModel(
             savedStateHandle = savedStateHandle,
             getActorMoviesUseCase = getActorMoviesUseCase,
             createSavedListUseCase = createSavedListUseCase,
@@ -58,23 +56,21 @@ class TopMoviePicksViewModelTest {
             addMovieToSavedListUseCase = addMovieToSavedListUseCase,
             removeMovieFromListUseCase = removeMovieFromListUseCase,
             getSavedListsUseCase = getSavedListsUseCase,
-            defaultDispatcher = testDispatcher
         )
     }
 
     @Test
     fun `onMovieDetailsClicked should Navigate To MovieDetails when clicked`() = runTest {
-        // Given
+
         var receivedEffect: TopMoviePicksEffect? = null
         val job = launch {
-            topMoviePicksViewModel.uiEffect.collect { effect ->
+            viewModel.uiEffect.collect { effect ->
                 receivedEffect = effect
             }
         }
-        // When
-        topMoviePicksViewModel.onMovieDetailsClicked(movieId)
+        viewModel.onMovieDetailsClicked(movieId)
         advanceUntilIdle()
-        // Then
+
         assertThat(receivedEffect is TopMoviePicksEffect.NavigateToMovieDetails).isTrue()
         assertThat(movieId == (receivedEffect as TopMoviePicksEffect.NavigateToMovieDetails).movieId).isTrue()
         job.cancel()
@@ -82,58 +78,137 @@ class TopMoviePicksViewModelTest {
 
     @Test
     fun `onSaveMovieClicked should open bottom sheet when movie is not saved`() = runTest {
-        // Given
-        val initialState = topMoviePicksViewModel.uiState.value
+
+        val initialState = viewModel.uiState.value
         val movie = initialState.movies.find { it.id == movieId }!!
         assertThat(movie.isSaved).isFalse()
 
-        // When
-        topMoviePicksViewModel.onSaveMovieClicked(movie)
+
+        viewModel.onSaveMovieClicked(movie)
         advanceUntilIdle()
 
-        // Then
-        val updatedState = topMoviePicksViewModel.uiState.value
+
+        val updatedState = viewModel.uiState.value
         assertThat(updatedState.addToListBottomSheetState.isVisible).isTrue()
         assertThat(updatedState.addToListBottomSheetState.selectedItemId).isEqualTo(movieId)
     }
 
+    @Test
+    fun `onSaveMovieClicked should remove movie from list when is saved`() = runTest {
+
+        val initialState = viewModel.uiState.value
+        val movie = initialState.movies.find { it.id == movieId }!!.copy(isSaved = true)
+        coEvery { removeMovieFromListUseCase(any(), any()) } returns Unit
+
+        viewModel.onSaveMovieClicked(movie)
+        advanceUntilIdle()
+
+
+        coVerify { removeMovieFromListUseCase(any(), any()) }
+    }
+
+    @Test
+    fun `onSaveItemToListClicked should add movie to saved list when list is selected`() = runTest {
+
+        viewModel.onListSelected(123L)
+        coEvery { addMovieToSavedListUseCase(any(), any()) } returns Unit
+
+        viewModel.onSaveItemToListClicked()
+        advanceUntilIdle()
+
+
+        coVerify { addMovieToSavedListUseCase(any(), any()) }
+    }
+
+    @Test
+    fun `onSaveItemToListClicked should not add movie to saved list when list is not selected`() =
+        runTest {
+
+            coEvery { addMovieToSavedListUseCase(any(), any()) } returns Unit
+
+            viewModel.onSaveItemToListClicked()
+            advanceUntilIdle()
+
+
+            coVerify(inverse = true) { addMovieToSavedListUseCase(any(), any()) }
+        }
+
 
     @Test
     fun `onBackClicked should Navigate Back when clicked`() = runTest {
-        // Given
+
         var receivedEffect: TopMoviePicksEffect? = null
         val job = launch {
-            topMoviePicksViewModel.uiEffect.collect { effect ->
+            viewModel.uiEffect.collect { effect ->
                 receivedEffect = effect
             }
         }
-        // When
-        topMoviePicksViewModel.onBackClicked()
+
+        viewModel.onBackClicked()
         advanceUntilIdle()
-        // Then
+
         assertThat(receivedEffect is TopMoviePicksEffect.NavigateBack).isTrue()
         job.cancel()
     }
 
     @Test
+    fun `onCreatedListNameChanged should change list name when it is clicked`() = runTest {
+
+        val listName = "action"
+
+        viewModel.onCreatedListNameChanged(listName)
+        advanceUntilIdle()
+
+        assertThat(viewModel.uiState.value.addListBottomSheetState.listName == listName).isTrue()
+    }
+
+    @Test
+    fun `onCreateListBottomSheetDismiss should hide bottom sheet and show addToListBottomSheet when it is clicked`() =
+        runTest {
+
+            viewModel.onCreateListBottomSheetDismiss()
+            advanceUntilIdle()
+
+            val addListBottomSheetState = viewModel.uiState.value.addListBottomSheetState
+            val addToListBottomSheetState = viewModel.uiState.value.addToListBottomSheetState
+            assertThat(addListBottomSheetState.isVisible == false).isTrue()
+            assertThat(addListBottomSheetState.listName == "").isTrue()
+            assertThat(addListBottomSheetState.isLoading == false).isTrue()
+            assertThat(addToListBottomSheetState.isVisible == true).isTrue()
+        }
+
+    @Test
+    fun `onCreateListBottomSheetAddClick should create new list when it is clicked`() = runTest {
+
+        viewModel.onCreatedListNameChanged("action")
+        coEvery { createSavedListUseCase(any()) } returns Unit
+
+        viewModel.onCreateListBottomSheetAddClick()
+        advanceUntilIdle()
+
+
+        coVerify { createSavedListUseCase(any()) }
+    }
+
+    @Test
     fun `onSnackBarActionLabelClicked should show no internet snackBar when NoInternetException is thrown`() =
         runTest {
-            // Given
+
             coEvery { getActorMoviesUseCase(actorId) } throws NoInternetException()
             val emittedSnackBarMessages = mutableListOf<BaseSnackBarMessage>()
 
             val job = launch {
-                topMoviePicksViewModel.snackBarState.collect {
+                viewModel.snackBarState.collect {
                     emittedSnackBarMessages.add(it.message)
                 }
             }
 
-            // When
-            topMoviePicksViewModel.onSnackBarActionLabelClicked()
+
+            viewModel.onSnackBarActionLabelClicked()
             advanceUntilIdle()
             job.cancel()
 
-            // Then
+
             assertThat(emittedSnackBarMessages).contains(BaseSnackBarMessage.NetworkError)
         }
 
@@ -144,43 +219,55 @@ class TopMoviePicksViewModelTest {
             )
         )
 
-        private fun mockedMovies(): List<Movie> = listOf(
-            Movie(
-                id = 1L,
-                title = "Test Movie 1",
-                genres = listOf(Genre(28L, "Action")),
-                averageRating = 8.0,
-                userRating = 7.5,
-                releaseDate = LocalDate.parse("2023-01-01"),
-                overview = "Test movie overview 1",
-                posterImageURL = "/movie_poster_1.jpg",
-                runtimeMinutes = 120,
-                trailerURL = "https://youtube.com/watch?v=test1"
+        private fun mockedMovies(): List<SavedMovie> = listOf(
+            SavedMovie(
+                movie = Movie(
+                    id = 1L,
+                    title = "Test Movie 1",
+                    genres = listOf(Genre(28L, "Action")),
+                    averageRating = 8.0,
+                    userRating = 7.5,
+                    releaseDate = LocalDate.parse("2023-01-01"),
+                    overview = "Test movie overview 1",
+                    posterImageURL = "/movie_poster_1.jpg",
+                    runtimeMinutes = 120,
+                    trailerURL = "https://youtube.com/watch?v=test1"
+                ),
+                isSaved = false,
+                listId = null,
             ),
-            Movie(
-                id = 2L,
-                title = "Test Movie 2",
-                genres = listOf(Genre(35L, "Comedy")),
-                averageRating = 7.5,
-                userRating = 8.0,
-                releaseDate = LocalDate.parse("2023-02-01"),
-                overview = "Test movie overview 2",
-                posterImageURL = "/movie_poster_2.jpg",
-                runtimeMinutes = 95,
-                trailerURL = "https://youtube.com/watch?v=test2"
+            SavedMovie(
+                Movie(
+                    id = 2L,
+                    title = "Test Movie 2",
+                    genres = listOf(Genre(35L, "Comedy")),
+                    averageRating = 7.5,
+                    userRating = 8.0,
+                    releaseDate = LocalDate.parse("2023-02-01"),
+                    overview = "Test movie overview 2",
+                    posterImageURL = "/movie_poster_2.jpg",
+                    runtimeMinutes = 95,
+                    trailerURL = "https://youtube.com/watch?v=test2"
+                ),
+                isSaved = false,
+                listId = null,
             ),
-            Movie(
-                id = 3L,
-                title = "Test Movie 3",
-                genres = listOf(Genre(18L, "Drama")),
-                averageRating = 9.0,
-                userRating = 8.5,
-                releaseDate = LocalDate.parse("2023-03-01"),
-                overview = "Test movie overview 3",
-                posterImageURL = "/movie_poster_3.jpg",
-                runtimeMinutes = 150,
-                trailerURL = "https://youtube.com/watch?v=test3"
-            )
+            SavedMovie(
+                movie = Movie(
+                    id = 3L,
+                    title = "Test Movie 3",
+                    genres = listOf(Genre(18L, "Drama")),
+                    averageRating = 9.0,
+                    userRating = 8.5,
+                    releaseDate = LocalDate.parse("2023-03-01"),
+                    overview = "Test movie overview 3",
+                    posterImageURL = "/movie_poster_3.jpg",
+                    runtimeMinutes = 150,
+                    trailerURL = "https://youtube.com/watch?v=test3"
+                ),
+                isSaved = false,
+                listId = null,
+            ),
         )
     }
 }
