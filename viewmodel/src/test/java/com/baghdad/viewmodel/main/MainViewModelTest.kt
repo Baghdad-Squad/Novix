e75c67294a099aa93507f2cbb19d18f4f34e2cfe
Aboud <gh.abd.com.207@gmail.com>
2025-08-14 02:02:46 -0700

    test: remove Given-When-Then comments from MainViewModelTest

--- viewmodel/src/test/java/com/baghdad/viewmodel/main/MainViewModelTest.kt
+++ viewmodel/src/test/java/com/baghdad/viewmodel/main/MainViewModelTest.kt
@@ -1,6 +1,5 @@
 package com.baghdad.viewmodel.main
 
-import com.baghdad.domain.repository.AuthenticationRepository
 import com.baghdad.domain.usecase.appConfigurations.GetAppLanguageUseCase
 import com.baghdad.domain.usecase.appConfigurations.GetAppThemeUseCase
 import com.baghdad.domain.usecase.appConfigurations.IsFirstTimeLaunchAppUseCase
@@ -24,26 +23,17 @@ import org.junit.jupiter.api.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainViewModelTest {
-
-    private lateinit var mockAuthRepository: AuthenticationRepository
-    private lateinit var isUserLoggedInUseCase: IsUserLoggedInUseCase
-    private lateinit var isFirstTimeLaunchAppUseCase: IsFirstTimeLaunchAppUseCase
-    private lateinit var getAppThemeUseCase: GetAppThemeUseCase
-    private lateinit var getAppLanguageUseCase: GetAppLanguageUseCase
-    private lateinit var syncSavedMoviesUseCase: SyncSavedMoviesUseCase
-    private lateinit var viewModel: MainViewModel
+    private val isUserLoggedInUseCase: IsUserLoggedInUseCase = mockk()
+    private val isFirstTimeLaunchAppUseCase: IsFirstTimeLaunchAppUseCase = mockk()
+    private val getAppThemeUseCase: GetAppThemeUseCase = mockk()
+    private val getAppLanguageUseCase: GetAppLanguageUseCase = mockk()
+    private val syncSavedMoviesUseCase: SyncSavedMoviesUseCase = mockk()
     private val testDispatcher = StandardTestDispatcher()
+    private lateinit var viewModel: MainViewModel
 
     @BeforeEach
     fun setup() {
         Dispatchers.setMain(testDispatcher)
-
-        mockAuthRepository = mockk()
-        isUserLoggedInUseCase = mockk()
-        isFirstTimeLaunchAppUseCase = mockk()
-        getAppThemeUseCase = mockk()
-        getAppLanguageUseCase = mockk()
-        syncSavedMoviesUseCase = mockk()
 
         coEvery { isUserLoggedInUseCase.invoke() } returns true
         coEvery { isFirstTimeLaunchAppUseCase() } returns true
@@ -69,20 +59,16 @@ class MainViewModelTest {
 
     @Test
     fun `isUserLoggedIn should be called when ViewModel starts`() = runTest {
-        // Given
         coEvery { isUserLoggedInUseCase.invoke() } returns true
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         coVerify { isUserLoggedInUseCase.invoke() }
     }
 
     @Test
     fun `isLoading should remain true when authentication check is not yet complete`() = runTest {
-        // Given
         coEvery { isUserLoggedInUseCase.invoke() } coAnswers {
             delay(1000)
             true
@@ -93,82 +79,65 @@ class MainViewModelTest {
             true
         }
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceTimeBy(500)
 
-        // Then
         assertThat(viewModel.uiState.value.isLoading).isTrue()
     }
 
 
     @Test
     fun `should update isAppInDarkTheme when getAppThemeUseCase returns true`() = runTest {
-        // Given
         coEvery { getAppThemeUseCase() } returns flowOf(true)
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         assertThat(viewModel.uiState.value.isAppInDarkTheme).isTrue()
     }
 
     @Test
     fun `should update appLanguage when getAppLanguageUseCase returns language`() = runTest {
-        // Given
         coEvery { getAppLanguageUseCase() } returns flowOf("ar")
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         assertThat(viewModel.uiState.value.appLanguage).isEqualTo("ar")
     }
 
     @Test
     fun `should call syncSavedMoviesUseCase when user is logged in`() = runTest {
-        // Given
         coEvery { isUserLoggedInUseCase.invoke() } returns true
         coEvery { syncSavedMoviesUseCase() } returns Unit
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         coVerify { syncSavedMoviesUseCase() }
     }
 
     @Test
     fun `should not call syncSavedMoviesUseCase when user is not logged in`() = runTest {
-        // Given
         coEvery { isUserLoggedInUseCase.invoke() } returns false
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         coVerify(exactly = 0) { syncSavedMoviesUseCase() }
     }
 
     @Test
     fun `should set isLoading to false when all checks are complete`() = runTest {
-        // Given
         coEvery { isUserLoggedInUseCase.invoke() } returns true
         coEvery { isFirstTimeLaunchAppUseCase() } returns false
         coEvery { getAppThemeUseCase() } returns flowOf(true)
         coEvery { getAppLanguageUseCase() } returns flowOf("en")
         coEvery { syncSavedMoviesUseCase() } returns Unit
 
-        // When
         viewModel = createViewModel()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         assertThat(viewModel.uiState.value.isLoading).isFalse()
     }
 }
