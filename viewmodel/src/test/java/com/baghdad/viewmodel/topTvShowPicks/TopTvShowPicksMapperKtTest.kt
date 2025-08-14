8966e8381f80d6f4606622c8025b87ad3ca3cb36
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-08-14 16:11:52 +0300

    refactor: test cases for TopTvShowPicksMapper

--- viewmodel/src/test/java/com/baghdad/viewmodel/topTvShowPicks/TopTvShowPicksMapperKtTest.kt
+++ viewmodel/src/test/java/com/baghdad/viewmodel/topTvShowPicks/TopTvShowPicksMapperKtTest.kt
@@ -9,20 +9,16 @@ class TopTvShowPicksMapperKtTest {
 
     @Test
     fun `should map TvShow to TvShowUiState when toUIState called`() {
-        // When
         val mappedState = TV_SHOW.toUIState()
 
-        // Then
         assertThat(mappedState.id).isEqualTo(TV_SHOW.id)
         assertThat(mappedState.posterPictureURL).isEqualTo(TV_SHOW.posterImageURL)
     }
 
     @Test
     fun `should map TvShows to TvShowUiStates when toUIStateList called`() {
-        // When
         val mappedStates = TV_SHOWS.toUIStateList()
 
-        // Then
         assertThat(mappedStates.size).isEqualTo(TV_SHOWS.size)
         assertThat(mappedStates[0].id).isEqualTo(TV_SHOWS[0].id)
         assertThat(mappedStates[0].posterPictureURL).isEqualTo(TV_SHOWS[0].posterImageURL)
