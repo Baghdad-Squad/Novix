9140ac3ff742afd79a14053e17bf92fcc77dccb7
omer1998 <omerfaris11@gmail.com>
2025-08-16 04:19:10 +0300

    refactor: snackbar messages

--- viewmodel/src/main/java/com/baghdad/viewmodel/errorStates/BaseSnackBarMessage.kt
+++ viewmodel/src/main/java/com/baghdad/viewmodel/errorStates/BaseSnackBarMessage.kt
@@ -1,17 +1,17 @@
 package com.baghdad.viewmodel.errorStates
 
 sealed interface BaseSnackBarMessage {
-    data object LoginSuccessfully : LoginSnackBarMessage()
-    data object EmptyFieldError : LoginSnackBarMessage()
-    data object InValidPasswordError : LoginSnackBarMessage()
-    data object InValidCredentialsError : LoginSnackBarMessage()
+    data object LoginSuccessfully : LoginSnackBarMessage
+    data object EmptyFieldError : LoginSnackBarMessage
+    data object InValidPasswordError : LoginSnackBarMessage
+    data object InValidCredentialsError : LoginSnackBarMessage
     data object NetworkError : SearchSnackBarMessage
     data object UnAuthorizedError : SearchSnackBarMessage
     data object UnknownError : SearchSnackBarMessage
     data object DataBaseError : SearchSnackBarMessage
     data object DefaultMessage : SearchSnackBarMessage
-    data object NoInternetException : LoginSnackBarMessage()
-    data object LoginOutSuccessfully : LoginSnackBarMessage()
+    data object NoInternetException : LoginSnackBarMessage
+    data object LoginOutSuccessfully : LoginSnackBarMessage
     data object SavedItemSuccessfully : DetailsScreensSnackBarMessage
     data object RemovedItemSuccessfully : DetailsScreensSnackBarMessage
     data object ItemRateSuccessfully : DetailsScreensSnackBarMessage
